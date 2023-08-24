ca9eb3a5eb9c750a70d84c47abf7d66320599181
jasper <jasper@info.nl>
2023-08-24 13:59:53 +0200

    Chore/add eslint (#6)
    * feat: replace tslint for eslint
    
    * fix: several eslint warnings no-unused-vars
    
    * Revert "fix: several eslint warnings no-unused-vars"
    
    This reverts commit b8182c1abebe972358d6bc7be1b2504f46192bbe.
    
    * fix: build app by removing no-unused-vars rule
    
    * fix: add no-prototype-builtins linting rule
    
    * fix: lint karma test config file

---
+++ src/main/app/.eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  ignorePatterns: ["dist", "coverage"],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  overrides: [
+    {
+      files: ["*.ts"],
+      parserOptions: {
+        project: "tsconfig.json",
+        tsconfigRootDir: __dirname,
+        sourceType: "module",
+      },
+      extends: [
+        "plugin:@angular-eslint/recommended",
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
+      ],
+      rules: {
+        "@angular-eslint/component-class-suffix": [
+          "off",
+          {
+            suffixes: ["Component", "Page", "Dialog"],
+          },
+        ],
+        "@angular-eslint/no-empty-lifecycle-method": "off",
+        "@angular-eslint/no-output-on-prefix": "off",
+        "@angular-eslint/no-output-native": "off",
+
+        "@typescript-eslint/ban-types": "off",
+        "@typescript-eslint/no-empty-function": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/no-unused-vars": "off",
+
+        "no-prototype-builtins": "off",
+      },
+    },
+  ],
+};
