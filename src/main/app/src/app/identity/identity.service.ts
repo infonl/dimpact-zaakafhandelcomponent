420b4ab44483e0fc168ee6c82ae25923000d38fa
Rick <rwoltheus@gmail.com>
2023-09-12 13:51:25 +0200

    Feature/pz 366 e2e typesafety angular (#48)
    *  Angular httpClient (ZacHttpClient) wrapper toegevoegd die volledig getyped is gebaseerd op de ZAC open-api spec
    
    *  ZacHttpClient geïmplementeerd in EntityService
    
    *  Documentatie toegevoegd voor "end-to-end" typesafety

--- src/main/app/src/app/identity/identity.service.ts
+++ src/main/app/src/app/identity/identity.service.ts
@@ -4,14 +4,15 @@
  */
 
 import { Injectable } from "@angular/core";
+
 import { Observable, of } from "rxjs";
 import { catchError, tap } from "rxjs/operators";
-import { Group } from "./model/group";
-import { HttpClient } from "@angular/common/http";
 import { FoutAfhandelingService } from "../fout-afhandeling/fout-afhandeling.service";
-import { User } from "./model/user";
 import { SessionStorageUtil } from "../shared/storage/session-storage.util";
+import { Group } from "./model/group";
 import { LoggedInUser } from "./model/logged-in-user";
+import { User } from "./model/user";
+import { ZacHttpClient } from "../shared/http/zac-http-client";
 
 @Injectable({
   providedIn: "root",
@@ -19,32 +20,32 @@ import { LoggedInUser } from "./model/logged-in-user";
 export class IdentityService {
   public static LOGGED_IN_USER_KEY = "loggedInUser";
 
-  private basepath = "/rest/identity";
-
   constructor(
-    private http: HttpClient,
+    private zacHttp: ZacHttpClient,
     private foutAfhandelingService: FoutAfhandelingService,
   ) {}
 
   listGroups(): Observable<Group[]> {
-    return this.http
-      .get<Group[]>(`${this.basepath}/groups`)
+    return this.zacHttp
+      .GET("/rest/identity/groups")
       .pipe(
         catchError((err) => this.foutAfhandelingService.foutAfhandelen(err)),
       );
   }
 
   listUsersInGroup(groupId: string): Observable<User[]> {
-    return this.http
-      .get<User[]>(`${this.basepath}/groups/${groupId}/users`)
+    return this.zacHttp
+      .GET("/rest/identity/groups/{groupId}/users", {
+        params: { path: { groupId } },
+      })
       .pipe(
         catchError((err) => this.foutAfhandelingService.foutAfhandelen(err)),
       );
   }
 
   listUsers(): Observable<User[]> {
-    return this.http
-      .get<User[]>(`${this.basepath}/users`)
+    return this.zacHttp
+      .GET(`/rest/identity/users`)
       .pipe(
         catchError((err) => this.foutAfhandelingService.foutAfhandelen(err)),
       );
@@ -57,8 +58,9 @@ export class IdentityService {
     if (loggedInUser) {
       return of(loggedInUser);
     }
-    return this.http.get<LoggedInUser>(`${this.basepath}/loggedInUser`).pipe(
+    return this.zacHttp.GET("/rest/identity/loggedInUser").pipe(
       tap((user) => {
+        console.log(user);
         SessionStorageUtil.setItem(IdentityService.LOGGED_IN_USER_KEY, user);
       }),
       catchError((err) => this.foutAfhandelingService.foutAfhandelen(err)),
---
+++ src/main/app/src/app/shared/http/zac-http-client.ts
@@ -0,0 +1,99 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Lifely
+ * SPDX-License-Identifier: EUPL-1.2+
+ */
+
+import { FetchOptions, FetchResponse } from "openapi-fetch";
+
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import type { FilterKeys } from "openapi-typescript-helpers";
+import { paths } from "src/generated/types/zac-openapi-types";
+
+type Paths = paths;
+
+type Response<
+  P extends keyof Paths,
+  type extends "get" | "post" | "put" | "delete" | "patch",
+> = FetchResponse<
+  type extends infer T
+    ? T extends type
+      ? T extends keyof Paths[P]
+        ? Paths[P][T]
+        : unknown
+      : never
+    : never
+>["data"];
+
+@Injectable({
+  providedIn: "root",
+})
+export class ZacHttpClient {
+  constructor(private http: HttpClient) {}
+
+  public GET<P extends keyof Paths>(
+    url: P,
+    init?: Omit<Parameters<HttpClient["get"]>[1], "params"> & {
+      params: FetchOptions<FilterKeys<Paths[P], "get">>["params"];
+    },
+  ) {
+    return this.http.get<Response<P, "get">>(url, {
+      params: (init?.params as any)?.path ?? undefined,
+    });
+  }
+
+  public POST<P extends keyof Paths>(
+    url: P,
+    body: FetchOptions<FilterKeys<Paths[P], "post">>["body"],
+    init?: Parameters<HttpClient["post"]>[2] & {
+      params: FetchOptions<FilterKeys<Paths[P], "post">>["params"];
+    },
+  ) {
+    return this.http.post<Response<P, "post">>(url, body, {
+      ...this.getOptions(init),
+    });
+  }
+
+  public PUT<P extends keyof Paths>(
+    url: P,
+    body: FetchOptions<FilterKeys<Paths[P], "put">>["body"],
+    init?: Parameters<HttpClient["put"]>[2] & {
+      params: FetchOptions<FilterKeys<Paths[P], "put">>["params"];
+    },
+  ) {
+    return this.http.put<Response<P, "put">>(url, body, {
+      ...this.getOptions(init),
+    });
+  }
+
+  public DELETE<P extends keyof Paths>(
+    url: P,
+    init?: Parameters<HttpClient["delete"]>[1] & {
+      params: FetchOptions<FilterKeys<Paths[P], "delete">>["params"];
+    },
+  ) {
+    return this.http.delete<Response<P, "delete">>(url, {
+      ...this.getOptions(init),
+    });
+  }
+
+  public PATCH<P extends keyof Paths>(
+    url: P,
+    body: FetchOptions<FilterKeys<Paths[P], "patch">>["body"],
+    init?: Parameters<HttpClient["patch"]>[2] & {
+      params: FetchOptions<FilterKeys<Paths[P], "patch">>["params"];
+    },
+  ) {
+    return this.http.patch<Response<P, "patch">>(url, body, {
+      ...this.getOptions(init),
+    });
+  }
+
+  private getOptions<T extends { params: any }>(init?: T) {
+    const { params, ...rest } = init ?? {};
+    return {
+      params: (params as any)?.path ?? undefined,
+      ...rest,
+    };
+  }
+}
