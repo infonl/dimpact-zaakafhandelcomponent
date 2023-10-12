463b23b692134eb14fadd7628cec21ab110084b6
Rick Woltheus <rwoltheus@gmail.com>
2023-10-12 20:00:06 +0200

    move e2e to src

--- start-docker-compose-e2e.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# ... (previous comments and headers)
-
-export APP_ENV=devlocal
-op run --env-file="./.env.tpl" -- sh -c "docker-compose -f docker-compose-e2e.yaml rm -sfv && docker-compose -f docker-compose-e2e.yaml --profile zac up -d"
