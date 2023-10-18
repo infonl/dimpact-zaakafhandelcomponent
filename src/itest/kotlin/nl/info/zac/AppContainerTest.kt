e78e26a0ee163181eafb7decf839645f274e95d3
Edgar Vonk <edgar@info.nl>
2023-10-18 17:42:13 +0200

    chore: clean up

--- src/itest/kotlin/nl/info/zac/AppContainerTest.kt
+++ src/itest/kotlin/nl/info/zac/AppContainerTest.kt
@@ -6,12 +6,9 @@
 package nl.info.zac
 
 import com.github.dockerjava.zerodep.shaded.org.apache.hc.core5.http.HttpStatus
-import io.github.oshai.kotlinlogging.KotlinLogging
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.provided.ProjectConfig
-
-private val logger = KotlinLogging.logger {}
 
 class AppContainerTest : BehaviorSpec({
     given("ZAC Docker container and all related Docker containers are running") {
@@ -20,9 +17,8 @@ class AppContainerTest : BehaviorSpec({
                 khttp.get(
                     url = "${ProjectConfig.zacContainer.managementUrl}/health/ready"
                 ).apply {
-                    logger.info { "response: $this" }
                     statusCode shouldBe HttpStatus.SC_OK
-                    this.jsonObject.getString("status") shouldBe "UP"
+                    jsonObject.getString("status") shouldBe "UP"
                 }
             }
         }
