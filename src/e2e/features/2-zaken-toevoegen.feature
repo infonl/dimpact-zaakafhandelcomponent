37de19a0f030b6a8bd9a586459fe21f0e977456c
Rick <rwoltheus@gmail.com>
2023-10-24 09:34:52 +0200

    feat: add scheduled pipeline for e2e tests (#121)
    This pr will add a Github action for running end to end tests to test in
    a scheduled cron-job (9pm every work day)
    Also I refactored some hardcoded code
    
    Solves: PZ-590
    
    ---------
    
    Co-authored-by: John Bol <john@info.nl>

--- src/e2e/features/2-zaken-toevoegen.feature
+++ src/e2e/features/2-zaken-toevoegen.feature
@@ -1,7 +1,7 @@
 Feature: Login
 
   Scenario: Bob wants to create a new zaak
-    Given "Bob" navigates to "http://zaakafhandelcomponent-zac-dev.westeurope.cloudapp.azure.com"
+    Given "Bob" navigates to "zac" with path "/"
     When "Bob" logs in with username "testuser1" and password "testuser1"
     When "Bob" wants to create a new zaak
     Then "Bob" sees the text: "Case |"
--- src/e2e/step-definitions/common.ts
+++ src/e2e/step-definitions/common.ts
@@ -2,11 +2,13 @@
  * SPDX-FileCopyrightText: 2023 Lifely
  * SPDX-License-Identifier: EUPL-1.2+
  */
-import { Given, When, Then } from "@cucumber/cucumber";
+import { Given, Then } from "@cucumber/cucumber";
 import { CustomWorld } from "../support/worlds/world";
+import { worldPossibleZacUrls } from "../utils/schemes";
 
-Given("{string} navigates to {string}", { timeout: 60 * 1000 }, async function (this: CustomWorld, user, url) {
-    await this.openUrl(url);
+Given("{string} navigates to {string} with path {string}", { timeout: 60 * 1000 }, async function (this: CustomWorld, user, urlKey, path) {
+    const res = worldPossibleZacUrls.parse(urlKey)
+    await this.openUrl(this.worldParameters.urls[res] + path);
 });
 
 Then("{string} sees the text: {string}", async function (this: CustomWorld, user, text) {
