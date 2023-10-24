46b4eb5b74728faa4c77d7098788aeaca917c178
Rick Woltheus <rwoltheus@gmail.com>
2023-10-24 16:46:03 +0200

    remove comment

--- src/e2e/support/worlds/world.ts
+++ src/e2e/support/worlds/world.ts
@@ -25,7 +25,6 @@ export class CustomWorld extends World {
     }
 
     async init() {
-    // create auth file if not exists
         if (!fs.existsSync(authFile)) {
             fs.writeFileSync(authFile, '{}');
         }
