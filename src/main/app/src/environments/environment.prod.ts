7e2477229fb9ea509dd3c8e55c9f7fcb10c77512
Rick Woltheus <rwoltheus@gmail.com>
2023-10-25 10:01:06 +0200

    update prod env

--- src/main/app/src/environments/environment.prod.ts
+++ src/main/app/src/environments/environment.prod.ts
@@ -5,8 +5,8 @@
 
 export const environment = {
   production: true,
-  LOCATIE_SERVER_API_URL: "https://api.pdok.nl/bzk/locatieserver/search/v3_1",
-  ACHTERGRONDKAART_API_URL:
+  LOCATION_SERVER_API_URL: "https://api.pdok.nl/bzk/locatieserver/search/v3_1",
+  BACKGROUND_MAP_API_URL:
     "https://service.pdok.nl/brt/achtergrondkaart/wmts/v2_0",
   GOOGLE_API_URL: "https://maps.googleapis.com/maps/api/js",
 };
