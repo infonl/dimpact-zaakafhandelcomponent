5551687c217cc464e690cca4d05605023454c557
edgarvonk <edgar@info.nl>
2023-10-30 08:59:50 +0100

    fix: revert accidental change in zac docker image

--- src/main/app/src/app/shared/pipes/datum.pipe.spec.ts
+++ src/main/app/src/app/shared/pipes/datum.pipe.spec.ts
@@ -8,7 +8,7 @@ import { DatumPipe } from "./datum.pipe";
 describe("DatumPipe", () => {
   it("should always be UTC", () => {
   // TODO
-    expect(new Date().getTimezoneOffset()).toBe(-60);
+    expect(new Date().getTimezoneOffset()).toBe(-120);
   });
 
   it("2021-06-23T00:00:00Z -> 23-06-2021 02:00", () => {
