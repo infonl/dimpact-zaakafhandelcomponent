0c5c8708ad62994a00cc5ee1f1f47e5c696343a2
Joran <joran@info.nl>
2023-12-11 10:13:14 +0100

    feat(test): test metrics (#298)
    This will add a test to see if the metrics endpoint is available
    
    Also checks if uptime is > 0
    
    ---------
    
    Co-authored-by: Edgar Vonk <edgar@info.nl>

--- src/itest/kotlin/nl/lifely/zac/itest/AppContainerTest.kt
+++ src/itest/kotlin/nl/lifely/zac/itest/AppContainerTest.kt
@@ -7,6 +7,7 @@ package nl.lifely.zac.itest
 
 import com.github.dockerjava.zerodep.shaded.org.apache.hc.core5.http.HttpStatus
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import nl.lifely.zac.itest.config.ItestConfiguration.ZAC_MANAGEMENT_URI
 
@@ -19,6 +20,18 @@ class AppContainerTest : BehaviorSpec({
                 ).apply {
                     statusCode shouldBe HttpStatus.SC_OK
                     jsonObject.getString("status") shouldBe "UP"
+                }
+            }
+        }
+    }
+    given("ZAC Docker container and all related Docker containers are running") {
+        When("the metrics endpoint is called") {
+            then("the response should be ok and the the uptime var should be present") {
+                khttp.get(
+                    url = "${ZAC_MANAGEMENT_URI}/metrics"
+                ).apply {
+                    statusCode shouldBe HttpStatus.SC_OK
+                    text.contains("base_jvm_uptime_seconds").shouldNotBeNull()
                 }
             }
         }
