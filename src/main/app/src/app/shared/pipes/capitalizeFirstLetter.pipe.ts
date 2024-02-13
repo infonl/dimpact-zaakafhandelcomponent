a6ad1bc1d00ecb56d17d2bcb8ae5007180269a52
Rick <rwoltheus@gmail.com>
2024-02-13 14:44:16 +0100

    fix: improve angular code informationobject add (#554)
    - fixed a bug when adding a document to a zaak twice, the form error handling did not work anymore

--- src/main/app/src/app/shared/pipes/capitalizeFirstLetter.pipe.ts
+++ src/main/app/src/app/shared/pipes/capitalizeFirstLetter.pipe.ts
@@ -3,7 +3,10 @@ import { Pipe, PipeTransform } from "@angular/core";
   name: "capitalizeFirstLetter",
 })
 export class CapitalizeFirstLetterPipe implements PipeTransform {
-  transform(value: string): string {
-    return value.charAt(0).toUpperCase() + value.slice(1);
+  transform(value: string | null): string | null {
+    if (typeof value === "string") {
+      return value.charAt(0).toUpperCase() + value.slice(1);
+    }
+    return null;
   }
 }
