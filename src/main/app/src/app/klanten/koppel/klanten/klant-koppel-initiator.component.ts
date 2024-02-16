53c1eeaea655ac5cba2a92342a5aa521f852f43e
Rick <rwoltheus@gmail.com>
2024-02-16 11:54:51 +0100

    feat: add better validation for add initiator and betrokkene fields (#570)
    Add better form handling of add betrokkene and initiator forms 
    
    Solves PZ-1360

---
+++ src/main/app/src/app/klanten/koppel/klanten/klant-koppel-initiator.component.ts
@@ -0,0 +1,50 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Lifely
+ * SPDX-License-Identifier: EUPL-1.2+
+ */
+
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { TranslateModule } from "@ngx-translate/core";
+import { MaterialFormBuilderModule } from "src/app/shared/material-form-builder/material-form-builder.module";
+import { SharedModule } from "src/app/shared/shared.module";
+import { KlantenModule } from "../../klanten.module";
+import { Klant } from "../../model/klanten/klant";
+import { KlantGegevens } from "../../model/klanten/klant-gegevens";
+
+@Component({
+  selector: "zac-klant-koppel-initiator-persoon",
+  standalone: true,
+  imports: [
+    SharedModule,
+    TranslateModule,
+    MaterialFormBuilderModule,
+    KlantenModule,
+  ],
+  template: `
+    <ng-template mat-tab-label>
+      @if (type === 'bedrijf') {
+      <mat-icon>business</mat-icon>
+      } @if (type === 'persoon') {
+      <mat-icon>emoji_people</mat-icon>
+
+      } @if (type === 'bedrijf') {
+      <span>{{ "betrokkene.bedrijf" | translate }}</span>
+      } @if (type === 'persoon') {
+      <span> {{ "betrokkene.persoon" | translate }}</span>
+      }
+    </ng-template>
+    @if (type === "persoon") {
+    <zac-persoon-zoek (persoon)="klantGeselecteerd($event)"></zac-persoon-zoek>
+    } @if (type === "bedrijf") {
+    <zac-bedrijf-zoek (bedrijf)="klantGeselecteerd($event)"></zac-bedrijf-zoek>
+    }
+  `,
+})
+export class KlantKoppelInitiator {
+  @Input() type: "persoon" | "bedrijf" = "persoon";
+  @Output() klantGegevens = new EventEmitter<KlantGegevens>();
+
+  klantGeselecteerd(klant: Klant): void {
+    this.klantGegevens.emit(new KlantGegevens(klant));
+  }
+}
--- src/main/app/src/app/klanten/koppel/klanten/klant-koppel.component.ts
+++ src/main/app/src/app/klanten/koppel/klanten/klant-koppel.component.ts
@@ -1,75 +1,97 @@
 /*
- * SPDX-FileCopyrightText: 2022 Atos
+ * SPDX-FileCopyrightText: 2024 Lifely
  * SPDX-License-Identifier: EUPL-1.2+
  */
 
-import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
-import { Klant } from "../../model/klanten/klant";
-import { SelectFormFieldBuilder } from "../../../shared/material-form-builder/form-components/select/select-form-field-builder";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { SelectFormField } from "../../../shared/material-form-builder/form-components/select/select-form-field";
-import { KlantenService } from "../../klanten.service";
-import { KlantGegevens } from "../../model/klanten/klant-gegevens";
-import { InputFormField } from "../../../shared/material-form-builder/form-components/input/input-form-field";
-import { InputFormFieldBuilder } from "../../../shared/material-form-builder/form-components/input/input-form-field-builder";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { MatDrawer } from "@angular/material/sidenav";
+import { TranslateModule } from "@ngx-translate/core";
+import { SharedModule } from "src/app/shared/shared.module";
+import { KlantGegevens } from "../../model/klanten/klant-gegevens";
+import { KlantKoppelBetrokkeneComponent } from "./klant-koppel-betrokkene.component";
+import { KlantKoppelInitiator } from "./klant-koppel-initiator.component";
 
 @Component({
   selector: "zac-klant-koppel",
-  templateUrl: "./klant-koppel.component.html",
+  imports: [
+    KlantKoppelBetrokkeneComponent,
+    KlantKoppelInitiator,
+    SharedModule,
+    TranslateModule,
+  ],
+  template: `
+    <div class="sidenav-title">
+      <h3>
+        <mat-icon>person_add_alt_1</mat-icon>
+        {{
+          (initiator
+            ? "actie.initiator.toevoegen"
+            : "actie.betrokkene.toevoegen"
+          ) | translate
+        }}
+      </h3>
+      <button mat-icon-button (click)="sideNav.close()">
+        <mat-icon>close</mat-icon>
+      </button>
+    </div>
+
+    <!--Initiator-->
+    <mat-tab-group mat-stretch-tabs="false" *ngIf="initiator">
+      <mat-tab>
+        <ng-template mat-tab-label>
+          <mat-icon>emoji_people</mat-icon>
+          {{ "betrokkene.persoon" | translate }}
+        </ng-template>
+        <zac-klant-koppel-initiator-persoon
+          type="persoon"
+          (klantGegevens)="klantGegevens.emit($event)"
+        />
+      </mat-tab>
+      <mat-tab>
+        <ng-template mat-tab-label>
+          <mat-icon>business</mat-icon>
+          {{ "betrokkene.bedrijf" | translate }}
+        </ng-template>
+        <zac-klant-koppel-initiator-persoon
+          type="bedrijf"
+          (klantGegevens)="klantGegevens.emit($event)"
+        />
+      </mat-tab>
+    </mat-tab-group>
+
+    <!--Betrokkene-->
+    <mat-tab-group mat-stretch-tabs="false" *ngIf="!initiator">
+      <mat-tab>
+        <ng-template mat-tab-label>
+          <mat-icon>emoji_people</mat-icon>
+          {{ "betrokkene.persoon" | translate }}
+        </ng-template>
+        <zac-klant-koppel-betrokkene-persoon
+          type="persoon"
+          [zaaktypeUUID]="zaaktypeUUID"
+          (klantGegevens)="klantGegevens.emit($event)"
+        />
+      </mat-tab>
+      <mat-tab>
+        <ng-template mat-tab-label>
+          <mat-icon>business</mat-icon>
+          {{ "betrokkene.bedrijf" | translate }}
+        </ng-template>
+        <zac-klant-koppel-betrokkene-persoon
+          type="bedrijf"
+          [zaaktypeUUID]="zaaktypeUUID"
+          (klantGegevens)="klantGegevens.emit($event)"
+        />
+      </mat-tab>
+    </mat-tab-group>
+  `,
+  standalone: true,
+
   styleUrls: ["./klant-koppel.component.less"],
 })
-export class KlantKoppelComponent implements OnInit {
+export class KlantKoppelComponent {
   @Input() initiator = false;
   @Input() zaaktypeUUID: string;
   @Input() sideNav: MatDrawer;
   @Output() klantGegevens = new EventEmitter<KlantGegevens>();
-  betrokkeneRoltype: SelectFormField;
-  betrokkeneToelichting: InputFormField;
-  formGroup: FormGroup;
-
-  constructor(
-    private klantenService: KlantenService,
-    private formBuilder: FormBuilder,
-  ) {}
-
-  ngOnInit(): void {
-    if (!this.initiator) {
-      this.betrokkeneRoltype = new SelectFormFieldBuilder()
-        .id("betrokkeneType")
-        .label("betrokkeneRoltype")
-        .optionLabel("naam")
-        .options(this.klantenService.listBetrokkeneRoltypen(this.zaaktypeUUID))
-        .validators(Validators.required)
-        .build();
-      this.betrokkeneToelichting = new InputFormFieldBuilder()
-        .id("betrokkenToelichting")
-        .label("toelichting")
-        .validators(Validators.required)
-        .maxlength(75)
-        .build();
-      this.formGroup = this.formBuilder.group({
-        rol: this.betrokkeneRoltype.formControl,
-        toelichting: this.betrokkeneToelichting.formControl,
-      });
-    }
-  }
-
-  klantGeselecteerd(klant: Klant): void {
-    if (this.initiator) {
-      this.klantGegevens.emit(new KlantGegevens(klant));
-    } else {
-      if (this.formGroup.invalid) {
-        this.betrokkeneRoltype.formControl.markAsTouched();
-        this.betrokkeneToelichting.formControl.markAsTouched();
-      } else {
-        const klantGegevens: KlantGegevens = new KlantGegevens(klant);
-        klantGegevens.betrokkeneRoltype =
-          this.betrokkeneRoltype.formControl.value;
-        klantGegevens.betrokkeneToelichting =
-          this.betrokkeneToelichting.formControl.value;
-        this.klantGegevens.emit(klantGegevens);
-      }
-    }
-  }
 }
