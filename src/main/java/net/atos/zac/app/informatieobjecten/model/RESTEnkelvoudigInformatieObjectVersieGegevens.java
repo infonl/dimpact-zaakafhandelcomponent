bc8e11552052cb515ebef3b34318e850a118cd21
Hristo Iliev <hsiliev@gmail.com>
2024-03-13 15:41:55 +0200

    feat: single endpoint to create EnkelvoudigInformatieobject and upload file  (#720)
    Keeps old endpoints for backward compatibility
    Added integration test
    
    Solves PZ-1663, PZ-1665
    
    ---------
    
    Co-authored-by: Hristo Iliev <hristo.iliev@info.nl>

--- src/main/java/net/atos/zac/app/informatieobjecten/model/RESTEnkelvoudigInformatieObjectVersieGegevens.java
+++ src/main/java/net/atos/zac/app/informatieobjecten/model/RESTEnkelvoudigInformatieObjectVersieGegevens.java
@@ -8,33 +8,53 @@ package net.atos.zac.app.informatieobjecten.model;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import jakarta.ws.rs.FormParam;
+
 import net.atos.client.zgw.drc.model.generated.EnkelvoudigInformatieObject;
 import net.atos.zac.app.configuratie.model.RESTTaal;
 
 
 public class RESTEnkelvoudigInformatieObjectVersieGegevens {
 
+    @FormParam("uuid")
     public UUID uuid;
 
+    @FormParam("zaakUuid")
     public UUID zaakUuid;
 
+    @FormParam("titel")
     public String titel;
 
+    @FormParam("vertrouwelijkheidaanduiding")
     public String vertrouwelijkheidaanduiding;
 
+    @FormParam("auteur")
     public String auteur;
 
+    @FormParam("status")
     public EnkelvoudigInformatieObject.StatusEnum status;
 
+    @FormParam("taal")
     public RESTTaal taal;
 
+    @FormParam("bestandsnaam")
     public String bestandsnaam;
 
+    @FormParam("formaat")
+    public String formaat;
+
+    @FormParam("file")
+    public byte[] file;
+
+    @FormParam("beschrijving")
     public String beschrijving;
 
+    @FormParam("verzenddatum")
     public LocalDate verzenddatum;
 
+    @FormParam("ontvangstdatum")
     public LocalDate ontvangstdatum;
 
+    @FormParam("toelichting")
     public String toelichting;
 }
---
+++ src/main/java/net/atos/zac/app/util/ZonedDateTimeReader.java
@@ -0,0 +1,39 @@
+package net.atos.zac.app.util;
+
+import static java.time.format.DateTimeFormatter.ISO_ZONED_DATE_TIME;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.time.ZonedDateTime;
+
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.ext.MessageBodyReader;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+@Produces(MediaType.TEXT_PLAIN)
+public class ZonedDateTimeReader implements MessageBodyReader<ZonedDateTime> {
+
+    @Override
+    public boolean isReadable(Class<?> aClass, Type type, Annotation[] annotations, MediaType mediaType) {
+        return type == ZonedDateTime.class;
+    }
+
+    @Override
+    public ZonedDateTime readFrom(
+            Class<ZonedDateTime> aClass,
+            Type type,
+            Annotation[] annotations,
+            MediaType mediaType,
+            MultivaluedMap<String, String> multivaluedMap,
+            InputStream inputStream
+    ) throws IOException, WebApplicationException {
+        byte[] zonedDateTimeBytes = inputStream.readAllBytes();
+        return ZonedDateTime.parse(new String(zonedDateTimeBytes), ISO_ZONED_DATE_TIME);
+    }
+}
