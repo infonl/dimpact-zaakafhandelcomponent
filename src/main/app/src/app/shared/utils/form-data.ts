f7b70c8b1e2b73a4e6e5adc834540dbcc745ab57
Rick <rwoltheus@gmail.com>
2024-03-26 12:51:21 +0100

    fix: taak document upload fix (#773)
    Een typo was natuurlijk de boosdoener 
    
    Solves PZ-1591
    
    ---------
    
    Co-authored-by: felixcicatt <felix.cornelissen@icatt.nl>

---
+++ src/main/app/src/app/shared/utils/form-data.ts
@@ -0,0 +1,58 @@
+type FormCompatible = string | Blob | { toString: () => string };
+
+type FormDataMappingFunction<T> = (
+  keyValuePair: readonly [string, T],
+) => readonly [string, string | Blob] | readonly [string, Blob, string];
+
+type FormDataMapper<T> = Partial<{
+  [K in keyof T]: T[K] extends FormCompatible[]
+    ? FormDataMappingFunction<T[K][number]> | true
+    : T[K] extends FormCompatible
+      ? FormDataMappingFunction<T[K]> | true
+      : T[K] extends Array<infer A>
+        ? FormDataMappingFunction<A>
+        : FormDataMappingFunction<T[K]>;
+}>;
+
+function parseFormValue(v: unknown) {
+  if (v === undefined || v === null) return false;
+  if (typeof v === "string" || v instanceof Blob) return v;
+  return v.toString();
+}
+
+export function createFormData<T extends {}>(
+  obj: T,
+  mapper: FormDataMapper<T>,
+): FormData {
+  const formData = new FormData();
+  Object.entries(mapper).forEach(
+    ([key, mappingFunction]: [
+      string,
+      FormDataMappingFunction<unknown> | true,
+    ]) => {
+      const value = obj[key];
+      if (Array.isArray(value)) {
+        value.forEach(add);
+      } else {
+        add(value);
+      }
+      function add(value: unknown) {
+        if (typeof mappingFunction === "function") {
+          const [newKey, stringOrBlob, maybeFileName] = mappingFunction([
+            key,
+            value,
+          ]);
+          if (stringOrBlob instanceof Blob) {
+            formData.append(newKey, stringOrBlob, maybeFileName);
+          } else {
+            formData.append(newKey, stringOrBlob);
+          }
+        } else {
+          const parsed = parseFormValue(value);
+          parsed && formData.append(key, parsed);
+        }
+      }
+    },
+  );
+  return formData;
+}
