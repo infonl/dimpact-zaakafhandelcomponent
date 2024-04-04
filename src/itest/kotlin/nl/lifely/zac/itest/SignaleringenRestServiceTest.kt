7b6aaa39ba2f56a3703e4268db2b6456923327dc
Hristo Iliev <hristo.iliev@info.nl>
2024-04-05 00:19:48 +0300

    feat: assign zaak to user

--- src/itest/kotlin/nl/lifely/zac/itest/SignaleringenRestServiceTest.kt
+++ src/itest/kotlin/nl/lifely/zac/itest/SignaleringenRestServiceTest.kt
@@ -6,15 +6,22 @@
 package nl.lifely.zac.itest
 
 import io.github.oshai.kotlinlogging.KotlinLogging
+import io.kotest.assertions.json.shouldBeJsonArray
+import io.kotest.assertions.json.shouldContainJsonKey
+import io.kotest.assertions.nondeterministic.eventually
+import io.kotest.assertions.nondeterministic.eventuallyConfig
 import io.kotest.core.spec.Order
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.date.shouldBeBetween
 import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.delay
 import nl.lifely.zac.itest.client.ItestHttpClient
 import nl.lifely.zac.itest.config.ItestConfiguration
 import nl.lifely.zac.itest.config.ItestConfiguration.OPEN_ZAAK_BASE_URI
 import nl.lifely.zac.itest.config.ItestConfiguration.START_DATE
+import nl.lifely.zac.itest.config.ItestConfiguration.TEST_GROUP_A_ID
 import nl.lifely.zac.itest.config.ItestConfiguration.TEST_SPEC_ORDER_AFTER_TASK_COMPLETED
+import nl.lifely.zac.itest.config.ItestConfiguration.TEST_USER_1_ID
 import nl.lifely.zac.itest.config.ItestConfiguration.ZAC_API_URI
 import okhttp3.Headers
 import org.json.JSONArray
@@ -23,6 +30,7 @@ import org.mockserver.model.HttpStatusCode
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.ZonedDateTime
+import kotlin.time.Duration.Companion.seconds
 
 /**
  * This test assumes previous tests completed successfully.
@@ -61,20 +69,37 @@ class SignaleringenRestServiceTest : BehaviorSpec() {
             }
         }
 
+        Given("A created zaak") {
+            When("it is assigned to a user") {
+                val response = itestHttpClient.performPatchRequest(
+                    url = "$ZAC_API_URI/zaken/toekennen",
+                    headers = Headers.headersOf(
+                        "Content-Type",
+                    "application/json"),
+                    requestBodyAsString = """{
+                        "zaakUUID":"$zaak1UUID",
+                        "behandelaarGebruikersnaam":"$TEST_USER_1_ID",
+                        "groepId":"$TEST_GROUP_A_ID",
+                        "reden":null
+                    }""".trimIndent()
+                )
+
+                Then("the responses should be 'ok'") {
+                    response.code shouldBe HttpStatusCode.OK_200.code()
+                }
+            }
+        }
+
         Given("A zaak with informatie objecten") {
             val zaakInformatieObjectenResponse = itestHttpClient.performGetRequest(
-                url = "$ZAC_API_URI/informatieobjecten/informatieobjecten/zaak/$zaak1UUID",
-                headers = Headers.headersOf(
-                    "Content-Type",
-                    "application/json"
-                )
+                url = "$ZAC_API_URI/informatieobjecten/informatieobjecten/zaak/$zaak1UUID"
             )
             var responseBody = zaakInformatieObjectenResponse.body!!.string()
             logger.info { "Response: $responseBody" }
             zaakInformatieObjectenResponse.code shouldBe HttpStatusCode.OK_200.code()
 
             val zaakInformatieObjectenUrl = JSONArray(responseBody).getJSONObject(0).getString("url")
-            logger.info { "Zaak informatie objecten UUID: $zaakInformatieObjectenUrl" }
+            logger.info { "Zaak informatie objecten URL: $zaakInformatieObjectenUrl" }
 
             When("a notification is sent to ZAC that a zaak is updated") {
                 val response = itestHttpClient.performJSONPostRequest(
@@ -104,7 +129,27 @@ class SignaleringenRestServiceTest : BehaviorSpec() {
             }
         }
 
-        Given("A task has been assigned") {
+        Given("A zaak has been assigned") {
+
+            When("the list of zaken signaleringen for ZAAK_DOCUMENT_TOEGEVOEGD is requested") {
+                lateinit var responseBody: String
+
+                eventually(eventuallyConfig {
+                    duration = 20.seconds
+                    interval = 1.seconds
+                }) {
+                    val response = itestHttpClient.performGetRequest(
+                        "$ZAC_API_URI/signaleringen/zaken/ZAAK_DOCUMENT_TOEGEVOEGD"
+                    )
+                    response.isSuccessful shouldBe true
+                    responseBody = response.body!!.string()
+                    responseBody shouldContainJsonKey "identificatie"
+                }
+
+                Then("""it returns the correct signaleringen""") {
+                    logger.info { "Response: $responseBody" }
+                }
+            }
 
             When("the latest signaleringen date is requested") {
                 val response = itestHttpClient.performGetRequest("$ZAC_API_URI/signaleringen/latest")
@@ -119,45 +164,6 @@ class SignaleringenRestServiceTest : BehaviorSpec() {
                 }
             }
 
-            When("the list of zaken signaleringen for ZAAK_OP_NAAM is requested") {
-                val response = itestHttpClient.performGetRequest("$ZAC_API_URI/signaleringen/zaken/ZAAK_OP_NAAM")
-
-                Then("""it returns the correct signaleringen""") {
-                    val responseBody = response.body!!.string()
-                    logger.info { "Response: $responseBody" }
-                    response.isSuccessful shouldBe true
-                }
-            }
-
-            When("the list of zaken signaleringen for TAAK_OP_NAAM is requested") {
-                val response = itestHttpClient.performGetRequest("$ZAC_API_URI/signaleringen/zaken/TAAK_OP_NAAM")
-
-                Then("""it returns the correct signaleringen""") {
-                    val responseBody = response.body!!.string()
-                    logger.info { "Response: $responseBody" }
-                    response.isSuccessful shouldBe true
-                }
-            }
-
-            When("the list of taken signaleringen for TAAK_OP_NAAM is requested") {
-                val response = itestHttpClient.performGetRequest("$ZAC_API_URI/signaleringen/taken/TAAK_OP_NAAM")
-
-                Then("""it returns the correct signaleringen""") {
-                    val responseBody = response.body!!.string()
-                    logger.info { "Response: $responseBody" }
-                    response.isSuccessful shouldBe true
-                }
-            }
-
-            When("the list of taken signaleringen for TAAK_OP_NAAM is requested") {
-                val response = itestHttpClient.performGetRequest("$ZAC_API_URI/signaleringen/typen/dashboard")
-
-                Then("""it returns the correct dashboard types""") {
-                    val responseBody = response.body!!.string()
-                    logger.info { "Response: $responseBody" }
-                    response.isSuccessful shouldBe true
-                }
-            }
         }
     }
 }
