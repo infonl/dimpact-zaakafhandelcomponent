b8ac6c05b02f6d7816a76ab32b9de8bbd5cfe60a
Edgar Vonk <edgar@info.nl>
2024-04-04 14:42:32 +0200

    feat: set zaak description field from productaanvraag  (#848)
    Set zaak description field from Open Formulieren productaanvraag using
    form 'bron' data. Do not set the 'toelichting' field.
    
    Solves PZ-1904

--- src/itest/kotlin/nl/lifely/zac/itest/NotificationsTest.kt
+++ src/itest/kotlin/nl/lifely/zac/itest/NotificationsTest.kt
@@ -17,6 +17,8 @@ import nl.lifely.zac.itest.config.ItestConfiguration
 import nl.lifely.zac.itest.config.ItestConfiguration.OBJECTS_BASE_URI
 import nl.lifely.zac.itest.config.ItestConfiguration.OBJECTTYPE_UUID_PRODUCTAANVRAAG_DIMPACT
 import nl.lifely.zac.itest.config.ItestConfiguration.OBJECT_PRODUCTAANVRAAG_UUID
+import nl.lifely.zac.itest.config.ItestConfiguration.OPEN_FORMULIEREN_FORMULIER_BRON_KENMERK
+import nl.lifely.zac.itest.config.ItestConfiguration.OPEN_FORMULIEREN_FORMULIER_BRON_NAAM
 import nl.lifely.zac.itest.config.ItestConfiguration.OPEN_NOTIFICATIONS_API_SECRET_KEY
 import nl.lifely.zac.itest.config.ItestConfiguration.OPEN_ZAAK_BASE_URI
 import nl.lifely.zac.itest.config.ItestConfiguration.TEST_SPEC_ORDER_INITIAL
@@ -101,18 +103,21 @@ class NotificationsTest : BehaviorSpec({
                 ).use { getZaakResponse ->
                     val responseBody = getZaakResponse.body!!.string()
                     logger.info { "Response: $responseBody" }
-                    val zaak = JSONObject(responseBody)
-                    zaak.getString("identificatie") shouldBe ZAAK_1_IDENTIFICATION
-                    zaak.getJSONObject("zaaktype")
-                        .getString("uuid") shouldBe ZAAKTYPE_MELDING_KLEIN_EVENEMENT_UUID.toString()
-                    zaak.getJSONObject("status").getString("naam") shouldBe "Intake"
-                    zaak.getJSONObject("groep").getString("id") shouldBe "test-group-a"
-                    // 'proces gestuurd' is true when a BPMN rather than a CMMN proces has been started
-                    // since we have defined zaakafhandelparameters for this zaaktype a CMMN proces should be started
-                    zaak.getBoolean("isProcesGestuurd") shouldBe false
-                    zaak.getJSONObject("communicatiekanaal")
-                        .getString("naam") shouldBe "E-formulier"
-                    zaak1UUID = zaak.getString("uuid").let(UUID::fromString)
+                    with(JSONObject(responseBody)) {
+                        getString("identificatie") shouldBe ZAAK_1_IDENTIFICATION
+                        getJSONObject("zaaktype")
+                            .getString("uuid") shouldBe ZAAKTYPE_MELDING_KLEIN_EVENEMENT_UUID.toString()
+                        getJSONObject("status").getString("naam") shouldBe "Intake"
+                        getJSONObject("groep").getString("id") shouldBe "test-group-a"
+                        // 'proces gestuurd' is true when a BPMN rather than a CMMN proces has been started
+                        // since we have defined zaakafhandelparameters for this zaaktype a CMMN proces should be started
+                        getBoolean("isProcesGestuurd") shouldBe false
+                        getJSONObject("communicatiekanaal")
+                            .getString("naam") shouldBe "E-formulier"
+                        getString("omschrijving") shouldBe "Aangemaakt vanuit $OPEN_FORMULIEREN_FORMULIER_BRON_NAAM " +
+                            "met kenmerk '$OPEN_FORMULIEREN_FORMULIER_BRON_KENMERK'"
+                        zaak1UUID = getString("uuid").let(UUID::fromString)
+                    }
                 }
             }
         }
