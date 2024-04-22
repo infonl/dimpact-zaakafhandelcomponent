f3542dd81f6a07039a161d6e2438ff5210126c59
Edgar Vonk <edgar@info.nl>
2024-04-22 13:15:43 +0200

    chore: move more logic to new TaskService class

--- src/main/kotlin/net/atos/zac/task/TaskService.kt
+++ src/main/kotlin/net/atos/zac/task/TaskService.kt
@@ -1,7 +1,8 @@
 package net.atos.zac.task
 
 import jakarta.inject.Inject
-import jakarta.validation.Valid
+import net.atos.zac.app.taken.converter.RESTTaakConverter
+import net.atos.zac.app.taken.model.RESTTaakToekennenGegevens
 import net.atos.zac.app.taken.model.RESTTaakVerdelenGegevens
 import net.atos.zac.authentication.LoggedInUser
 import net.atos.zac.event.EventingService
@@ -12,20 +13,54 @@ import net.atos.zac.websocket.event.ScreenEventType
 import net.atos.zac.zoeken.IndexeerService
 import net.atos.zac.zoeken.model.index.ZoekObjectType
 import org.flowable.task.api.Task
-import java.util.*
+import java.util.UUID
 import kotlin.collections.ArrayList
 
 class TaskService @Inject constructor(
     private val flowableTaskService: FlowableTaskService,
     private val indexeerService: IndexeerService,
-    private val eventingService: EventingService
+    private val eventingService: EventingService,
+    private val restTaakConverter: RESTTaakConverter
 ) {
+    fun assignTask(
+        restTaakToekennenGegevens: RESTTaakToekennenGegevens,
+        task: Task,
+        loggedInUser: LoggedInUser
+    ) {
+        val groep = restTaakConverter.extractGroupId(task.identityLinks)
+        var changed = false
+        var updatedTask = task
+        restTaakToekennenGegevens.behandelaarId?.let {
+            if (task.assignee != it) {
+                updatedTask = assignTaskToUser(
+                    taskId = task.id,
+                    assignee = it,
+                    loggedInUser = loggedInUser,
+                    explanation = restTaakToekennenGegevens.reden
+                )
+                changed = true
+            }
+        }
+        if (groep != restTaakToekennenGegevens.groepId) {
+            updatedTask = flowableTaskService.assignTaskToGroup(
+                task,
+                restTaakToekennenGegevens.groepId,
+                restTaakToekennenGegevens.reden
+            )
+            changed = true
+        }
+        if (changed) {
+            taakBehandelaarGewijzigd(updatedTask, restTaakToekennenGegevens.zaakUuid)
+            indexeerService.indexeerDirect(restTaakToekennenGegevens.taakId, ZoekObjectType.TAAK)
+        }
+    }
+
     /**
-     * Asynchronously assigns a list of zaken to a group and/or user and updates the search index on the fly.
+     * Assigns a list of tasks to a group and/or user and updates the search index.
      */
     @Suppress("LongParameterList")
-    fun assignTasksAsync(
-        @Valid restTaakVerdelenGegevens: RESTTaakVerdelenGegevens,
+    fun assignTasks(
+        restTaakVerdelenGegevens: RESTTaakVerdelenGegevens,
         loggedInUser: LoggedInUser
     ) {
         val taakIds: MutableList<String?> = ArrayList()
