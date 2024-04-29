bf4d2a5f7b6c7ed9c19213927d5d29ad35a63660
felixcicatt <felix.cornelissen@icatt.nl>
2024-04-29 19:32:45 +0200

    refactor: directly index at all times

--- src/main/java/net/atos/zac/util/IndexerenRESTService.java
+++ src/main/java/net/atos/zac/util/IndexerenRESTService.java
@@ -5,11 +5,7 @@
 
 package net.atos.zac.util;
 
-import java.util.Arrays;
-
-import jakarta.enterprise.inject.Instance;
 import jakarta.inject.Inject;
-import jakarta.servlet.http.HttpSession;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
@@ -18,11 +14,7 @@ import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-import net.atos.zac.authentication.ActiveSession;
-import net.atos.zac.authentication.SecurityUtil;
 import net.atos.zac.zoeken.IndexeerService;
-import net.atos.zac.zoeken.model.index.HerindexerenInfo;
-import net.atos.zac.zoeken.model.index.IndexResult;
 import net.atos.zac.zoeken.model.index.ZoekObjectType;
 
 @Path("indexeren")
@@ -33,36 +25,10 @@ public class IndexerenRESTService {
     @Inject
     private IndexeerService indexeerService;
 
-    @Inject
-    @ActiveSession
-    private Instance<HttpSession> httpSession;
-
     @GET
     @Path("herindexeren/{type}")
-    public HerindexerenInfo herindexeren(@PathParam("type") ZoekObjectType type) {
-        return indexeerService.herindexeren(type);
-    }
-
-    /**
-     * Indexeert eerst {aantal} taken, daarna {aantal} zaken.
-     *
-     * @param aantal 100 is een goede default waarde
-     * @return het aantal resterende items na het uitvoeren van deze aanroep
-     */
-    @GET
-    @Path("{aantal}")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String indexeren(@PathParam("aantal") int aantal) {
-        SecurityUtil.setFunctioneelGebruiker(httpSession.get());
-        final StringBuilder info = new StringBuilder();
-        Arrays.stream(ZoekObjectType.values()).forEach(type -> {
-            final IndexResult resultaat = indexeerService.indexeer(aantal, type);
-            info.append(
-                    "[%s] geindexeerd: %d, verwijderd: %d, resterend: %d\n"
-                            .formatted(type.toString(), resultaat.indexed(), resultaat.removed(), resultaat.remaining())
-            );
-        });
-        return info.toString();
+    public void herindexeren(@PathParam("type") ZoekObjectType type) {
+        indexeerService.herindexeren(type);
     }
 
     @POST
