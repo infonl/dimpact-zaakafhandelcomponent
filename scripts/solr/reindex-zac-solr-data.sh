7f3ce04e475104802667d008f00a37f1510d40e0
felixcicatt <felix.cornelissen@icatt.nl>
2024-04-30 10:56:02 +0200

    chore: remove references to the removed indexeren/{number} endpoint

--- scripts/solr/reindex-zac-solr-data.sh
+++ scripts/solr/reindex-zac-solr-data.sh
@@ -10,26 +10,22 @@ help()
 {
    echo "Sends requests to ZAC to reindex zaak, taak and document data in Solr."
    echo
-   echo "Syntax: $0 [-u|m|h]"
+   echo "Syntax: $0 [-u|h]"
    echo "options:"
    echo "-u     Base ZAC URL, defaults to 'http://localhost:8080'."
-   echo "-m     Maximum number of items to reindex; defaults to '1000'."
    echo "-h     Print this help."
    echo
 }
 
 zacBaseURL="http://localhost:8080"
-maxItems=1000
 
-while getopts 'u:m:h' OPTION; do
+while getopts 'u:h' OPTION; do
   case $OPTION in
     h)
       Help
       exit;;
     u)
       zacBaseURL=$OPTARG;;
-    m)
-      maxItems=$OPTARG;;
     \?)
       echo "Error: Invalid option"
       help
@@ -37,10 +33,9 @@ while getopts 'u:m:h' OPTION; do
   esac
 done
 
-echo "Sending requests to ZAC to reindex zaak, taak and document data in Solr using ZAC base URL: '$zacBaseURL' and maximum number of items: '$maxItems'."
+echo "Sending requests to ZAC to reindex zaak, taak and document data in Solr using ZAC base URL: '$zacBaseURL'."
 
 ## Note that we first need to mark all items for re-indexation per item type
 curl ${zacBaseURL}/rest/indexeren/herindexeren/ZAAK \
     && curl ${zacBaseURL}/rest/indexeren/herindexeren/TAAK \
-    && curl ${zacBaseURL}/rest/indexeren/herindexeren/DOCUMENT \
-    && curl ${zacBaseURL}/rest/indexeren/${maxItems}
+    && curl ${zacBaseURL}/rest/indexeren/herindexeren/DOCUMENT
