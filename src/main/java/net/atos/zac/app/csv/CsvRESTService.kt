42dbde0dfb6bb904aeaf892696e38e10b26fb238
Edgar Vonk <edgar@info.nl>
2024-04-30 13:48:42 +0200

    chore: convert CSV REST service to Kotlin - step 2

--- src/main/java/net/atos/zac/app/csv/CsvRESTService.kt
+++ src/main/java/net/atos/zac/app/csv/CsvRESTService.kt
@@ -2,55 +2,42 @@
  * SPDX-FileCopyrightText: 2022 Atos
  * SPDX-License-Identifier: EUPL-1.2+
  */
+package net.atos.zac.app.csv
 
-package net.atos.zac.app.csv;
-
-import static net.atos.zac.gebruikersvoorkeuren.model.TabelInstellingen.AANTAL_PER_PAGINA_MAX;
-
-import jakarta.inject.Inject;
-import jakarta.inject.Singleton;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.StreamingOutput;
-
-import net.atos.zac.app.zoeken.converter.RESTZoekParametersConverter;
-import net.atos.zac.app.zoeken.model.RESTZoekParameters;
-import net.atos.zac.csv.CsvService;
-import net.atos.zac.zoeken.ZoekenService;
-import net.atos.zac.zoeken.model.ZoekObject;
-import net.atos.zac.zoeken.model.ZoekParameters;
-import net.atos.zac.zoeken.model.ZoekResultaat;
+import jakarta.inject.Inject
+import jakarta.inject.Singleton
+import jakarta.ws.rs.Consumes
+import jakarta.ws.rs.POST
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.Produces
+import jakarta.ws.rs.core.MediaType
+import jakarta.ws.rs.core.Response
+import net.atos.zac.app.zoeken.converter.RESTZoekParametersConverter
+import net.atos.zac.app.zoeken.model.RESTZoekParameters
+import net.atos.zac.csv.CsvService
+import net.atos.zac.gebruikersvoorkeuren.model.TabelInstellingen
+import net.atos.zac.zoeken.ZoekenService
+import nl.lifely.zac.util.NoArgConstructor
 
 @Path("csv")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 @Singleton
-public class CsvRESTService {
-
-    @Inject
-    private ZoekenService zoekenService;
-
-    @Inject
-    private RESTZoekParametersConverter restZoekParametersConverter;
-
-    @Inject
-    private CsvService csvService;
-
+@NoArgConstructor
+class CsvRESTService @Inject constructor(
+    private val zoekenService: ZoekenService,
+    private val restZoekParametersConverter: RESTZoekParametersConverter,
+    private val csvService: CsvService
+) {
     @POST
     @Path("export")
-    public Response downloadCSV(final RESTZoekParameters restZoekParameters) {
-        final ZoekParameters zoekParameters = restZoekParametersConverter.convert(restZoekParameters);
-        if (zoekParameters.getRows() == 0) { // If rows isn't set, use max per page.
-            zoekParameters.setRows(AANTAL_PER_PAGINA_MAX);
+    fun downloadCSV(restZoekParameters: RESTZoekParameters?): Response {
+        val zoekParameters = restZoekParametersConverter.convert(restZoekParameters)
+        if (zoekParameters.rows == 0) { // If rows isn't set, use max per page.
+            zoekParameters.rows = TabelInstellingen.AANTAL_PER_PAGINA_MAX
         }
-        final ZoekResultaat<? extends ZoekObject> zoekResultaat = zoekenService.zoek(zoekParameters);
-
-        final StreamingOutput streamingOutput = csvService.exportToCsv(zoekResultaat);
-
-        return Response.ok(streamingOutput).header("Content-Type", "text/csv").build();
+        val zoekResultaat = zoekenService.zoek(zoekParameters)
+        val streamingOutput = csvService.exportToCsv(zoekResultaat)
+        return Response.ok(streamingOutput).header("Content-Type", "text/csv").build()
     }
 }
