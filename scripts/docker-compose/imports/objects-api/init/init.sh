f1b0d81b816bd62ad14ebd81311c7ebef8f20c25
Edgar Vonk <edgar@info.nl>
2024-05-13 13:50:47 +0200

    chore: added explicit sleep for 10 seconds before importing our test data into the Objecten database

--- scripts/docker-compose/imports/objects-api/init/init.sh
+++ scripts/docker-compose/imports/objects-api/init/init.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+# Wait here a bit for the Objects API database to be initialized (by Objecten).
+# In the end better to add a health check to the Objects API service in Docker Compose but
+# that is not trivial without curl and wget.
+sleep 10
+
 #
 # SPDX-FileCopyrightText: 2024 Lifely
 # SPDX-License-Identifier: EUPL-1.2+
