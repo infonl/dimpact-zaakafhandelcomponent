5932b896d08ba8f8ec5b9516982c0a21386ea283
Edgar Vonk <edgar@info.nl>
2024-05-13 13:40:03 +0200

    chore: removed wait for objects

--- scripts/docker-compose/imports/objects-api/init/init.sh
+++ scripts/docker-compose/imports/objects-api/init/init.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-c=0
-while ! curl http://objecten-api.local:8000
-do
-    c=$((c+1))
-    if [ "$c" = "12" ]; then
-        break
-    fi
-    sleep 10
-done
+set -e
+
+#
+# SPDX-FileCopyrightText: 2024 Lifely
+# SPDX-License-Identifier: EUPL-1.2+
+#
 
 python /app/src/manage.py loaddata demodata
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.org', 'admin')" | python /app/src/manage.py shell
