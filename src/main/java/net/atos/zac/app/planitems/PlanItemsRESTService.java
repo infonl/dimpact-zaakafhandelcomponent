c2405ad2aad3e2c19187ecea5c7081a1b1131e9e
Edgar Vonk <edgar@info.nl>
2024-05-16 12:26:27 +0200

    chore: made 'fromZaak' into a Kotlin extension function

--- src/main/java/net/atos/zac/app/planitems/PlanItemsRESTService.java
+++ src/main/java/net/atos/zac/app/planitems/PlanItemsRESTService.java
@@ -223,7 +223,7 @@ public class PlanItemsRESTService {
                             taakVariabelenService.readMailBody(taakdata).orElse(null),
                             taakVariabelenService.readMailBijlagen(taakdata).orElse(null),
                             true),
-                    BronnenKt.fromZaak(zaak)));
+                    BronnenKt.getBronnenFromZaak(zaak)));
         }
         cmmnService.startHumanTaskPlanItem(
                 humanTaskData.planItemInstanceId,
