31853ec9ed86ba7d2fed10868862f6b73101b10c
Hristo Iliev <hristo.iliev@info.nl>
2024-05-22 17:48:25 +0300

    feat: migrate from MailJet to SMTP
    Replace MailJet API with SMTP
    
    Solves: PZ-2506

--- src/main/kotlin/net/atos/zac/mail/model/MailAdres.kt
+++ src/main/kotlin/net/atos/zac/mail/model/MailAdres.kt
@@ -5,10 +5,18 @@
 package net.atos.zac.mail.model
 
 import jakarta.json.bind.annotation.JsonbProperty
+import jakarta.mail.Address
+import jakarta.mail.internet.InternetAddress
 
 class MailAdres(
     @field:JsonbProperty("Email") var email: String,
     @field:JsonbProperty("Name") var name: String?
 ) {
     constructor(email: String) : this(email, null)
+
+    fun toAddress(): Address = InternetAddress(email, name)
+
+    override fun toString(): String {
+        return "$email ($name)"
+    }
 }
