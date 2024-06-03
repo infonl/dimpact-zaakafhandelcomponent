19cc3a273461e85b2fb0a9d47e047d3d69e0f784
Hristo Iliev <hristo.iliev@info.nl>
2024-06-03 10:31:55 +0300

    feat: add parent field to TemplateGroup

--- src/main/resources/schemas/V56__smartdocuments_templates.sql
+++ src/main/resources/schemas/V56__smartdocuments_templates.sql
@@ -5,10 +5,11 @@
 
 CREATE TABLE ${schema}.template_group
 (
-    id_template_group BIGINT                   NOT NULL,
-    smartdocuments_id VARCHAR                  NOT NULL,
-    naam              VARCHAR                  NOT NULL,
-    creatiedatum      TIMESTAMP WITH TIME ZONE NOT NULL,
+    id_template_group        BIGINT                   NOT NULL,
+    smartdocuments_id        VARCHAR                  NOT NULL,
+    naam                     VARCHAR                  NOT NULL,
+    parent_template_group_id BIGINT,
+    creatiedatum             TIMESTAMP WITH TIME ZONE NOT NULL,
     CONSTRAINT pk_template_group PRIMARY KEY (id_template_group)
 );
 
@@ -16,6 +17,11 @@ CREATE SEQUENCE ${schema}.sq_template_group START WITH 1 INCREMENT BY 1 NO MINVA
 CREATE INDEX idx_template_group_smartdocuments_id ON ${schema}.template_group USING btree (smartdocuments_id);
 CREATE INDEX idx_template_group_naam ON ${schema}.template_group USING btree (naam);
 CREATE INDEX idx_template_group_creatiedatum ON ${schema}.template_group USING btree (creatiedatum);
+COMMENT ON COLUMN ${schema}.template_group.id_template_group IS 'Unique ID for the template group';
+COMMENT ON COLUMN ${schema}.template_group.smartdocuments_id IS 'ID for the template group in SmartDocuments';
+COMMENT ON COLUMN ${schema}.template_group.naam IS 'Name of the template group';
+COMMENT ON COLUMN ${schema}.template_group.parent_template_group_id IS 'ID of the parent template group (or NULL for root)';
+COMMENT ON COLUMN ${schema}.template_group.creatiedatum IS 'Date on which the template group was stored in this table';
 
 CREATE TABLE ${schema}.template
 (
@@ -36,3 +42,8 @@ CREATE INDEX idx_template_smartdocuments_id ON ${schema}.template USING btree (s
 CREATE INDEX idx_template_id_template_group ON ${schema}.template USING btree (id_template_group);
 CREATE INDEX idx_template_naam ON ${schema}.template USING btree (naam);
 CREATE INDEX idx_template_creatiedatum ON ${schema}.template USING btree (creatiedatum);
+COMMENT ON COLUMN ${schema}.template.id_template IS 'Unique ID for the template';
+COMMENT ON COLUMN ${schema}.template.smartdocuments_id IS 'ID for the template in SmartDocuments';
+COMMENT ON COLUMN ${schema}.template.id_template_group IS 'ID of the template group this template is part of';
+COMMENT ON COLUMN ${schema}.template.naam IS 'Name of the template';
+COMMENT ON COLUMN ${schema}.template.creatiedatum IS 'Date on which the template was stored in this table';
