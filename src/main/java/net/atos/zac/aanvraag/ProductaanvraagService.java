85f57db3640fb948fc80136edb8a2229c7d8836d
Edgar Vonk <edgar@info.nl>
2024-06-04 11:37:01 +0200

    chore: Spotless

--- src/main/java/net/atos/zac/aanvraag/ProductaanvraagService.java
+++ src/main/java/net/atos/zac/aanvraag/ProductaanvraagService.java
@@ -5,11 +5,30 @@
 
 package net.atos.zac.aanvraag;
 
+import static net.atos.zac.configuratie.ConfiguratieService.BRON_ORGANISATIE;
+import static net.atos.zac.configuratie.ConfiguratieService.COMMUNICATIEKANAAL_EFORMULIER;
+import static net.atos.zac.util.UriUtil.uuidFromURI;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
+import java.net.URI;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
 import jakarta.json.bind.JsonbConfig;
+
+import org.apache.commons.collections4.ListUtils;
+import org.apache.commons.lang3.StringUtils;
+
 import net.atos.client.or.object.ObjectsClientService;
 import net.atos.client.or.object.model.ORObject;
 import net.atos.client.vrl.VRLClientService;
@@ -48,23 +67,6 @@ import net.atos.zac.util.JsonbUtil;
 import net.atos.zac.zaaksturing.ZaakafhandelParameterBeheerService;
 import net.atos.zac.zaaksturing.ZaakafhandelParameterService;
 import net.atos.zac.zaaksturing.model.ZaakafhandelParameters;
-import org.apache.commons.collections4.ListUtils;
-import org.apache.commons.lang3.StringUtils;
-
-import java.net.URI;
-import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.UUID;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import static net.atos.zac.configuratie.ConfiguratieService.BRON_ORGANISATIE;
-import static net.atos.zac.configuratie.ConfiguratieService.COMMUNICATIEKANAAL_EFORMULIER;
-import static net.atos.zac.util.UriUtil.uuidFromURI;
-import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 @ApplicationScoped
 public class ProductaanvraagService {
@@ -327,9 +329,9 @@ public class ProductaanvraagService {
         );
         final var zaakgegevens = productaanvraag.getZaakgegevens();
         if (
-                zaakgegevens != null &&
-                zaakgegevens.getGeometry() != null &&
-                zaakgegevens.getGeometry().getType() == Geometry.Type.POINT
+            zaakgegevens != null &&
+            zaakgegevens.getGeometry() != null &&
+            zaakgegevens.getGeometry().getType() == Geometry.Type.POINT
         ) {
             zaak.setZaakgeometrie(AanvraagToZgwConverterKt.convertToZgwPoint(zaakgegevens.getGeometry()));
         }
