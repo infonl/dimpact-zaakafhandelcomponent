16b1ad5810458ffb0cab72397a31613f3a6fa87d
Edgar Vonk <edgar@info.nl>
2024-06-05 16:09:36 +0200

    feat: added endpoint and service to delete old signaleringen

--- src/main/java/net/atos/zac/util/SignaleringenAdminRESTService.java
+++ src/main/java/net/atos/zac/util/SignaleringenAdminRESTService.java
@@ -9,6 +9,7 @@ import jakarta.enterprise.inject.Instance;
 import jakarta.inject.Inject;
 import jakarta.servlet.http.HttpSession;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -17,25 +18,37 @@ import jakarta.ws.rs.core.MediaType;
 import net.atos.zac.authentication.ActiveSession;
 import net.atos.zac.authentication.SecurityUtil;
 import net.atos.zac.event.EventingService;
+import net.atos.zac.signalering.SignaleringenService;
 import net.atos.zac.util.event.JobEvent;
 import net.atos.zac.util.event.JobId;
 
-@Path("signaleren")
+@Path("admin/signaleringen")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
-public class SignalerenRESTService {
+public class SignaleringenAdminRESTService {
 
     @Inject
     private EventingService eventingService;
+
+    @Inject
+    private SignaleringenService signaleringenService;
 
     @Inject
     @ActiveSession
     private Instance<HttpSession> httpSession;
 
     @GET
+    @Path("send-zaak-signaleringen")
     public String zaakSignaleringenVerzenden() {
         SecurityUtil.setFunctioneelGebruiker(httpSession.get());
         eventingService.send(new JobEvent(JobId.SIGNALERINGEN_JOB));
-        return String.format("%s: gestart...", JobId.SIGNALERINGEN_JOB.getName());
+        return String.format("Started sending zaak signaleringen using job: '%s'", JobId.SIGNALERINGEN_JOB.getName());
+    }
+
+    @DELETE
+    @Path("delete-old")
+    public void deleteOldSignaleringen() {
+        SecurityUtil.setFunctioneelGebruiker(httpSession.get());
+        signaleringenService.deleteOldSignaleringen();
     }
 }
