57444275b3247b0d947a2a9915bbf98b00aef99e
Edgar Vonk <edgar@info.nl>
2024-06-06 12:14:33 +0200

    chore: add unit test for signaleringen service; work in progress

---
+++ src/test/kotlin/net/atos/zac/signalering/SignaleringServiceTest.kt
@@ -0,0 +1,50 @@
+package net.atos.zac.signalering
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.mockk.checkUnnecessaryStub
+import io.mockk.clearAllMocks
+import io.mockk.mockk
+import net.atos.client.zgw.zrc.ZRCClientService
+import net.atos.zac.app.zaken.converter.RESTZaakOverzichtConverter
+import net.atos.zac.event.EventingService
+import net.atos.zac.mail.MailService
+
+class SignaleringServiceTest : BehaviorSpec({
+    val eventingService = mockk<EventingService>()
+    val mailService = mockk<MailService>()
+    val signaleringenMailHelper = mockk<SignaleringenMailHelper>()
+    val signaleringenZACHelper = mockk<SignaleringenZACHelper>()
+    val signaleringenPredicateHelper = mockk<SignaleringenPredicateHelper>()
+    val zrcClientService = mockk<ZRCClientService>()
+    val restZaakOverzichtConverter = mockk<RESTZaakOverzichtConverter>()
+
+    val signaleringService = SignaleringenService(
+        eventingService = eventingService,
+        mailService = mailService,
+        signaleringenMailHelper = signaleringenMailHelper,
+        signaleringenZACHelper = signaleringenZACHelper,
+        signaleringenPredicateHelper = signaleringenPredicateHelper,
+        zrcClientService = zrcClientService,
+        restZaakOverzichtConverter = restZaakOverzichtConverter
+    )
+
+    beforeEach {
+        checkUnnecessaryStub()
+    }
+
+    beforeSpec {
+        clearAllMocks()
+    }
+
+    Given("two signaleringen, one of which has a creation date older than 1 day") {
+        val deleteOlderThanDays = 1L
+
+        When("signaleringen older than 1 day are deleted") {
+
+            signaleringService.deleteOldSignaleringen(deleteOlderThanDays)
+
+            Then("one signalering is deleted but the other is not") {
+            }
+        }
+    }
+})
