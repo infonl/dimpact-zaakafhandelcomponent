b59246d3e4d2013e2fe619ba8a036234cf389fac
Hristo Iliev <hristo.iliev@info.nl>
2024-06-12 22:37:22 +0300

    feat: convert EnkelvoudigInformatieObjectUpdateService to Kotlin

--- src/main/java/net/atos/zac/app/informatieobjecten/EnkelvoudigInformatieObjectUpdateService.kt
+++ src/main/java/net/atos/zac/app/informatieobjecten/EnkelvoudigInformatieObjectUpdateService.kt
@@ -2,80 +2,79 @@
  * SPDX-FileCopyrightText: 2022 Atos, 2024 Lifely
  * SPDX-License-Identifier: EUPL-1.2+
  */
+package net.atos.zac.app.informatieobjecten
 
-package net.atos.zac.app.informatieobjecten;
-
-import static org.apache.commons.lang3.ObjectUtils.isNotEmpty;
-
-import java.time.LocalDate;
-import java.util.UUID;
-
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.enterprise.inject.Instance;
-import jakarta.inject.Inject;
-import jakarta.transaction.Transactional;
-
-import net.atos.client.zgw.drc.DrcClientService;
-import net.atos.client.zgw.drc.model.generated.EnkelvoudigInformatieObjectWithLockData;
-import net.atos.client.zgw.drc.model.generated.Ondertekening;
-import net.atos.zac.authentication.LoggedInUser;
-import net.atos.zac.enkelvoudiginformatieobject.EnkelvoudigInformatieObjectLockService;
-import net.atos.zac.enkelvoudiginformatieobject.model.EnkelvoudigInformatieObjectLock;
+import jakarta.enterprise.context.ApplicationScoped
+import jakarta.enterprise.inject.Instance
+import jakarta.inject.Inject
+import jakarta.transaction.Transactional
+import net.atos.client.zgw.drc.DrcClientService
+import net.atos.client.zgw.drc.model.generated.EnkelvoudigInformatieObjectWithLockData
+import net.atos.client.zgw.drc.model.generated.Ondertekening
+import net.atos.zac.authentication.LoggedInUser
+import net.atos.zac.enkelvoudiginformatieobject.EnkelvoudigInformatieObjectLockService
+import net.atos.zac.enkelvoudiginformatieobject.model.EnkelvoudigInformatieObjectLock
+import org.apache.commons.lang3.ObjectUtils
+import java.time.LocalDate
+import java.util.UUID
 
 @ApplicationScoped
 @Transactional
-public class EnkelvoudigInformatieObjectUpdateService {
-
-    private static final String VERZEND_TOELICHTING_PREFIX = "Per post";
-
-    private static final String ONDERTEKENEN_TOELICHTING = "Door ondertekenen";
+class EnkelvoudigInformatieObjectUpdateService {
+    @Inject
+    private val enkelvoudigInformatieObjectLockService: EnkelvoudigInformatieObjectLockService? = null
 
     @Inject
-    private EnkelvoudigInformatieObjectLockService enkelvoudigInformatieObjectLockService;
+    private val drcClientService: DrcClientService? = null
 
     @Inject
-    private DrcClientService drcClientService;
+    private val loggedInUserInstance: Instance<LoggedInUser>? = null
 
-    @Inject
-    private Instance<LoggedInUser> loggedInUserInstance;
-
-    public void verzendEnkelvoudigInformatieObject(final UUID uuid, final LocalDate verzenddatum, final String toelichting) {
-        final var update = new EnkelvoudigInformatieObjectWithLockData();
-        update.setVerzenddatum(verzenddatum);
-        updateEnkelvoudigInformatieObjectWithLockData(uuid, update, isNotEmpty(toelichting) ? "%s: %s".formatted(
-                VERZEND_TOELICHTING_PREFIX, toelichting) :
-                VERZEND_TOELICHTING_PREFIX);
+    fun verzendEnkelvoudigInformatieObject(uuid: UUID?, verzenddatum: LocalDate?, toelichting: String?) {
+        val update = EnkelvoudigInformatieObjectWithLockData()
+        update.verzenddatum = verzenddatum
+        updateEnkelvoudigInformatieObjectWithLockData(
+            uuid, update, if (ObjectUtils.isNotEmpty(toelichting)) "%s: %s".formatted(
+                VERZEND_TOELICHTING_PREFIX, toelichting
+            ) else VERZEND_TOELICHTING_PREFIX
+        )
     }
 
-    public void ondertekenEnkelvoudigInformatieObject(final UUID uuid) {
-        final var update = new EnkelvoudigInformatieObjectWithLockData();
-        final Ondertekening ondertekening = new Ondertekening();
-        ondertekening.setSoort(Ondertekening.SoortEnum.DIGITAAL);
-        ondertekening.setDatum(LocalDate.now());
-        update.setOndertekening(ondertekening);
-        update.setStatus(EnkelvoudigInformatieObjectWithLockData.StatusEnum.DEFINITIEF);
-        updateEnkelvoudigInformatieObjectWithLockData(uuid, update, ONDERTEKENEN_TOELICHTING);
+    fun ondertekenEnkelvoudigInformatieObject(uuid: UUID?) {
+        val update = EnkelvoudigInformatieObjectWithLockData()
+        val ondertekening = Ondertekening()
+        ondertekening.soort = Ondertekening.SoortEnum.DIGITAAL
+        ondertekening.datum = LocalDate.now()
+        update.ondertekening = ondertekening
+        update.status = EnkelvoudigInformatieObjectWithLockData.StatusEnum.DEFINITIEF
+        updateEnkelvoudigInformatieObjectWithLockData(uuid, update, ONDERTEKENEN_TOELICHTING)
     }
 
-    public EnkelvoudigInformatieObjectWithLockData updateEnkelvoudigInformatieObjectWithLockData(
-            final UUID uuid,
-            final EnkelvoudigInformatieObjectWithLockData update,
-            final String toelichting
-    ) {
-        EnkelvoudigInformatieObjectLock tempLock = null;
+    fun updateEnkelvoudigInformatieObjectWithLockData(
+        uuid: UUID?,
+        update: EnkelvoudigInformatieObjectWithLockData,
+        toelichting: String?
+    ): EnkelvoudigInformatieObjectWithLockData {
+        var tempLock: EnkelvoudigInformatieObjectLock? = null
         try {
-            final var existingLock = enkelvoudigInformatieObjectLockService.findLock(uuid);
+            val existingLock = enkelvoudigInformatieObjectLockService!!.findLock(uuid!!)
             if (existingLock != null) {
-                update.setLock(existingLock.getLock());
+                update.lock = existingLock.lock
             } else {
-                tempLock = enkelvoudigInformatieObjectLockService.createLock(uuid, loggedInUserInstance.get().getId());
-                update.setLock(tempLock.getLock());
+                tempLock = enkelvoudigInformatieObjectLockService.createLock(uuid, loggedInUserInstance!!.get().id)
+                update.lock = tempLock.lock
             }
-            return drcClientService.updateEnkelvoudigInformatieobject(uuid, update, toelichting);
+            return drcClientService!!.updateEnkelvoudigInformatieobject(uuid, update, toelichting)
         } finally {
             if (tempLock != null) {
-                enkelvoudigInformatieObjectLockService.deleteLock(uuid);
+                enkelvoudigInformatieObjectLockService!!.deleteLock(uuid!!)
             }
         }
+    }
+
+    companion object {
+        private const val VERZEND_TOELICHTING_PREFIX = "Per post"
+
+        private const val ONDERTEKENEN_TOELICHTING = "Door ondertekenen"
     }
 }
