821890fdb52941d2c87ea2fff603ab048de8d9c0
Hristo Iliev <hristo.iliev@info.nl>
2024-06-17 11:10:12 +0300

    feat: add smartdocuments_ prefix to tables

--- src/main/resources/schemas/V58__smartdocuments_document_creation_templates.sql
+++ src/main/resources/schemas/V58__smartdocuments_document_creation_templates.sql
@@ -4,10 +4,10 @@
  */
 
 ALTER TABLE ${schema}.template_group
-    RENAME TO document_creation_template_group;
+    RENAME TO smartdocuments_document_creation_template_group;
 ALTER SEQUENCE sq_template_group
     RENAME TO sq_document_creation_template_group;
-ALTER TABLE ${schema}.document_creation_template_group
+ALTER TABLE ${schema}.smartdocuments_document_creation_template_group
     RENAME CONSTRAINT fk_template_group_zaakafhandelparameters TO fk_document_creation_template_group_zaakafhandelparameters;
 ALTER INDEX idx_template_group_smartdocuments_id
     RENAME TO idx_document_creation_template_group_smartdocuments_id;
@@ -20,12 +20,12 @@ ALTER INDEX idx_template_group_zaakafhandelparameters_id
 
 
 ALTER TABLE ${schema}.template
-    RENAME TO document_creation_template;
+    RENAME TO smartdocuments_document_creation_template;
 ALTER SEQUENCE sq_template
     RENAME TO sq_document_creation_template;
-ALTER TABLE ${schema}.document_creation_template
+ALTER TABLE ${schema}.smartdocuments_document_creation_template
     RENAME CONSTRAINT fk_template_zaakafhandelparameters TO fk_document_creation_template_zaakafhandelparameters;
-ALTER TABLE ${schema}.document_creation_template
+ALTER TABLE ${schema}.smartdocuments_document_creation_template
     RENAME CONSTRAINT fk_template_template_group_id TO fk_document_creation_template_template_group_id;
 ALTER INDEX idx_template_smartdocuments_id
     RENAME TO idx_document_creation_template_smartdocuments_id;
@@ -37,4 +37,4 @@ ALTER INDEX idx_template_zaakafhandelparameters_id
     RENAME TO idx_document_creation_template_zaakafhandelparameters_id;
 
 CREATE INDEX idx_document_creation_template_template_group_id
-    ON ${schema}.document_creation_template USING btree (template_group_id);
+    ON ${schema}.smartdocuments_document_creation_template USING btree (template_group_id);
