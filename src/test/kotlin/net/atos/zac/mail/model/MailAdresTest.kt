c4f0e15c7ca400d2d945f2d12975acbc6401ad91
Hristo Iliev <hristo.iliev@info.nl>
2024-06-19 18:35:26 +0300

    feat: add test for MailAdres

---
+++ src/test/kotlin/net/atos/zac/mail/model/MailAdresTest.kt
@@ -0,0 +1,27 @@
+package net.atos.zac.mail.model
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+
+class MailAdresTest : BehaviorSpec({
+
+    Given("a valid email address") {
+        When("constructing and object") {
+            MailAdres("valid@email.address")
+            Then("it should succeed") {}
+        }
+    }
+
+    Given("an invalid email address") {
+        When("constructing and object") {
+            val exception = shouldThrow<IllegalArgumentException> {
+                MailAdres("fake email")
+            }
+
+            Then("it should error") {
+                exception.message shouldBe "Email 'fake email' is not valid"
+            }
+        }
+    }
+})
