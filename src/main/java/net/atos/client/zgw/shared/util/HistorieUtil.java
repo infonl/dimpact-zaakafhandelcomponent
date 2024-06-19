c69c261a8018ba297c099e02a6d7ff5354de1cd7
Hristo Iliev <hristo.iliev@info.nl>
2024-06-19 11:10:04 +0300

    fix: spotless formatting

--- src/main/java/net/atos/client/zgw/shared/util/HistorieUtil.java
+++ src/main/java/net/atos/client/zgw/shared/util/HistorieUtil.java
@@ -19,7 +19,7 @@ public final class HistorieUtil {
     private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm")
-                                                                     .withZone(ZoneId.systemDefault());
+            .withZone(ZoneId.systemDefault());
 
     private static final String TRUE = "Ja";
 
