f0eab3e17c9251535a32b0e7c9a5386068b377e5
felixcicatt <104420825+felixcicatt@users.noreply.github.com>
2024-06-24 14:16:31 +0200

    Update src/test/kotlin/net/atos/zac/app/ontkoppeldedocumenten/OntkoppeldeDocumentenRESTServiceTest.kt
    Co-authored-by: Hristo Iliev <hsiliev@gmail.com>

--- src/test/kotlin/net/atos/zac/app/ontkoppeldedocumenten/OntkoppeldeDocumentenRESTServiceTest.kt
+++ src/test/kotlin/net/atos/zac/app/ontkoppeldedocumenten/OntkoppeldeDocumentenRESTServiceTest.kt
@@ -101,7 +101,7 @@ class OntkoppeldeDocumentenRESTServiceTest : BehaviorSpec({
             sut.delete(document.id)
 
             Then("the document is deleted") {
-                verify {
+                verify(exactly = 1) {
                     ontkoppeldeDocumentenService.delete(document.id)
                 }
             }
