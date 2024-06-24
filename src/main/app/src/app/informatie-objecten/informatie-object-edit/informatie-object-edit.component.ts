7c7c3cbf467ee2044a171fca571b33597bb8e600
Felix Cornelissen <felix@icatt.nl>
2024-06-24 16:30:12 +0200

    chore: fix null reference error if taal is missing from informatieobject

--- src/main/app/src/app/informatie-objecten/informatie-object-edit/informatie-object-edit.component.ts
+++ src/main/app/src/app/informatie-objecten/informatie-object-edit/informatie-object-edit.component.ts
@@ -110,7 +110,7 @@ export class InformatieObjectEditComponent implements OnInit, OnDestroy {
       .build();
 
     const taal = new SelectFormFieldBuilder({
-      naam: this.translateService.instant(this.infoObject.taal.naam),
+      naam: this.translateService.instant(this.infoObject.taal?.naam),
       value: this.infoObject.taal,
     })
       .id("taal")
