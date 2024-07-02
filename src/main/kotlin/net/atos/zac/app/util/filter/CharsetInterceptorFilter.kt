38a1dc15804849725c783edd4b6f911c96f417e5
Edgar Vonk <edgar@info.nl>
2024-07-02 15:28:02 +0200

    chore: fixed typo

--- src/main/kotlin/net/atos/zac/app/util/filter/CharsetInterceptorFilter.kt
+++ src/main/kotlin/net/atos/zac/app/util/filter/CharsetInterceptorFilter.kt
@@ -11,10 +11,11 @@ import jakarta.ws.rs.ext.Provider
 import org.jboss.resteasy.plugins.providers.multipart.InputPart
 
 /**
- * Filter to set the default charset to UTF-8 for multipart requesr inputs (as used in multipart/formdata).
+ * Filter to set the default charset to UTF-8 for multipart request inputs (as used in multipart/formdata).
  * This fixes the issue where JBoss/RESTEasy does not default (yet) to UTF-8.
  * See e.g.: https://github.com/quarkusio/quarkus/issues/10323
- * Once JBoss/RESTEasy has fixed this issue and defaults correctly to UTF-8, as per specification, this filter can be removed.
+ * Once JBoss/RESTEasy has fixed this issue and defaults correctly to UTF-8, as per specification,
+ * this filter can be removed.
  */
 @Provider
 class CharsetInterceptorFilter : ContainerRequestFilter {
