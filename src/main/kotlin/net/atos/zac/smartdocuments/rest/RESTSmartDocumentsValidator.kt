1c90526fbac035e1f5a454bb763d7c3e3928404a
Hristo Iliev <hsiliev@gmail.com>
2024-07-04 21:54:33 +0300

    Update RESTSmartDocumentsValidator.kt

--- src/main/kotlin/net/atos/zac/smartdocuments/rest/RESTSmartDocumentsValidator.kt
+++ src/main/kotlin/net/atos/zac/smartdocuments/rest/RESTSmartDocumentsValidator.kt
@@ -6,7 +6,7 @@ import net.atos.zac.smartdocuments.SmartDocumentsException
  * Validates that all elements in a templates group set are part of pre-defined templates group superset.
  * The superset can be returned by SmartDocuments structure API or stored in DB
  *
- * @param supersetTemplates set of IRESTSmartDocumentsTemplateGroup to validate against
+ * @param supersetTemplates set of RESTSmartDocumentsTemplateGroup to validate against
  */
 infix fun Set<RESTMappedSmartDocumentsTemplateGroup>.isSubsetOf(
     supersetTemplates: Set<RESTSmartDocumentsTemplateGroup>
