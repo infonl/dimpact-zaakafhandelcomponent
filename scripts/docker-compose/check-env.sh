1a1913e92d7690ce790be8a1c37c1df9b61dde54
Hristo Iliev <hristo.iliev@info.nl>
2024-07-09 10:45:01 +0300

    feature: Allow Docker Compose volumes to be kept after IT complete
    Adds a script that can configure environment variable controlling this behaviour
    
    Solves: PZ-3153

---
+++ scripts/docker-compose/check-env.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+#
+# SPDX-FileCopyrightText: 2024 Lifely
+# SPDX-License-Identifier: EUPL-1.2+
+#
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+(
+  cd "$SCRIPT_DIR"
+
+  if sudo lsof -i@localhost:8080; then
+    echo -e "\nPlease stop the currently running WildFly!"
+    exit 1
+  fi
+
+  if docker compose ls --filter name=^zac$ | grep running; then
+    echo -e "\nDocker compose already running!"
+    exit 1
+  fi
+)
