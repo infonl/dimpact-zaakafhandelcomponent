71614e36ca26f6d7ddce05a7634e674afd9df6df
Hristo Iliev <hristo.iliev@info.nl>
2024-07-09 11:22:24 +0300

    chore: more descriptive check containers script name

--- scripts/docker-compose/check-for-running-containers.sh
+++ scripts/docker-compose/check-for-running-containers.sh
@@ -13,7 +13,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
   cd "$SCRIPT_DIR"
 
   if sudo lsof -i@localhost:8080; then
-    echo -e "\nPlease stop the currently running WildFly!"
+    echo -e "\nPlease stop the currently running process on port 8080!"
     exit 1
   fi
 
