e853cd8fb198d7d76a490caa6dfa14ce987e043e
Hristo Iliev <hristo.iliev@info.nl>
2024-07-17 18:40:28 +0300

    chore: convert audit package to Kotlin

--- src/main/java/net/atos/zac/app/audit/converter/AbstractAuditWijzigingConverter.kt
+++ src/main/java/net/atos/zac/app/audit/converter/AbstractAuditWijzigingConverter.kt
@@ -2,91 +2,91 @@
  * SPDX-FileCopyrightText: 2021 Atos
  * SPDX-License-Identifier: EUPL-1.2+
  */
+package net.atos.zac.app.audit.converter
 
-package net.atos.zac.app.audit.converter;
+import net.atos.client.zgw.drc.model.generated.StatusEnum
+import net.atos.client.zgw.drc.model.generated.VertrouwelijkheidaanduidingEnum
+import net.atos.client.zgw.shared.model.ObjectType
+import net.atos.client.zgw.shared.model.audit.AuditWijziging
+import net.atos.zac.app.audit.model.RESTHistorieRegel
+import org.apache.commons.lang3.ObjectUtils
+import org.apache.commons.lang3.StringUtils
+import java.time.LocalDate
+import java.time.ZonedDateTime
+import java.util.stream.Stream
 
-import java.time.LocalDate;
-import java.time.ZonedDateTime;
-import java.util.List;
-import java.util.stream.Stream;
-
-import org.apache.commons.lang3.ObjectUtils;
-import org.apache.commons.lang3.StringUtils;
-
-import net.atos.client.zgw.drc.model.generated.StatusEnum;
-import net.atos.client.zgw.drc.model.generated.VertrouwelijkheidaanduidingEnum;
-import net.atos.client.zgw.shared.model.ObjectType;
-import net.atos.client.zgw.shared.model.audit.AuditWijziging;
-import net.atos.zac.app.audit.model.RESTHistorieRegel;
-
-public abstract class AbstractAuditWijzigingConverter<W extends AuditWijziging<?>> {
-
-    public Stream<RESTHistorieRegel> convert(final AuditWijziging<?> wijziging) {
-        return doConvert((W) wijziging);
+abstract class AbstractAuditWijzigingConverter<W : AuditWijziging<*>?> {
+    fun convert(wijziging: AuditWijziging<*>): Stream<RESTHistorieRegel?> {
+        return doConvert(wijziging as W)
     }
 
-    public abstract boolean supports(final ObjectType objectType);
+    abstract fun supports(objectType: ObjectType): Boolean
 
-    protected abstract Stream<RESTHistorieRegel> doConvert(final W wijziging);
+    protected abstract fun doConvert(wijziging: W): Stream<RESTHistorieRegel?>
 
-    protected void checkAttribuut(final String label, final String oud, final String nieuw, final List<RESTHistorieRegel> historieRegels) {
+    protected fun checkAttribuut(
+        label: String,
+        oud: String?,
+        nieuw: String?,
+        historieRegels: MutableList<RESTHistorieRegel?>
+    ) {
         if (!StringUtils.equals(oud, nieuw)) {
-            historieRegels.add(new RESTHistorieRegel(label, oud, nieuw));
+            historieRegels.add(RESTHistorieRegel(label, oud, nieuw))
         }
     }
 
-    protected void checkAttribuut(
-            final String label,
-            final StatusEnum oud,
-            final StatusEnum nieuw,
-            final List<RESTHistorieRegel> historieRegels
+    protected fun checkAttribuut(
+        label: String,
+        oud: StatusEnum,
+        nieuw: StatusEnum,
+        historieRegels: MutableList<RESTHistorieRegel?>
     ) {
         if (oud != nieuw) {
-            historieRegels.add(new RESTHistorieRegel(label, oud, nieuw));
+            historieRegels.add(RESTHistorieRegel(label, oud, nieuw))
         }
     }
 
-    protected void checkAttribuut(
-            final String label,
-            final VertrouwelijkheidaanduidingEnum oud,
-            final VertrouwelijkheidaanduidingEnum nieuw,
-            final List<RESTHistorieRegel> historieRegels
+    protected fun checkAttribuut(
+        label: String,
+        oud: VertrouwelijkheidaanduidingEnum,
+        nieuw: VertrouwelijkheidaanduidingEnum,
+        historieRegels: MutableList<RESTHistorieRegel?>
     ) {
         if (oud != nieuw) {
-            historieRegels.add(new RESTHistorieRegel(label, oud, nieuw));
+            historieRegels.add(RESTHistorieRegel(label, oud, nieuw))
         }
     }
 
-    protected void checkAttribuut(
-            final String label,
-            final Boolean oud,
-            final Boolean nieuw,
-            final List<RESTHistorieRegel> historieRegels
+    protected fun checkAttribuut(
+        label: String,
+        oud: Boolean?,
+        nieuw: Boolean?,
+        historieRegels: MutableList<RESTHistorieRegel?>
     ) {
         if (ObjectUtils.notEqual(oud, nieuw)) {
-            historieRegels.add(new RESTHistorieRegel(label, oud, nieuw));
+            historieRegels.add(RESTHistorieRegel(label, oud, nieuw))
         }
     }
 
-    protected void checkAttribuut(
-            final String label,
-            final LocalDate oud,
-            final LocalDate nieuw,
-            final List<RESTHistorieRegel> historieRegels
+    protected fun checkAttribuut(
+        label: String,
+        oud: LocalDate?,
+        nieuw: LocalDate?,
+        historieRegels: MutableList<RESTHistorieRegel?>
     ) {
         if (ObjectUtils.notEqual(oud, nieuw)) {
-            historieRegels.add(new RESTHistorieRegel(label, oud, nieuw));
+            historieRegels.add(RESTHistorieRegel(label, oud, nieuw))
         }
     }
 
-    protected void checkAttribuut(
-            final String label,
-            final ZonedDateTime oud,
-            final ZonedDateTime nieuw,
-            final List<RESTHistorieRegel> historieRegels
+    protected fun checkAttribuut(
+        label: String,
+        oud: ZonedDateTime?,
+        nieuw: ZonedDateTime?,
+        historieRegels: MutableList<RESTHistorieRegel?>
     ) {
         if (ObjectUtils.notEqual(oud, nieuw)) {
-            historieRegels.add(new RESTHistorieRegel(label, oud, nieuw));
+            historieRegels.add(RESTHistorieRegel(label, oud, nieuw))
         }
     }
 }
--- src/main/java/net/atos/zac/app/audit/converter/besluiten/AuditBesluitConverter.kt
+++ src/main/java/net/atos/zac/app/audit/converter/besluiten/AuditBesluitConverter.kt
@@ -1,41 +1,36 @@
-package net.atos.zac.app.audit.converter.besluiten;
+package net.atos.zac.app.audit.converter.besluiten
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.stream.Stream;
+import net.atos.client.zgw.brc.model.generated.Besluit
+import net.atos.client.zgw.shared.model.ObjectType
+import net.atos.client.zgw.shared.model.audit.besluiten.BesluitWijziging
+import net.atos.zac.app.audit.converter.AbstractAuditWijzigingConverter
+import net.atos.zac.app.audit.model.RESTHistorieRegel
+import java.util.LinkedList
+import java.util.stream.Stream
 
-import net.atos.client.zgw.brc.model.generated.Besluit;
-import net.atos.client.zgw.shared.model.ObjectType;
-import net.atos.client.zgw.shared.model.audit.besluiten.BesluitWijziging;
-import net.atos.zac.app.audit.converter.AbstractAuditWijzigingConverter;
-import net.atos.zac.app.audit.model.RESTHistorieRegel;
-
-public class AuditBesluitConverter extends AbstractAuditWijzigingConverter<BesluitWijziging> {
-
-    @Override
-    public boolean supports(final ObjectType objectType) {
-        return ObjectType.BESLUIT == objectType;
+class AuditBesluitConverter : AbstractAuditWijzigingConverter<BesluitWijziging>() {
+    override fun supports(objectType: ObjectType): Boolean {
+        return ObjectType.BESLUIT == objectType
     }
 
-    @Override
-    protected Stream<RESTHistorieRegel> doConvert(final BesluitWijziging wijziging) {
-        final Besluit oud = wijziging.getOud();
-        final Besluit nieuw = wijziging.getNieuw();
+    override fun doConvert(wijziging: BesluitWijziging): Stream<RESTHistorieRegel?> {
+        val oud = wijziging.oud
+        val nieuw = wijziging.nieuw
 
         if (oud == null || nieuw == null) {
-            return Stream.of(new RESTHistorieRegel("Besluit", toWaarde(oud), toWaarde(nieuw)));
+            return Stream.of(RESTHistorieRegel("Besluit", toWaarde(oud), toWaarde(nieuw)))
         }
 
-        final List<RESTHistorieRegel> historieRegels = new LinkedList<>();
-        checkAttribuut("identificatie", oud.getIdentificatie(), nieuw.getIdentificatie(), historieRegels);
-        checkAttribuut("verzenddatum", oud.getVerzenddatum(), nieuw.getVerzenddatum(), historieRegels);
-        checkAttribuut("ingangsdatum", oud.getIngangsdatum(), nieuw.getIngangsdatum(), historieRegels);
-        checkAttribuut("vervaldatum", oud.getVervaldatum(), nieuw.getVervaldatum(), historieRegels);
-        checkAttribuut("toelichting", oud.getToelichting(), nieuw.getToelichting(), historieRegels);
-        return historieRegels.stream();
+        val historieRegels: List<RESTHistorieRegel?> = LinkedList()
+        checkAttribuut("identificatie", oud.identificatie, nieuw.identificatie, historieRegels)
+        checkAttribuut("verzenddatum", oud.verzenddatum, nieuw.verzenddatum, historieRegels)
+        checkAttribuut("ingangsdatum", oud.ingangsdatum, nieuw.ingangsdatum, historieRegels)
+        checkAttribuut("vervaldatum", oud.vervaldatum, nieuw.vervaldatum, historieRegels)
+        checkAttribuut("toelichting", oud.toelichting, nieuw.toelichting, historieRegels)
+        return historieRegels.stream()
     }
 
-    private String toWaarde(final Besluit besluit) {
-        return besluit != null ? besluit.getIdentificatie() : null;
+    private fun toWaarde(besluit: Besluit?): String? {
+        return besluit?.identificatie
     }
 }
