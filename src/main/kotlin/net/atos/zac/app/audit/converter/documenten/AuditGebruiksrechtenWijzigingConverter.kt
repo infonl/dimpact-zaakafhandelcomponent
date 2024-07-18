d931ca459ff28d4392fd5e65b229819292d428e8
Hristo Iliev <hristo.iliev@info.nl>
2024-07-18 14:27:06 +0300

    feature: simplify audit wijziging converter class hierarchy
    Remove converter hierarchy
    
    Solves: PZ-3261

--- src/main/kotlin/net/atos/zac/app/audit/converter/documenten/AuditGebruiksrechtenWijzigingConverter.kt
+++ src/main/kotlin/net/atos/zac/app/audit/converter/documenten/AuditGebruiksrechtenWijzigingConverter.kt
@@ -2,19 +2,14 @@
  * SPDX-FileCopyrightText: 2021 Atos, 2024 Lifely
  * SPDX-License-Identifier: EUPL-1.2+
  */
-
 package net.atos.zac.app.audit.converter.documenten
 
 import net.atos.client.zgw.drc.model.generated.Gebruiksrechten
-import net.atos.client.zgw.shared.model.ObjectType
-import net.atos.client.zgw.shared.model.audit.documenten.GebuiksrechtenWijziging
-import net.atos.zac.app.audit.converter.AbstractAuditWijzigingConverter
+import net.atos.client.zgw.shared.model.audit.AuditWijziging
 import net.atos.zac.app.audit.model.RESTHistorieRegel
 
-class AuditGebruiksrechtenWijzigingConverter : AbstractAuditWijzigingConverter<GebuiksrechtenWijziging>() {
-    override fun supports(objectType: ObjectType): Boolean = ObjectType.GEBRUIKSRECHTEN == objectType
-
-    override fun doConvert(wijziging: GebuiksrechtenWijziging): List<RESTHistorieRegel> =
+object AuditGebruiksrechtenWijzigingConverter {
+    fun convert(wijziging: AuditWijziging<Gebruiksrechten>): List<RESTHistorieRegel> =
         listOf(
             RESTHistorieRegel(
                 "indicatieGebruiksrecht",
