adc7e9e8462c417e2def5a4d53afd14cba1d8053
Edgar Vonk <edgar@info.nl>
2024-07-24 16:43:42 +0200

    chore: renamed code; added integration tests for customer contact moments

--- src/main/java/net/atos/zac/app/contactmoment/model/RESTListContactmomentenParameters.java
+++ src/main/java/net/atos/zac/app/contactmoment/model/RESTListContactmomentenParameters.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: EUPL-1.2+
  */
 
-package net.atos.zac.app.contactmomenten.model;
+package net.atos.zac.app.contactmoment.model;
 
 public class RESTListContactmomentenParameters {
 
