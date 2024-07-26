15fd36e7a2ebc438a934f7da338eaa8ef800522d
Edgar Vonk <edgar@info.nl>
2024-07-26 16:34:25 +0200

    chore: convert ReferenceTableValue.java to Kotlin

--- src/main/kotlin/net/atos/zac/admin/model/ReferenceTableValue.kt
+++ src/main/kotlin/net/atos/zac/admin/model/ReferenceTableValue.kt
@@ -14,7 +14,6 @@ import jakarta.persistence.ManyToOne
 import jakarta.persistence.SequenceGenerator
 import jakarta.persistence.Table
 import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.NotNull
 import net.atos.zac.util.FlywayIntegrator
 import nl.lifely.zac.util.AllOpen
 
