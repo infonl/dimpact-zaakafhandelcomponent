0d9612f5ade80880c5e5120cc9497557cd77f2aa
Edgar Vonk <edgar@info.nl>
2024-08-14 15:48:41 +0200

    chore: cleaned up code

--- src/main/kotlin/net/atos/zac/identity/model/Group.kt
+++ src/main/kotlin/net/atos/zac/identity/model/Group.kt
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2022 Atos, 2024 Lifely
+ * SPDX-FileCopyrightText: 2024 Lifely
  * SPDX-License-Identifier: EUPL-1.2+
  */
 package net.atos.zac.identity.model
@@ -7,12 +7,11 @@ package net.atos.zac.identity.model
 data class Group(
     val id: String,
 
-    // TODO: non-nullable?
-    val name: String? = null,
+    val name: String,
 
     val email: String? = null
 ) {
-/**
+    /**
      * Constructor for creating an unknown Group, a group with a given group id which is not known in the identity system.
      *
      * @param id ID of the group which is unknown
@@ -22,10 +21,3 @@ data class Group(
         name = id
     )
 }
-//
-//    constructor(id: String, name: String?, email: String?) {
-//        this.id = id
-//        this.name = name ?: id
-//        this.email = email
-//    }
-// }
