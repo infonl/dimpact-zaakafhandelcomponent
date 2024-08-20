ef498643e73d36c8e4fa16cd1e78f1f708a8322b
Hristo Iliev <hristo.iliev@info.nl>
2024-08-20 16:13:51 +0300

    fix: base64 decoded string length

--- src/main/kotlin/net/atos/client/zgw/drc/DrcClientUtil.kt
+++ src/main/kotlin/net/atos/client/zgw/drc/DrcClientUtil.kt
@@ -4,7 +4,7 @@ import net.atos.client.zgw.drc.model.generated.EnkelvoudigInformatieObject
 import net.atos.client.zgw.drc.model.generated.SoortEnum
 import java.util.Base64
 
-private const val BASE64_PADDING_CHARACTER = "="
+private const val BASE64_PADDING_CHARACTER = '='
 private const val NUMBER_OF_OCTETS = 3
 private const val NUMBER_OF_SEXTETS = 4
 
@@ -21,7 +21,7 @@ fun ByteArray.toBase64String(): String {
  * Returns the length of the original (base64 decoded) string
  */
 fun String.decodedBase64StringLength(): Int {
-    return substringBeforeLast(BASE64_PADDING_CHARACTER).length * NUMBER_OF_OCTETS / NUMBER_OF_SEXTETS
+    return trimEnd(BASE64_PADDING_CHARACTER).length * NUMBER_OF_OCTETS / NUMBER_OF_SEXTETS
 }
 
 /**
---
+++ src/test/kotlin/net/atos/client/zgw/drc/DrcClientUtilKtTest.kt
@@ -0,0 +1,28 @@
+package net.atos.client.zgw.drc
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.checkUnnecessaryStub
+import java.util.Base64
+
+class DrcClientUtilKtTest : BehaviorSpec({
+
+    beforeEach {
+        checkUnnecessaryStub()
+    }
+
+    Given("a base64 encoded string") {
+        val testDataThatRequiresPadding = "testData12"
+        val base64encoded = Base64.getEncoder().encodeToString(testDataThatRequiresPadding.toByteArray())
+
+        When("length of decoded string is requested") {
+            val length = base64encoded.decodedBase64StringLength()
+
+            Then("length is correct") {
+                length shouldBe testDataThatRequiresPadding.length
+            }
+        }
+    }
+
+
+})
