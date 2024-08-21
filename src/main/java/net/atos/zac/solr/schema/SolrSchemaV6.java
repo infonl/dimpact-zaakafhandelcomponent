35c79c3ad81ec152c06e1072b783413de824d93d
Edgar Vonk <edgar@info.nl>
2024-08-21 17:20:34 +0200

    fix: index zaak vertrouwelijkheidaanduiding in uppercase (=enum name) because the frontend expects this in the translations

---
+++ src/main/java/net/atos/zac/solr/schema/SolrSchemaV6.java
@@ -0,0 +1,35 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Atos
+ * SPDX-License-Identifier: EUPL-1.2+
+ */
+
+package net.atos.zac.solr.schema;
+
+import net.atos.zac.solr.SolrSchemaUpdate;
+import net.atos.zac.zoeken.model.index.ZoekObjectType;
+import org.apache.solr.client.solrj.request.schema.SchemaRequest;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import static net.atos.zac.solr.FieldType.STRING;
+import static net.atos.zac.solr.SolrSchemaUpdateHelper.addFieldMultiValued;
+
+class SolrSchemaV6 implements SolrSchemaUpdate {
+
+    @Override
+    public int getVersie() {
+        return 6;
+    }
+
+    @Override
+    public Set<ZoekObjectType> getTeHerindexerenZoekObjectTypes() {
+        return Set.of(ZoekObjectType.ZAAK);
+    }
+
+    @Override
+    public List<SchemaRequest.Update> getSchemaUpdates() {
+        return Collections.emptyList();
+    }
+}
