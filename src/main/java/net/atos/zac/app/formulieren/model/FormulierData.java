76f66f7b15587c4aa746ea09789cdb4b44678144
Andy Verberne <andy.verberne@atos.net>
2024-09-01 10:58:30 +0200

    PZ-3455-POC-BPMN-proces - Fix null pointer in FomrmulierData

--- src/main/java/net/atos/zac/app/formulieren/model/FormulierData.java
+++ src/main/java/net/atos/zac/app/formulieren/model/FormulierData.java
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Atos
+ * SPDX-License-Identifier: EUPL-1.2+
+ */
+
 package net.atos.zac.app.formulieren.model;
 
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
@@ -56,53 +61,53 @@ public class FormulierData {
 
     public FormulierData(final Map<String, Object> taakData) {
         this.zaakVariabelen = new HashMap<>(taakData);
-        if (taakData.containsKey(ZAAK_OPSCHORTEN)) {
+        if (taakData.containsKey(ZAAK_OPSCHORTEN) && taakData.get(ZAAK_OPSCHORTEN) != null) {
             if (BooleanUtils.TRUE.equals(taakData.get(ZAAK_OPSCHORTEN))) {
                 this.zaakOpschorten = true;
             }
             zaakVariabelen.remove(ZAAK_OPSCHORTEN);
         }
-        if (taakData.containsKey(ZAAK_HERVATTEN)) {
+        if (taakData.containsKey(ZAAK_HERVATTEN) && taakData.get(ZAAK_HERVATTEN) != null) {
             if (BooleanUtils.TRUE.equals(taakData.get(ZAAK_HERVATTEN))) {
                 this.zaakHervatten = true;
             }
             zaakVariabelen.remove(ZAAK_HERVATTEN);
         }
-        if (taakData.containsKey(TAAK_FATALE_DATUM)) {
+        if (taakData.containsKey(TAAK_FATALE_DATUM) && taakData.get(TAAK_FATALE_DATUM) != null) {
             this.taakFataleDatum = new LocalDateAdapter().adaptFromJson(taakData.get(TAAK_FATALE_DATUM).toString());
             zaakVariabelen.remove(TAAK_FATALE_DATUM);
         }
-        if (taakData.containsKey(TOELICHTING)) {
+        if (taakData.containsKey(TOELICHTING) && taakData.get(TOELICHTING) != null) {
             this.toelichting = taakData.get(TOELICHTING).toString();
             zaakVariabelen.remove(TOELICHTING);
         }
-        if (taakData.containsKey(TAAK_TOEKENNEN_GROEP)) {
+        if (taakData.containsKey(TAAK_TOEKENNEN_GROEP) && taakData.get(TAAK_TOEKENNEN_GROEP) != null) {
             this.taakToekennenGroep = taakData.get(TAAK_TOEKENNEN_GROEP).toString();
             zaakVariabelen.remove(TAAK_TOEKENNEN_GROEP);
         }
-        if (taakData.containsKey(TAAK_TOEKENNEN_MEDEWERKER)) {
+        if (taakData.containsKey(TAAK_TOEKENNEN_MEDEWERKER) && taakData.get(TAAK_TOEKENNEN_MEDEWERKER) != null) {
             this.taakToekennenMedewerker = taakData.get(TAAK_TOEKENNEN_MEDEWERKER).toString();
             zaakVariabelen.remove(TAAK_TOEKENNEN_MEDEWERKER);
         }
-        if (taakData.containsKey(MAIL_BIJLAGEN)) {
+        if (taakData.containsKey(MAIL_BIJLAGEN) && taakData.get(MAIL_BIJLAGEN) != null) {
             if (isNotBlank(taakData.get(MAIL_BIJLAGEN).toString())) {
                 this.mailBijlagen = taakData.get(MAIL_BIJLAGEN).toString();
             }
             zaakVariabelen.remove(MAIL_BIJLAGEN);
         }
-        if (taakData.containsKey(DOCUMENTEN_VERZENDEN)) {
+        if (taakData.containsKey(DOCUMENTEN_VERZENDEN) && taakData.get(DOCUMENTEN_VERZENDEN) != null) {
             if (isNotBlank(taakData.get(DOCUMENTEN_VERZENDEN).toString())) {
                 this.documentenVerzenden = taakData.get(DOCUMENTEN_VERZENDEN).toString();
             }
             zaakVariabelen.remove(DOCUMENTEN_VERZENDEN);
         }
-        if (taakData.containsKey(DOCUMENTEN_VERZENDEN_DATUM)) {
+        if (taakData.containsKey(DOCUMENTEN_VERZENDEN_DATUM) && taakData.get(DOCUMENTEN_VERZENDEN_DATUM) != null) {
             this.documentenVerzendenDatum = new LocalDateAdapter().adaptFromJson(taakData.get(DOCUMENTEN_VERZENDEN_DATUM).toString());
             zaakVariabelen.remove(DOCUMENTEN_VERZENDEN_DATUM);
         } else {
             this.documentenVerzendenDatum = LocalDate.now();
         }
-        if (taakData.containsKey(DOCUMENTEN_ONDERTEKENEN)) {
+        if (taakData.containsKey(DOCUMENTEN_ONDERTEKENEN) && taakData.get(DOCUMENTEN_ONDERTEKENEN) != null) {
             if (isNotBlank(taakData.get(DOCUMENTEN_ONDERTEKENEN).toString())) {
                 this.documentenOndertekenen = taakData.get(DOCUMENTEN_ONDERTEKENEN).toString();
             }
