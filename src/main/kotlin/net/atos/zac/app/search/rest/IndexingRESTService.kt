0a4ef4888cfb0487e50bf43ccf0efbdf7fbe25fa
Hristo Iliev <hristo.iliev@info.nl>
2024-09-02 19:00:12 +0300

    chore: move IndexingRestService to search package

--- src/main/kotlin/net/atos/zac/app/search/rest/IndexingRESTService.kt
+++ src/main/kotlin/net/atos/zac/app/search/rest/IndexingRESTService.kt
@@ -2,7 +2,7 @@
  * SPDX-FileCopyrightText: 2022 Atos
  * SPDX-License-Identifier: EUPL-1.2+
  */
-package net.atos.zac.util
+package net.atos.zac.app.search.rest
 
 import jakarta.inject.Inject
 import jakarta.inject.Singleton
