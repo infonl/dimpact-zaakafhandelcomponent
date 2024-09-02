4ad3e9f094be43c06486fcecd8823b6727786760
Edgar Vonk <edgar@info.nl>
2024-09-02 11:41:16 +0200

    feat: fixed accidentally broken findActiveZaakafhandelParametersForProductaanvraagtype SQL query

--- src/main/java/net/atos/zac/admin/model/ZaakafhandelParameters.java
+++ src/main/java/net/atos/zac/admin/model/ZaakafhandelParameters.java
@@ -37,10 +37,11 @@ import org.apache.commons.lang3.StringUtils;
 @NamedNativeQuery(
         name = ZaakafhandelParameters.FIND_ACTIVE_ZAAKAFHANDELPARAMETERS_FOR_PRODUCTAANVRAAGTYPE_QUERY, query = "SELECT * FROM zaakafhandelcomponent.zaakafhandelparameters z " +
                                                                                                                 "INNER JOIN ( " +
-                                                                                                                "  SELECT z_inner.zaaktype_omschrijving AS inner_zaaktype_omschrijving, MAX(z_inner.creatiedatum) " +
+                                                                                                                "  SELECT z_inner.zaaktype_omschrijving AS inner_zaaktype_omschrijving, MAX(z_inner.creatiedatum) as max_creatiedatum " +
                                                                                                                 "  FROM zaakafhandelcomponent.zaakafhandelparameters z_inner " +
                                                                                                                 "  GROUP BY inner_zaaktype_omschrijving " +
                                                                                                                 ") recent_zaaktypes ON z.zaaktype_omschrijving = recent_zaaktypes.inner_zaaktype_omschrijving " +
+                                                                                                                " AND z.creatiedatum = recent_zaaktypes.max_creatiedatum " +
                                                                                                                 "WHERE z.productaanvraagtype = :productaanvraagtype ", resultClass = ZaakafhandelParameters.class
 )
 public class ZaakafhandelParameters {
