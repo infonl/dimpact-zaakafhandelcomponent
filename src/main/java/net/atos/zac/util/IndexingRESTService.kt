314f9a59e36ac3cbae4e47f37cd93f668faedf64
Hristo Iliev <hristo.iliev@info.nl>
2024-09-02 18:57:42 +0300

    feature: convert IndexingRESTService to Kotlin

--- src/main/java/net/atos/zac/util/IndexingRESTService.kt
+++ src/main/java/net/atos/zac/util/IndexingRESTService.kt
@@ -2,38 +2,40 @@
  * SPDX-FileCopyrightText: 2022 Atos
  * SPDX-License-Identifier: EUPL-1.2+
  */
+package net.atos.zac.util
 
-package net.atos.zac.util;
+import jakarta.inject.Inject
+import jakarta.inject.Singleton
+import jakarta.ws.rs.Consumes
+import jakarta.ws.rs.GET
+import jakarta.ws.rs.POST
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.PathParam
+import jakarta.ws.rs.Produces
+import jakarta.ws.rs.core.MediaType
+import net.atos.zac.zoeken.IndexingService
+import net.atos.zac.zoeken.model.index.ZoekObjectType
+import nl.lifely.zac.util.AllOpen
+import nl.lifely.zac.util.NoArgConstructor
 
-import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
-
-import net.atos.zac.zoeken.IndexingService;
-import net.atos.zac.zoeken.model.index.ZoekObjectType;
-
+@Singleton
 @Path("indexeren")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
-public class IndexingRESTService {
-
-    @Inject
-    private IndexingService indexingService;
-
+@NoArgConstructor
+@AllOpen
+class IndexingRESTService @Inject constructor(
+    private val indexingService: IndexingService
+) {
     @GET
     @Path("herindexeren/{type}")
-    public void reindex(@PathParam("type") ZoekObjectType type) {
-        indexingService.reindex(type);
+    fun reindex(@PathParam("type") type: ZoekObjectType) {
+        indexingService.reindex(type)
     }
 
     @POST
     @Path("commit-pending-changes-to-search-index")
-    public void commit() {
-        indexingService.commit();
+    fun commit() {
+        indexingService.commit()
     }
 }
