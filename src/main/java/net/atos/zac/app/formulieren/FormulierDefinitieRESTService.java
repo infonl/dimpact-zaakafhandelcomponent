3b23410365e6e7987412b369af596c51af7a16b7
Andy Verberne <andy.verberne@atos.net>
2024-09-12 13:18:58 +0200

    fix: poc bpmn proces - fixed some small issues (#1715)
    poc bpmn proces - fixed some small issues
    
    Solves PZ-3455

--- src/main/java/net/atos/zac/app/formulieren/FormulierDefinitieRESTService.java
+++ src/main/java/net/atos/zac/app/formulieren/FormulierDefinitieRESTService.java
@@ -11,14 +11,7 @@ import java.util.List;
 
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.DELETE;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.PUT;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
 import net.atos.zac.app.formulieren.converter.RESTFormulierDefinitieConverter;
