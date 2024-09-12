94f93995ae6450003b3b09970f14494625c9be84
Andy Verberne <andy.verberne@atos.net>
2024-09-12 08:04:13 +0200

    feat: PZ-3455-POC-BPMN-proces - Resolved comments - fixed BPMN formulier definities menu item

--- src/main/java/net/atos/zac/app/formulieren/FormulierDefinitieRESTService.java
+++ src/main/java/net/atos/zac/app/formulieren/FormulierDefinitieRESTService.java
@@ -11,14 +11,7 @@ import java.util.List;
 
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.DELETE;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.PUT;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
 import net.atos.zac.app.formulieren.converter.RESTFormulierDefinitieConverter;
@@ -27,7 +20,7 @@ import net.atos.zac.formulieren.FormulierDefinitieService;
 import net.atos.zac.policy.PolicyService;
 
 @Singleton
-@Path("formulierDefinities")
+@Path("formulierdefinities")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class FormulierDefinitieRESTService {
