72e0a852834d66757161dee099347da43c4d1f1b
infonl-marcel <82890006+infonl-marcel@users.noreply.github.com>
2024-10-03 14:48:55 +0200

    feat: betrokkene and initiator koppelen - using communications service between forms to clear data after selection (#1801)
    Betrokkene and initiator koppelen, using communications service between
    forms to clear data after selection
    
    Solves: PZ-4065

---
+++ src/main/app/src/app/klanten/zoek/form-communicatie-service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+
+@Injectable({
+  providedIn: "root",
+})
+export class FormCommunicatieService {
+  private itemSelectedSource = new BehaviorSubject<{
+    selected: boolean;
+    uuid: string | null;
+  }>({ selected: false, uuid: null });
+  itemSelected$ = this.itemSelectedSource.asObservable();
+
+  notifyItemSelected(uuid: string) {
+    this.itemSelectedSource.next({ selected: true, uuid });
+  }
+}
