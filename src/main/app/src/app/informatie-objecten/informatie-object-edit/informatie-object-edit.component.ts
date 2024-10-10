03472025ae0bf4da7c3fcf69ad7339e072f14c37
infonl-marcel <marcele@info.nl>
2024-10-10 20:01:52 +0200

    feat: lean 2

--- src/main/app/src/app/informatie-objecten/informatie-object-edit/informatie-object-edit.component.ts
+++ src/main/app/src/app/informatie-objecten/informatie-object-edit/informatie-object-edit.component.ts
@@ -31,9 +31,7 @@ import { FormComponent } from "../../shared/material-form-builder/form/form/form
 import { AbstractFormField } from "../../shared/material-form-builder/model/abstract-form-field";
 import { FormConfig } from "../../shared/material-form-builder/model/form-config";
 import { FormConfigBuilder } from "../../shared/material-form-builder/model/form-config-builder";
-import { NavigationService } from "../../shared/navigation/navigation.service";
 import { OrderUtil } from "../../shared/order/order-util";
-import { ZakenService } from "../../zaken/zaken.service";
 import { InformatieObjectenService } from "../informatie-objecten.service";
 import { EnkelvoudigInformatieObjectVersieGegevens } from "../model/enkelvoudig-informatie-object-versie-gegevens";
 import { EnkelvoudigInformatieobject } from "../model/enkelvoudig-informatieobject";
