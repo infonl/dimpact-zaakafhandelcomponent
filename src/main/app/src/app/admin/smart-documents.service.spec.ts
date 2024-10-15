37f308bde1b7a4b8e3c1dc2dd91ffbb4934a78b5
infonl-marcel <82890006+infonl-marcel@users.noreply.github.com>
2024-10-15 08:12:53 +0200

    feat: FE unit test flattening the documents template object (#1830)
    FE unit test flattening the documents template Object, and a minor form
    update removingrequired from disabled fields.
    
    Solves: PZ-4134
    
    ---------
    
    Co-authored-by: infojohn <john@info.nl>

---
+++ src/main/app/src/app/admin/smart-documents.service.spec.ts
@@ -0,0 +1,145 @@
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { TestBed } from "@angular/core/testing";
+import { TranslateService } from "@ngx-translate/core";
+import { UtilService } from "../core/service/util.service";
+import { FoutAfhandelingService } from "../fout-afhandeling/fout-afhandeling.service";
+import { SmartDocumentsService } from "./smart-documents.service";
+
+describe("SmartDocumentsService", () => {
+  let smartDocumentsService: SmartDocumentsService;
+
+  // Mock the dependencies
+  const mockTranslateService = {
+    translate: jest.fn(),
+  };
+  const mockUtilService = {
+    someUtilFunction: jest.fn(),
+  };
+  const mockFoutAfhandelingService = {
+    handleError: jest.fn(),
+  };
+
+  const objInput = {
+    id: "id-group-level-root",
+    name: "Dimpact",
+    groups: [
+      {
+        id: "id-group-level-1",
+        name: "level1 group",
+        groups: [
+          {
+            id: "id-group-level-2",
+            name: "level2 group",
+            groups: [
+              {
+                id: "id-group-level-3",
+                name: "level3 group",
+                groups: [],
+                templates: [
+                  {
+                    id: "id-template-level-3",
+                    name: "level 1 template",
+                    informatieObjectTypeUUID: "info-object-type-id-level-3",
+                  },
+                ],
+              },
+            ],
+            templates: [
+              {
+                id: "id-template-level-2",
+                name: "level 1 template",
+                informatieObjectTypeUUID: "info-object-type-id-level-2",
+              },
+            ],
+          },
+        ],
+        templates: [
+          {
+            id: "id-template-level-1",
+            name: "level 1 template",
+            informatieObjectTypeUUID: "info-object-type-id-level-1",
+          },
+        ],
+      },
+    ],
+    templates: [
+      {
+        id: "id-template-level-root",
+        name: "root level template",
+        informatieObjectTypeUUID: "info-object-type-id-level-3",
+      },
+    ],
+  };
+
+  // Expected flattened output
+  const expectedOutput = [
+    {
+      id: "id-group-level-root",
+      name: "Dimpact",
+      templates: [
+        {
+          id: "id-template-level-root",
+          name: "root level template",
+          informatieObjectTypeUUID: "info-object-type-id-level-3",
+        },
+      ],
+    },
+    {
+      id: "id-group-level-1",
+      name: "level1 group",
+      templates: [
+        {
+          id: "id-template-level-1",
+          name: "level 1 template",
+          informatieObjectTypeUUID: "info-object-type-id-level-1",
+        },
+      ],
+    },
+    {
+      id: "id-group-level-2",
+      name: "level2 group",
+      templates: [
+        {
+          id: "id-template-level-2",
+          name: "level 1 template",
+          informatieObjectTypeUUID: "info-object-type-id-level-2",
+        },
+      ],
+    },
+    {
+      id: "id-group-level-3",
+      name: "level3 group",
+      templates: [
+        {
+          id: "id-template-level-3",
+          name: "level 1 template",
+          informatieObjectTypeUUID: "info-object-type-id-level-3",
+        },
+      ],
+    },
+  ];
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        SmartDocumentsService,
+        {
+          provide: FoutAfhandelingService,
+          useValue: mockFoutAfhandelingService,
+        },
+        { provide: UtilService, useValue: mockUtilService },
+        { provide: TranslateService, useValue: mockTranslateService },
+      ],
+    });
+
+    smartDocumentsService = TestBed.inject(SmartDocumentsService);
+  });
+
+  it("should flatten the document groups and return expected output", () => {
+    const result =
+      smartDocumentsService.flattenDocumentsTemplateGroup(objInput);
+
+    expect(result).toEqual(expectedOutput);
+  });
+});
