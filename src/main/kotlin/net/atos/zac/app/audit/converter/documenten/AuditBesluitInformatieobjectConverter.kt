178e44260688d4cdc67c4299e0310dafe3eb14f0
Hristo Iliev <hristo.iliev@info.nl>
2024-10-16 22:42:17 +0300

    feature: history converter for decision with file
    Add converter for besluit info objects
    Add unit test
    
    Solves: PZ-3864

---
+++ src/main/kotlin/net/atos/zac/app/audit/converter/documenten/AuditBesluitInformatieobjectConverter.kt
@@ -0,0 +1,30 @@
+/*
+ * SPDX-FileCopyrightText: 2021 Atos, 2024 Lifely
+ * SPDX-License-Identifier: EUPL-1.2+
+ */
+
+package net.atos.zac.app.audit.converter.documenten
+
+import jakarta.inject.Inject
+import net.atos.client.or.shared.util.URIUtil
+import net.atos.client.zgw.brc.BrcClientService
+import net.atos.client.zgw.brc.model.generated.BesluitInformatieObject
+import net.atos.client.zgw.shared.model.audit.besluiten.BesluitInformatieobjectWijziging
+import net.atos.zac.app.audit.model.RESTHistorieRegel
+
+class AuditBesluitInformatieobjectConverter @Inject constructor(
+    private val brcClientService: BrcClientService
+) {
+
+    fun convert(wijziging: BesluitInformatieobjectWijziging): List<RESTHistorieRegel> {
+        val oud = wijziging.oud
+        val nieuw = wijziging.nieuw
+
+        return listOf(RESTHistorieRegel("informatieobject", toWaarde(oud), toWaarde(nieuw)))
+    }
+
+    private fun toWaarde(besluitInformatieObject: BesluitInformatieObject?): String? =
+        besluitInformatieObject?.let {
+            brcClientService.readBesluit(URIUtil.getUUID(it.besluit)).identificatie
+        }
+}
