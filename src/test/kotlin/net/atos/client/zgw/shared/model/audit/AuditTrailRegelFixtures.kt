68365c287db537bc745cadb181141aff8fbd5038
Hristo Iliev <hsiliev@gmail.com>
2024-10-21 10:53:08 +0300

    feature: history converter for decision with file (#1887)
    Add converter for besluit info objects
    Add unit test
    
    Solves: PZ-3864
    
    ---------
    
    Co-authored-by: Hristo Iliev <hristo.iliev@info.nl>
    Co-authored-by: Edgar Vonk <edgar@info.nl>

--- src/test/kotlin/net/atos/client/zgw/shared/model/audit/AuditTrailRegelFixtures.kt
+++ src/test/kotlin/net/atos/client/zgw/shared/model/audit/AuditTrailRegelFixtures.kt
@@ -7,6 +7,45 @@ import java.time.ZonedDateTime
 import java.util.UUID
 
 @Suppress("LongParameterList")
+fun <T> createAuditTrailRegel(
+    uri: URI = URI("https://example.com/resource"),
+    uuid: UUID = UUID.randomUUID(),
+    bron: Bron,
+    applicatieId: String = UUID.randomUUID().toString(),
+    applicatieWeergave: String = "ZAC",
+    gebruikersId: String = "testuser",
+    gebruikersWeergave: String = "Test User",
+    actie: String,
+    actieWeergave: String,
+    resultaat: Int,
+    hoofdObject: URI,
+    resource: String,
+    resourceUrl: URI,
+    toelichting: String,
+    resourceWeergave: String = "123443210 - ZAAK-2024-0000000003",
+    aanmaakdatum: ZonedDateTime = ZonedDateTime.now(),
+    wijzigingen: AuditWijziging<T>
+) = AuditTrailRegel().apply {
+    url = uri
+    this.uuid = uuid
+    this.bron = bron
+    this.applicatieId = applicatieId
+    this.applicatieWeergave = applicatieWeergave
+    this.gebruikersId = gebruikersId
+    this.gebruikersWeergave = gebruikersWeergave
+    this.actie = actie
+    this.actieWeergave = actieWeergave
+    this.resultaat = resultaat
+    this.hoofdObject = hoofdObject
+    this.resource = resource
+    this.resourceUrl = resourceUrl
+    this.toelichting = toelichting
+    this.resourceWeergave = resourceWeergave
+    this.aanmaakdatum = aanmaakdatum
+    this.wijzigingen = wijzigingen
+}
+
+@Suppress("LongParameterList")
 fun createZRCAuditTrailRegel(
     uri: URI = URI("https://example.com/resource"),
     uuid: UUID = UUID.randomUUID(),
