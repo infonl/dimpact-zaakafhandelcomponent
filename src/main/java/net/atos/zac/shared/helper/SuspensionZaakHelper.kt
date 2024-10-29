7d894256bb65ed71e6a4426a567509b7204894c6
Hristo Iliev <hristo.iliev@info.nl>
2024-10-29 16:34:20 +0200

    feature: convert to Kotlin

--- src/main/java/net/atos/zac/shared/helper/SuspensionZaakHelper.kt
+++ src/main/java/net/atos/zac/shared/helper/SuspensionZaakHelper.kt
@@ -1,132 +1,127 @@
-package net.atos.zac.shared.helper;
+package net.atos.zac.shared.helper
 
-import static net.atos.zac.policy.PolicyService.assertPolicy;
+import jakarta.inject.Inject
+import net.atos.client.zgw.zrc.ZrcClientService
+import net.atos.client.zgw.zrc.model.Zaak
+import net.atos.client.zgw.zrc.model.generated.Opschorting
+import net.atos.zac.flowable.ZaakVariabelenService
+import net.atos.zac.policy.PolicyService
+import org.apache.commons.lang3.StringUtils
+import java.time.LocalDate
+import java.time.ZonedDateTime
+import java.time.temporal.ChronoUnit
 
-import java.time.LocalDate;
-import java.time.ZonedDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.UUID;
-
-import jakarta.inject.Inject;
-
-import org.apache.commons.lang3.StringUtils;
-
-import net.atos.client.zgw.zrc.ZrcClientService;
-import net.atos.client.zgw.zrc.model.Zaak;
-import net.atos.client.zgw.zrc.model.generated.Opschorting;
-import net.atos.zac.flowable.ZaakVariabelenService;
-import net.atos.zac.policy.PolicyService;
-
-public class SuspensionZaakHelper {
-    private static final String SUSPENSION = "Opschorting";
-    private static final String RESUMING = "Hervatting";
-
-    private PolicyService policyService;
-    private ZrcClientService zrcClientService;
-    private ZaakVariabelenService zaakVariabelenService;
+class SuspensionZaakHelper {
+    private var policyService: PolicyService? = null
+    private var zrcClientService: ZrcClientService? = null
+    private var zaakVariabelenService: ZaakVariabelenService? = null
 
     /**
      * Default no-arg constructor, required by Weld.
      */
-    public SuspensionZaakHelper() {
-    }
+    constructor()
 
     @Inject
-    SuspensionZaakHelper(
-            final PolicyService policyService,
-            final ZrcClientService zrcClientService,
-            final ZaakVariabelenService zaakVariabelenService
+    internal constructor(
+        policyService: PolicyService?,
+        zrcClientService: ZrcClientService?,
+        zaakVariabelenService: ZaakVariabelenService?
     ) {
-        this.policyService = policyService;
-        this.zrcClientService = zrcClientService;
-        this.zaakVariabelenService = zaakVariabelenService;
+        this.policyService = policyService
+        this.zrcClientService = zrcClientService
+        this.zaakVariabelenService = zaakVariabelenService
     }
 
-    public Zaak suspendZaak(Zaak zaak, final long numberOfDays, final String suspensionReason) {
-        assertPolicy(policyService.readZaakRechten(zaak).opschorten());
-        assertPolicy(StringUtils.isEmpty(zaak.getOpschorting().getReden()));
-        final UUID zaakUUID = zaak.getUuid();
-        final String toelichting = String.format("%s: %s", SUSPENSION, suspensionReason);
-        LocalDate einddatumGepland = null;
-        if (zaak.getEinddatumGepland() != null) {
-            einddatumGepland = zaak.getEinddatumGepland().plusDays(numberOfDays);
+    fun suspendZaak(zaak: Zaak, numberOfDays: Long, suspensionReason: String?): Zaak {
+        PolicyService.assertPolicy(policyService!!.readZaakRechten(zaak).opschorten)
+        PolicyService.assertPolicy(StringUtils.isEmpty(zaak.opschorting.reden))
+        val zaakUUID = zaak.uuid
+        val toelichting = String.format("%s: %s", SUSPENSION, suspensionReason)
+        var einddatumGepland: LocalDate? = null
+        if (zaak.einddatumGepland != null) {
+            einddatumGepland = zaak.einddatumGepland.plusDays(numberOfDays)
         }
-        final LocalDate uiterlijkeEinddatumAfdoening = zaak.getUiterlijkeEinddatumAfdoening().plusDays(numberOfDays);
-        final var patchZaak = addSuspensionToZaakPatch(
-                createZaakPatch(einddatumGepland, uiterlijkeEinddatumAfdoening),
-                suspensionReason,
-                true
-        );
+        val uiterlijkeEinddatumAfdoening = zaak.uiterlijkeEinddatumAfdoening.plusDays(numberOfDays)
+        val patchZaak = addSuspensionToZaakPatch(
+            createZaakPatch(einddatumGepland, uiterlijkeEinddatumAfdoening),
+            suspensionReason,
+            true
+        )
 
-        final Zaak updatedZaak = zrcClientService.patchZaak(zaakUUID, patchZaak, toelichting);
-        zaakVariabelenService.setDatumtijdOpgeschort(zaakUUID, ZonedDateTime.now());
-        zaakVariabelenService.setVerwachteDagenOpgeschort(zaakUUID, Math.toIntExact(numberOfDays));
-        return updatedZaak;
+        val updatedZaak = zrcClientService!!.patchZaak(zaakUUID, patchZaak, toelichting)
+        zaakVariabelenService!!.setDatumtijdOpgeschort(zaakUUID, ZonedDateTime.now())
+        zaakVariabelenService!!.setVerwachteDagenOpgeschort(zaakUUID, Math.toIntExact(numberOfDays))
+        return updatedZaak
     }
 
-    public Zaak resumeZaak(final Zaak zaak, final String resumeReason) {
-        assertPolicy(policyService.readZaakRechten(zaak).hervatten());
-        assertPolicy(zaak.isOpgeschort());
-        final UUID zaakUUID = zaak.getUuid();
-        final ZonedDateTime datumOpgeschort = zaakVariabelenService.findDatumtijdOpgeschort(zaak.getUuid()).orElseGet(ZonedDateTime::now);
-        final int verwachteDagenOpgeschort = zaakVariabelenService.findVerwachteDagenOpgeschort(zaak.getUuid()).orElse(0);
-        final long dagenVerschil = ChronoUnit.DAYS.between(datumOpgeschort, ZonedDateTime.now());
-        final long offset = dagenVerschil - verwachteDagenOpgeschort;
-        LocalDate einddatumGepland = null;
-        if (zaak.getEinddatumGepland() != null) {
-            einddatumGepland = zaak.getEinddatumGepland().plusDays(offset);
+    fun resumeZaak(zaak: Zaak, resumeReason: String?): Zaak {
+        PolicyService.assertPolicy(policyService!!.readZaakRechten(zaak).hervatten)
+        PolicyService.assertPolicy(zaak.isOpgeschort)
+        val zaakUUID = zaak.uuid
+        val datumOpgeschort =
+            zaakVariabelenService!!.findDatumtijdOpgeschort(zaak.uuid).orElseGet { ZonedDateTime.now() }
+        val verwachteDagenOpgeschort = zaakVariabelenService!!.findVerwachteDagenOpgeschort(zaak.uuid).orElse(0)
+        val dagenVerschil = ChronoUnit.DAYS.between(datumOpgeschort, ZonedDateTime.now())
+        val offset = dagenVerschil - verwachteDagenOpgeschort
+        var einddatumGepland: LocalDate? = null
+        if (zaak.einddatumGepland != null) {
+            einddatumGepland = zaak.einddatumGepland.plusDays(offset)
         }
-        final LocalDate uiterlijkeEinddatumAfdoening = zaak.getUiterlijkeEinddatumAfdoening().plusDays(offset);
+        val uiterlijkeEinddatumAfdoening = zaak.uiterlijkeEinddatumAfdoening.plusDays(offset)
 
-        final String toelichting = String.format("%s: %s", RESUMING, resumeReason);
-        final var patchZaak = addSuspensionToZaakPatch(
-                createZaakPatch(einddatumGepland, uiterlijkeEinddatumAfdoening),
-                resumeReason,
-                false
-        );
+        val toelichting = String.format("%s: %s", RESUMING, resumeReason)
+        val patchZaak = addSuspensionToZaakPatch(
+            createZaakPatch(einddatumGepland, uiterlijkeEinddatumAfdoening),
+            resumeReason,
+            false
+        )
 
-        final Zaak updatedZaak = zrcClientService.patchZaak(zaakUUID, patchZaak, toelichting);
-        zaakVariabelenService.removeDatumtijdOpgeschort(zaakUUID);
-        zaakVariabelenService.removeVerwachteDagenOpgeschort(zaakUUID);
-        return updatedZaak;
+        val updatedZaak = zrcClientService!!.patchZaak(zaakUUID, patchZaak, toelichting)
+        zaakVariabelenService!!.removeDatumtijdOpgeschort(zaakUUID)
+        zaakVariabelenService!!.removeVerwachteDagenOpgeschort(zaakUUID)
+        return updatedZaak
     }
 
-    public Zaak extendZaakFatalDate(final Zaak zaak, final long numberOfDays, final String description) {
-        var zaakRechten = policyService.readZaakRechten(zaak);
-        assertPolicy(zaakRechten.wijzigen() && zaakRechten.verlengenDoorlooptijd());
+    fun extendZaakFatalDate(zaak: Zaak, numberOfDays: Long, description: String?): Zaak {
+        val zaakRechten = policyService!!.readZaakRechten(zaak)
+        PolicyService.assertPolicy(zaakRechten.wijzigen && zaakRechten.verlengenDoorlooptijd)
 
-        final UUID zaakUUID = zaak.getUuid();
-        LocalDate endDatePlanned = null;
-        if (zaak.getEinddatumGepland() != null) {
-            endDatePlanned = zaak.getEinddatumGepland().plusDays(numberOfDays);
+        val zaakUUID = zaak.uuid
+        var endDatePlanned: LocalDate? = null
+        if (zaak.einddatumGepland != null) {
+            endDatePlanned = zaak.einddatumGepland.plusDays(numberOfDays)
         }
-        final LocalDate finalCompletionDate = zaak.getUiterlijkeEinddatumAfdoening().plusDays(numberOfDays);
+        val finalCompletionDate = zaak.uiterlijkeEinddatumAfdoening.plusDays(numberOfDays)
 
-        return zrcClientService.patchZaak(zaakUUID, createZaakPatch(endDatePlanned, finalCompletionDate), description);
+        return zrcClientService!!.patchZaak(zaakUUID, createZaakPatch(endDatePlanned, finalCompletionDate), description)
     }
 
-    private Zaak createZaakPatch(
-            final LocalDate endDatePlanned,
-            final LocalDate finalCompletionDate
-    ) {
-        final Zaak zaak = new Zaak();
+    private fun createZaakPatch(
+        endDatePlanned: LocalDate?,
+        finalCompletionDate: LocalDate
+    ): Zaak {
+        val zaak = Zaak()
         if (endDatePlanned != null) {
-            zaak.setEinddatumGepland(endDatePlanned);
+            zaak.einddatumGepland = endDatePlanned
         }
-        zaak.setUiterlijkeEinddatumAfdoening(finalCompletionDate);
-        return zaak;
+        zaak.uiterlijkeEinddatumAfdoening = finalCompletionDate
+        return zaak
     }
 
-    private Zaak addSuspensionToZaakPatch(
-            final Zaak zaak,
-            final String reason,
-            final boolean isOpschorting
-    ) {
-        final Opschorting opschorting = new Opschorting();
-        opschorting.setReden(reason);
-        opschorting.setIndicatie(isOpschorting);
-        zaak.setOpschorting(opschorting);
-        return zaak;
+    private fun addSuspensionToZaakPatch(
+        zaak: Zaak,
+        reason: String?,
+        isOpschorting: Boolean
+    ): Zaak {
+        val opschorting = Opschorting()
+        opschorting.reden = reason
+        opschorting.indicatie = isOpschorting
+        zaak.opschorting = opschorting
+        return zaak
     }
 
+    companion object {
+        private const val SUSPENSION = "Opschorting"
+        private const val RESUMING = "Hervatting"
+    }
 }
