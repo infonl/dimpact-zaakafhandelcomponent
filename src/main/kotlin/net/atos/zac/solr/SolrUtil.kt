fbfd89a7aec18a4264fe68b683474d30673f53f6
edgarvonk <edgar@info.nl>
2024-11-01 21:17:19 +0100

    chore: convert to Kotlin

--- src/main/kotlin/net/atos/zac/solr/SolrUtil.kt
+++ src/main/kotlin/net/atos/zac/solr/SolrUtil.kt
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: EUPL-1.2+
  */
 
-package net.atos.zac.zoeken
+package net.atos.zac.solr
 
 private const val SOLR_ESCAPE = '\\'
 private const val SOLR_QUOTE = '\"'
@@ -19,17 +19,17 @@ fun quoted(value: String) = SOLR_QUOTE.toString() + encoded(value) + SOLR_QUOTE
 /**
  * Produces an encoded Solr string from a raw Java string.
  *
- * @param waarde the raw unencoded string
+ * @param value the raw unencoded string
  * @return the encoded Solr string
  */
-fun encoded(waarde: String) = escape(escape(waarde, SOLR_ESCAPE), SOLR_QUOTE)
+fun encoded(value: String) = escape(escape(value, SOLR_ESCAPE), SOLR_QUOTE)
 
 /**
  * Replaces all occurrences of a given character with the correct Solr escape sequence.
  * N.B. Always start by escaping the escape character itself, only then escape any other characters.
  *
- * @param waarde the string that may contain the raw unescaped characters
+ * @param value the string that may contain the raw unescaped characters
  * @param c      the character that will be escaped
  * @return the string with the Solr escaped characters
  */
-fun escape(waarde: String, c: Char) = waarde.replace(c.toString(), SOLR_ESCAPE.toString() + c)
+fun escape(value: String, c: Char) = value.replace(c.toString(), SOLR_ESCAPE.toString() + c)
