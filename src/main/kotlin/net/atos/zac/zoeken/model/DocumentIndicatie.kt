89d5926bf5f017f2bb1b43ab7c68f8b3b8e71dae
edgarvonk <edgar@info.nl>
2024-11-10 18:57:53 +0100

    chore: convert zoeken.model package to Kotlin

--- src/main/kotlin/net/atos/zac/zoeken/model/DocumentIndicatie.kt
+++ src/main/kotlin/net/atos/zac/zoeken/model/DocumentIndicatie.kt
@@ -5,7 +5,7 @@
 package net.atos.zac.zoeken.model
 
 /*
- * This enum represents the possible indicaties for a informatieobject.
+ * This enum represents the possible indicaties for an informatieobject.
  *
  * The order of the enum values determines the sort priority of the indicators (the highest priority indicator first)
  * Nota bene: When the order of the enum values changes, the Solr index for zaken MUST be rebuilt.
--- src/main/kotlin/net/atos/zac/zoeken/model/ZoekResultaat.kt
+++ src/main/kotlin/net/atos/zac/zoeken/model/ZoekResultaat.kt
@@ -7,14 +7,12 @@ package net.atos.zac.zoeken.model
 import net.atos.zac.shared.model.Resultaat
 import java.util.EnumMap
 
-class ZoekResultaat<TYPE>(items: List<TYPE>?, count: Long) :
-    Resultaat<TYPE>(items, count) {
-
-    val filters: EnumMap<FilterVeld, List<FilterResultaat>> = EnumMap(
-        FilterVeld::class.java
-    )
+class ZoekResultaat<TYPE>(items: List<TYPE>?, count: Long) : Resultaat<TYPE>(items, count) {
+    private val filters = EnumMap<FilterVeld, List<FilterResultaat>>(FilterVeld::class.java)
 
     fun addFilter(facetVeld: FilterVeld, waardes: List<FilterResultaat>) {
         filters[facetVeld] = waardes
     }
+
+    fun getFilters(): Map<FilterVeld, List<FilterResultaat>> = filters
 }
