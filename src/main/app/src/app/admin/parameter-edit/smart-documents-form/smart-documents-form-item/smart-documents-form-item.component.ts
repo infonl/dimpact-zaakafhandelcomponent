c75724f00a9e5e20b3804aa813220fa11d5a5bc9
Sander <mail@sanderboer.nl>
2024-11-18 13:23:40 +0100

    push

--- src/main/app/src/app/admin/parameter-edit/smart-documents-form/smart-documents-form-item/smart-documents-form-item.component.ts
+++ src/main/app/src/app/admin/parameter-edit/smart-documents-form/smart-documents-form-item/smart-documents-form-item.component.ts
@@ -5,32 +5,24 @@
  */
 
 import { Component, Input } from "@angular/core";
+import { FormGroup } from "@angular/forms";
 import { GeneratedType } from "../../../../shared/utils/generated-types";
-import {AbstractControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: "smart-documents-form-item",
   templateUrl: "./smart-documents-form-item.component.html",
 })
 export class SmartDocumentsFormItemComponent {
-  @Input() template: GeneratedType<"RestSmartDocumentsTemplate">;
+  @Input() template: GeneratedType<"RestMappedSmartDocumentsTemplate">;
   @Input() documentTypes: GeneratedType<"RestMappedSmartDocumentsTemplate">[];
-  @Input() formGroup: FormGroup<{ documentType: AbstractControl<GeneratedType<"RestMappedSmartDocumentsTemplate">, GeneratedType<"RestMappedSmartDocumentsTemplate">> }>;
-  @Input() initialInformationObjectTypeUuid?: string;
-
-  documentType: GeneratedType<"RestMappedSmartDocumentsTemplate">;
+  @Input() formGroup: FormGroup;
 
   constructor() {
-    if (!this.initialInformationObjectTypeUuid) return;
 
-    this.documentType = this.documentTypes.find(
-      ({ informatieObjectTypeUUID }) =>
-        (this.initialInformationObjectTypeUuid = informatieObjectTypeUUID),
-    );
-    this.formGroup.setValue({ documentType: this.documentType });
   }
 
   clearSelectedDocumentType() {
     console.log("clearSelectedDocumentType");
+    this.template.informatieObjectTypeUUID = ''
   }
 }
