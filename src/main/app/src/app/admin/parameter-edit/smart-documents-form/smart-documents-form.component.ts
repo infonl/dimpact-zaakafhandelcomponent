9d1aa27abb399a7a4e07aa5d4ebefa146de6338c
infonl-marcel <marcele@info.nl>
2024-11-18 16:16:41 +0100

    generated

--- src/main/app/src/app/admin/parameter-edit/smart-documents-form/smart-documents-form.component.ts
+++ src/main/app/src/app/admin/parameter-edit/smart-documents-form/smart-documents-form.component.ts
@@ -14,6 +14,7 @@ import {
   MatTreeFlattener,
 } from "@angular/material/tree";
 import { Informatieobjecttype } from "src/app/informatie-objecten/model/informatieobjecttype";
+import { GeneratedType } from "src/app/shared/utils/generated-types";
 
 interface FlatNode {
   expandable: boolean;
@@ -31,7 +32,7 @@ export class SmartDocumentsFormComponent {
   @Output() formValidityChanged = new EventEmitter<boolean>();
 
   allSmartDocumentTemplateGroup: SmartDocumentsTemplateGroup[] = [];
-  informationObjectTypes: Informatieobjecttype[] = [];
+  informationObjectTypes: GeneratedType<"RestInformatieobjecttype">[] = [];
   zaakTypeTemplateMappings: DocumentsTemplateGroup[] = [];
 
   constructor(
