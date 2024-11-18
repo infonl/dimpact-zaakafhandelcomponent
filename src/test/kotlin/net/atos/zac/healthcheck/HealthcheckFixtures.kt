556927ba6922baece6f176688701c79a9341c8e4
Hristo Iliev <hristo.iliev@info.nl>
2024-11-18 17:21:22 +0200

    feature: add unit test for zaaktype validity health-check

---
+++ src/test/kotlin/net/atos/zac/healthcheck/HealthcheckFixtures.kt
@@ -0,0 +1,36 @@
+package net.atos.zac.healthcheck
+
+import net.atos.client.zgw.ztc.model.createZaakType
+import net.atos.client.zgw.ztc.model.generated.ZaakType
+import net.atos.zac.healthcheck.model.ZaaktypeInrichtingscheck
+
+fun createZaaktypeInrichtingscheck(
+    zaaktype: ZaakType = createZaakType(),
+    statustypeIntakeAanwezig: Boolean = true,
+    statustypeInBehandelingAanwezig: Boolean = true,
+    statustypeHeropendAanwezig: Boolean = true,
+    statustypeAfgerondAanwezig: Boolean = true,
+    statustypeAfgerondLaatsteVolgnummer: Boolean = true,
+    statustypeAanvullendeInformatieVereist: Boolean = true,
+    rolInitiatorAanwezig: Boolean = true,
+    rolBehandelaarAanwezig: Boolean = true,
+    rolOverigeAanwezig: Boolean = true,
+    informatieobjecttypeEmailAanwezig: Boolean = true,
+    resultaattypeAanwezig: Boolean = true,
+    zaakafhandelParametersValide: Boolean = true,
+    besluittypeAanwezig: Boolean = true,
+) = ZaaktypeInrichtingscheck(zaaktype).apply {
+    isStatustypeIntakeAanwezig = statustypeIntakeAanwezig
+    isStatustypeInBehandelingAanwezig = statustypeInBehandelingAanwezig
+    isStatustypeHeropendAanwezig = statustypeHeropendAanwezig
+    isStatustypeAfgerondAanwezig = statustypeAfgerondAanwezig
+    isStatustypeAfgerondLaatsteVolgnummer = statustypeAfgerondLaatsteVolgnummer
+    isStatustypeAanvullendeInformatieVereist = statustypeAanvullendeInformatieVereist
+    isRolInitiatorAanwezig = rolInitiatorAanwezig
+    isRolBehandelaarAanwezig = rolBehandelaarAanwezig
+    isRolOverigeAanwezig = rolOverigeAanwezig
+    isInformatieobjecttypeEmailAanwezig = informatieobjecttypeEmailAanwezig
+    isResultaattypeAanwezig = resultaattypeAanwezig
+    isZaakafhandelParametersValide = zaakafhandelParametersValide
+    isBesluittypeAanwezig = besluittypeAanwezig
+}
