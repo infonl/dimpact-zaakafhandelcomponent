7836fb5a39065762247150d797f13cd2f098b321
Sander <mail@sanderboer.nl>
2024-11-18 14:18:31 +0100

    init styling

--- src/main/app/src/app/admin/parameter-edit/smart-documents-form/smart-documents-form-item/smart-documents-form-item.component.ts
+++ src/main/app/src/app/admin/parameter-edit/smart-documents-form/smart-documents-form-item/smart-documents-form-item.component.ts
@@ -5,7 +5,8 @@
  */
 
 import { Component, Input } from "@angular/core";
-import { FormGroup } from "@angular/forms";
+import { FormControl, FormGroup } from "@angular/forms";
+import { TranslateService } from "@ngx-translate/core";
 import { GeneratedType } from "../../../../shared/utils/generated-types";
 
 @Component({
@@ -14,18 +15,35 @@ import { GeneratedType } from "../../../../shared/utils/generated-types";
 })
 export class SmartDocumentsFormItemComponent {
   @Input() template: GeneratedType<"RestMappedSmartDocumentsTemplate">;
-  @Input() documentTypes: GeneratedType<"RestInformatieobjecttype">[];
   @Input() informationObjectTypes: GeneratedType<"RestInformatieobjecttype">[];
   @Input() formGroup: FormGroup;
 
-  confidentiality = ''
+  confidentiality = new FormControl({ value: "", disabled: true });
+  enabled = new FormControl({ value: false, disabled: false });
 
-  constructor() {
+  constructor(private readonly translateService: TranslateService) {}
 
+  ngOnInit() {
+    this.enabled.setValue(this.template.informatieObjectTypeUUID !== "");
   }
 
   clearSelectedDocumentType() {
-    console.log("clearSelectedDocumentType");
-    this.template.informatieObjectTypeUUID = ''
+    this.template.informatieObjectTypeUUID = "";
+    this.confidentiality.reset();
+    this.enabled.reset();
+  }
+
+  setConfidentiality() {
+    const { informatieObjectTypeUUID } = this.template;
+
+    const confidentiality = this.informationObjectTypes.find(
+      ({ uuid }) => informatieObjectTypeUUID === uuid,
+    )?.vertrouwelijkheidaanduiding;
+
+    const translated = this.translateService.instant(
+      `vertrouwelijkheidaanduiding.${confidentiality}`,
+    );
+
+    this.confidentiality.setValue(translated);
   }
 }
