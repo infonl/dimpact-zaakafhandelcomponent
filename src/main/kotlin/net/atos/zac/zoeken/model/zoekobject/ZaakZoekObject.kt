8dd555264abcbc823d9ebef44a1c884f169882ae
edgarvonk <edgar@info.nl>
2024-11-18 20:14:03 +0100

    chore: convert to Kotlin

--- src/main/kotlin/net/atos/zac/zoeken/model/zoekobject/ZaakZoekObject.kt
+++ src/main/kotlin/net/atos/zac/zoeken/model/zoekobject/ZaakZoekObject.kt
@@ -22,7 +22,124 @@ data class ZaakZoekObject(
     var id: String,
 
     @Field
-    private var type: String
+    private var type: String,
+
+    @Field("zaak_identificatie")
+    var identificatie: String? = null,
+
+    @Field(OMSCHRIJVING_FIELD)
+    var omschrijving: String? = null,
+
+    @Field(TOELICHTING_FIELD)
+    var toelichting: String? = null,
+
+    @Field("zaak_registratiedatum")
+    var registratiedatum: Date? = null,
+
+    @Field("zaak_archiefNominatie")
+    var archiefNominatie: String? = null,
+
+    @Field("zaak_archiefActiedatum")
+    var archiefActiedatum: Date? = null,
+
+    @Field("zaak_startdatum")
+    var startdatum: Date? = null,
+
+    @Field("zaak_einddatumGepland")
+    var einddatumGepland: Date? = null,
+
+    @Field("zaak_einddatum")
+    var einddatum: Date? = null,
+
+    @Field("zaak_uiterlijkeEinddatumAfdoening")
+    var uiterlijkeEinddatumAfdoening: Date? = null,
+
+    @Field("zaak_publicatiedatum")
+    var publicatiedatum: Date? = null,
+
+    @Field("zaak_communicatiekanaal")
+    var communicatiekanaal: String? = null,
+
+    @Field("zaak_vertrouwelijkheidaanduiding")
+    var vertrouwelijkheidaanduiding: String? = null,
+
+    @Field(AFGEHANDELD_FIELD)
+    var isAfgehandeld: Boolean = false,
+
+    @Field("zaak_groepId")
+    var groepID: String? = null,
+
+    @Field("zaak_groepNaam")
+    var groepNaam: String? = null,
+
+    @Field("zaak_behandelaarNaam")
+    var behandelaarNaam: String? = null,
+
+    @Field(BEHANDELAAR_ID_FIELD)
+    var behandelaarGebruikersnaam: String? = null,
+
+    @Field("zaak_initiatorIdentificatie")
+    var initiatorIdentificatie: String? = null,
+
+    @Field("zaak_initiatorType")
+    private var initiatorType: String? = null,
+
+    @Field("zaak_locatie")
+    var locatie: String? = null,
+
+    @Field("zaak_duurVerlenging")
+    var duurVerlenging: String? = null,
+
+    @Field("zaak_redenVerlenging")
+    var redenVerlenging: String? = null,
+
+    @Field("zaak_redenOpschorting")
+    var redenOpschorting: String? = null,
+
+    @Field("zaak_zaaktypeUuid")
+    var zaaktypeUuid: String? = null,
+
+    @Field("zaak_zaaktypeIdentificatie")
+    var zaaktypeIdentificatie: String? = null,
+
+    @Field("zaak_zaaktypeOmschrijving")
+    var zaaktypeOmschrijving: String? = null,
+
+    @Field("zaak_resultaattypeOmschrijving")
+    var resultaattypeOmschrijving: String? = null,
+
+    @Field("zaak_resultaatToelichting")
+    var resultaatToelichting: String? = null,
+
+    @Field(EINDSTATUS_FIELD)
+    var isStatusEindstatus: Boolean = false,
+
+    @Field("zaak_statustypeOmschrijving")
+    var statustypeOmschrijving: String? = null,
+
+    @Field("zaak_statusDatumGezet")
+    var statusDatumGezet: Date? = null,
+
+    @Field("zaak_statusToelichting")
+    var statusToelichting: String? = null,
+
+    @Field("zaak_aantalOpenstaandeTaken")
+    var aantalOpenstaandeTaken: Long = 0,
+
+    @Field(ZoekObject.Companion.IS_TOEGEKEND_FIELD)
+    var isToegekend: Boolean = false,
+
+    @Field("zaak_indicaties")
+    private var indicaties: MutableList<String>? = null,
+
+    @Field("zaak_indicaties_sort")
+    private var indicatiesVolgorde: Long = 0,
+
+    @Field("zaak_betrokkene_*")
+    var betrokkenen: MutableMap<String, MutableList<String>> = mutableMapOf(),
+
+    @Field("zaak_bagObjecten")
+    var bagObjectIDs: List<String>? = null
 ) : ZoekObject {
     companion object {
         const val AFGEHANDELD_FIELD: String = "zaak_afgehandeld"
@@ -32,123 +149,6 @@ data class ZaakZoekObject(
         const val TOELICHTING_FIELD: String = "zaak_toelichting"
         const val ZAAK_BETROKKENE_PREFIX: String = "zaak_betrokkene_"
     }
-
-    @Field("zaak_identificatie")
-    var identificatie: String? = null
-
-    @Field(OMSCHRIJVING_FIELD)
-    var omschrijving: String? = null
-
-    @Field(TOELICHTING_FIELD)
-    var toelichting: String? = null
-
-    @Field("zaak_registratiedatum")
-    var registratiedatum: Date? = null
-
-    @Field("zaak_archiefNominatie")
-    var archiefNominatie: String? = null
-
-    @Field("zaak_archiefActiedatum")
-    var archiefActiedatum: Date? = null
-
-    @Field("zaak_startdatum")
-    var startdatum: Date? = null
-
-    @Field("zaak_einddatumGepland")
-    var einddatumGepland: Date? = null
-
-    @Field("zaak_einddatum")
-    var einddatum: Date? = null
-
-    @Field("zaak_uiterlijkeEinddatumAfdoening")
-    var uiterlijkeEinddatumAfdoening: Date? = null
-
-    @Field("zaak_publicatiedatum")
-    var publicatiedatum: Date? = null
-
-    @Field("zaak_communicatiekanaal")
-    var communicatiekanaal: String? = null
-
-    @Field("zaak_vertrouwelijkheidaanduiding")
-    var vertrouwelijkheidaanduiding: String? = null
-
-    @Field(AFGEHANDELD_FIELD)
-    var isAfgehandeld: Boolean = false
-
-    @Field("zaak_groepId")
-    var groepID: String? = null
-
-    @Field("zaak_groepNaam")
-    var groepNaam: String? = null
-
-    @Field("zaak_behandelaarNaam")
-    var behandelaarNaam: String? = null
-
-    @Field(BEHANDELAAR_ID_FIELD)
-    var behandelaarGebruikersnaam: String? = null
-
-    @Field("zaak_initiatorIdentificatie")
-    var initiatorIdentificatie: String? = null
-
-    @Field("zaak_initiatorType")
-    private var initiatorType: String? = null
-
-    @Field("zaak_locatie")
-    var locatie: String? = null
-
-    @Field("zaak_duurVerlenging")
-    var duurVerlenging: String? = null
-
-    @Field("zaak_redenVerlenging")
-    var redenVerlenging: String? = null
-
-    @Field("zaak_redenOpschorting")
-    var redenOpschorting: String? = null
-
-    @Field("zaak_zaaktypeUuid")
-    var zaaktypeUuid: String? = null
-
-    @Field("zaak_zaaktypeIdentificatie")
-    var zaaktypeIdentificatie: String? = null
-
-    @Field("zaak_zaaktypeOmschrijving")
-    var zaaktypeOmschrijving: String? = null
-
-    @Field("zaak_resultaattypeOmschrijving")
-    var resultaattypeOmschrijving: String? = null
-
-    @Field("zaak_resultaatToelichting")
-    var resultaatToelichting: String? = null
-
-    @Field(EINDSTATUS_FIELD)
-    var isStatusEindstatus: Boolean = false
-
-    @Field("zaak_statustypeOmschrijving")
-    var statustypeOmschrijving: String? = null
-
-    @Field("zaak_statusDatumGezet")
-    var statusDatumGezet: Date? = null
-
-    @Field("zaak_statusToelichting")
-    var statusToelichting: String? = null
-
-    @Field("zaak_aantalOpenstaandeTaken")
-    var aantalOpenstaandeTaken: Long = 0
-
-    @Field(ZoekObject.Companion.IS_TOEGEKEND_FIELD)
-    var isToegekend: Boolean = false
-
-    @Field("zaak_indicaties")
-    private var indicaties: MutableList<String>? = null
-
-    @Field("zaak_indicaties_sort")
-    private var indicatiesVolgorde: Long = 0
-
-    @Field("zaak_betrokkene_*")
-    var betrokkenen: MutableMap<String, MutableList<String>> = mutableMapOf()
-
-    @Field("zaak_bagObjecten")
-    var bagObjectIDs: List<String>? = null
 
     override fun getObjectId() = id
 
