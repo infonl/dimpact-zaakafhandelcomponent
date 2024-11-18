847e4a9d325d5d327555a15edb853024fb7b3063
infonl-marcel <marcele@info.nl>
2024-11-18 11:16:28 +0100

    mat tree listed something

--- src/main/app/src/app/admin/parameter-edit/smart-documents-form/smart-documents-form.component.ts
+++ src/main/app/src/app/admin/parameter-edit/smart-documents-form/smart-documents-form.component.ts
@@ -1,5 +1,17 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, effect, EventEmitter, Input, Output } from "@angular/core";
 import { FormGroup } from "@angular/forms";
+import { injectQuery } from "@tanstack/angular-query-experimental";
+import { firstValueFrom } from "rxjs";
+import { InformatieObjectenService } from "src/app/informatie-objecten/informatie-objecten.service";
+import {
+  SmartDocumentsService,
+  SmartDocumentsTemplateGroup,
+} from "../../smart-documents.service";
+import { FlatTreeControl } from "@angular/cdk/tree";
+import {
+  MatTreeFlatDataSource,
+  MatTreeFlattener,
+} from "@angular/material/tree";
 
 @Component({
   selector: "smart-documents-form",
@@ -7,11 +19,58 @@ import { FormGroup } from "@angular/forms";
 })
 export class SmartDocumentsFormComponent {
   @Input() formGroup: FormGroup;
+  @Input() zaakTypeUuid: string;
   @Output() formValidityChanged = new EventEmitter<boolean>();
 
-  constructor() {
+  constructor(
+    private smartDocumentsService: SmartDocumentsService,
+    private informatieObjectenService: InformatieObjectenService,
+  ) {
     console.log("SmartDocumentsFormComponent constructor called");
+
+    effect(() => {
+      this.dataSource.data = this.allSmartDocumentTemplates.data() || [];
+    });
   }
+
+  private _transformer = (node: SmartDocumentsTemplateGroup, level: number) => {
+    return {
+      expandable: !!node.templates && node.templates.length > 0,
+      id: node.id,
+      name: node.name,
+      level: level,
+    };
+  };
+
+  treeControl = new FlatTreeControl<SmartDocumentsTemplateGroup>(
+    (node) => 0,
+    (node) => true,
+  );
+
+  treeFlattener = new MatTreeFlattener(
+    this._transformer,
+    (node) => node.level,
+    (node) => node.expandable,
+    (node) => node.templates,
+  );
+
+  allSmartDocumentTemplates = injectQuery(() => ({
+    queryKey: ["all smart documents"],
+    queryFn: () => firstValueFrom(this.smartDocumentsService.listTemplates()),
+  }));
+
+  templateMappings = injectQuery(() => ({
+    queryKey: [
+      "smart documents template mapping for zaaktype",
+      this.zaakTypeUuid,
+    ],
+    queryFn: () =>
+      firstValueFrom(
+        this.smartDocumentsService.getTemplatesMappingFlat(this.zaakTypeUuid),
+      ),
+  }));
+
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
 
   ngOnInit() {
     if (this.formGroup) {
