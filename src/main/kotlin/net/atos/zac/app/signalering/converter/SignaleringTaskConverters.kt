3051d2ef702fe98164833561c85956d474f36b9c
Edgar Vonk <edgar@info.nl>
2024-11-21 21:09:20 +0100

    chore: added working initial config for https://github.com/apache/skywalking-eyes. Added GitHub action. Fixed a number of SPDX headers in Kotlin files.

--- src/main/kotlin/net/atos/zac/app/signalering/converter/SignaleringTaskConverters.kt
+++ src/main/kotlin/net/atos/zac/app/signalering/converter/SignaleringTaskConverters.kt
@@ -1,3 +1,7 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Lifely
+ * SPDX-License-Identifier: EUPL-1.2+
+ */
 package net.atos.zac.app.signalering.converter
 
 import net.atos.zac.app.signalering.model.RestSignaleringTaskSummary
