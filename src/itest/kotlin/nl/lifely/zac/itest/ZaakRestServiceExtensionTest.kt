e730c49552555154082ff3dff00ee804fbfbb8df
Edgar Vonk <edgar@info.nl>
2024-11-21 11:40:35 +0100

    chore: refactored code and skip verlenging and opschorting audit trail lines

--- src/itest/kotlin/nl/lifely/zac/itest/ZaakRestServiceExtensionTest.kt
+++ src/itest/kotlin/nl/lifely/zac/itest/ZaakRestServiceExtensionTest.kt
@@ -4,27 +4,12 @@ import io.github.oshai.kotlinlogging.KotlinLogging
 import io.kotest.assertions.json.shouldContainJsonKeyValue
 import io.kotest.core.spec.Order
 import io.kotest.core.spec.style.BehaviorSpec
-import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.shouldNotBe
 import nl.lifely.zac.itest.client.ItestHttpClient
-import nl.lifely.zac.itest.client.ZacClient
-import nl.lifely.zac.itest.config.ItestConfiguration.ACTIE_INTAKE_AFRONDEN
-import nl.lifely.zac.itest.config.ItestConfiguration.DATE_TIME_2000_01_01
-import nl.lifely.zac.itest.config.ItestConfiguration.HTTP_STATUS_NO_CONTENT
 import nl.lifely.zac.itest.config.ItestConfiguration.HTTP_STATUS_OK
-import nl.lifely.zac.itest.config.ItestConfiguration.TEST_GROUP_A_DESCRIPTION
-import nl.lifely.zac.itest.config.ItestConfiguration.TEST_GROUP_A_ID
 import nl.lifely.zac.itest.config.ItestConfiguration.TEST_SPEC_ORDER_AFTER_ZAAK_CREATED
-import nl.lifely.zac.itest.config.ItestConfiguration.TEST_SPEC_ORDER_AFTER_ZAAK_UPDATED
-import nl.lifely.zac.itest.config.ItestConfiguration.ZAAKTYPE_INDIENEN_AANSPRAKELIJKSTELLING_DOOR_DERDEN_BEHANDELEN_UUID
 import nl.lifely.zac.itest.config.ItestConfiguration.ZAC_API_URI
 import nl.lifely.zac.itest.config.ItestConfiguration.zaakProductaanvraag1Uuid
-import nl.lifely.zac.itest.util.sleep
-import org.json.JSONArray
-import org.json.JSONObject
-import java.time.LocalDate
-import java.util.UUID
 
 @Order(TEST_SPEC_ORDER_AFTER_ZAAK_CREATED)
 @Suppress("MagicNumber")
@@ -47,7 +32,7 @@ class ZaakRestServiceExtensionTest : BehaviorSpec({
                         "duurDagen": $daysExtended,
                         "takenVerlengen": false
                     }
-                    """.trimIndent()
+                """.trimIndent()
             )
             Then(
                 """the response should be OK and the returned zaak should be extended"""
