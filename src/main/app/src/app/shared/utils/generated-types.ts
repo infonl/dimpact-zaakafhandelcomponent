fdc8bc57d67109403315709a67ce6db974639cee
Sander <mail@sanderboer.nl>
2024-11-25 16:18:04 +0100

    update generated type to set nested properties

--- src/main/app/src/app/shared/utils/generated-types.ts
+++ src/main/app/src/app/shared/utils/generated-types.ts
@@ -5,5 +5,13 @@
 
 import { components } from "../../../generated/types/zac-openapi-types";
 
-export type GeneratedType<T extends keyof components["schemas"]> =
-  components["schemas"][T];
+type NestedSchemaProperty<Type, Key> = Key extends `${infer P}.${infer R}`
+  ? P extends keyof Type
+    ? NestedSchemaProperty<Type[P], R>
+    : never
+  : Key extends keyof Type
+    ? Type[Key]
+    : never;
+
+export type GeneratedType<Key extends keyof components["schemas"]> =
+  NestedSchemaProperty<components["schemas"], Key>;
