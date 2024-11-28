996da740c4afc2a8949c020e01f86d858361ac37
Edgar Vonk <edgar@info.nl>
2024-11-28 15:57:45 +0100

    Update src/test/kotlin/nl/lifely/zac/test/util/TestStringUtils.kt
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/test/kotlin/nl/lifely/zac/test/util/TestStringUtils.kt
+++ src/test/kotlin/nl/lifely/zac/test/util/TestStringUtils.kt
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Lifely
+ * SPDX-License-Identifier: EUPL-1.2+
+ */
+
 package nl.lifely.zac.test.util
 
 fun createRandomStringWithAlphanumericCharacters(stringLength: Int) = List(stringLength) {
