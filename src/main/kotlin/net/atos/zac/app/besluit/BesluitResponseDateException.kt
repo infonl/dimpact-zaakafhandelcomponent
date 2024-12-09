c4f8edc256e300f206abd2912551b141ec4e2047
Hristo Iliev <hristo.iliev@info.nl>
2024-12-09 21:44:48 +0200

    chore: remove unused class

--- src/main/kotlin/net/atos/zac/app/besluit/BesluitResponseDateException.kt
+++
@@ -1,8 +0,0 @@
-/*
- * SPDX-FileCopyrightText: 2024 Lifely
- * SPDX-License-Identifier: EUPL-1.2+
- */
-
-package net.atos.zac.app.besluit
-
-class BesluitResponseDateException(message: String) : RuntimeException(message)
