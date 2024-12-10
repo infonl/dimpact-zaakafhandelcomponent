b5a1ed77568394e8d9054e9de6ec36322a070b5f
Edgar Vonk <edgar@info.nl>
2024-12-10 15:47:56 +0100

    chore: added additional OpenNotificaties configuration

--- scripts/docker-compose/imports/opennotificaties-database/database/1-setup-applicatie.sql
+++ scripts/docker-compose/imports/opennotificaties-database/database/1-setup-applicatie.sql
@@ -9,8 +9,11 @@ INSERT INTO authorizations_authorizationsconfig (api_root, component) VALUES('ht
 INSERT INTO zgw_consumers_service (label, api_type, api_root, client_id, secret, auth_type, header_key, header_value, oas, nlx, user_id, user_representation, oas_file, client_certificate_id, server_certificate_id, uuid, timeout, api_connection_check_path) VALUES('notificaties-self', 'nrc', 'http://host.docker.internal:8003/api/v1/', 'opennotificaties', 'openNotificatiesApiSecretKey', 'zgw', '', '', 'http://host.docker.internal:8003/api/v1/schema/openapi.yaml', '', '', '', '', NULL, NULL, 'd7d82c34-f6f2-4cf7-9376-bd23e7633a1f', 10, '');
 
 -- Set up the Notificatiescomponentconfiguratie
--- We assume here that a record already exists with id=1
+-- We assume here that a record already exists with id=1 (this is provisioned by OpenNotificaties on startup)
 UPDATE notifications_api_common_notificationsconfig SET notifications_api_service_id=(SELECT id FROM zgw_consumers_service WHERE label = 'notificaties-self'), notification_delivery_max_retries=5, notification_delivery_retry_backoff=3, notification_delivery_retry_backoff_max=48 WHERE id=1;
 
 -- Set up the External API credentials
 INSERT INTO vng_api_common_apicredential (api_root, client_id, secret, label, user_id, user_representation) VALUES('http://openzaak.local:8000/autorisaties/api/v1/', 'open-zaak-autorisaties', 'openZaakAutorisatiesApiSecretKey', 'Open Zaak - Autorisaties', 'open-zaak-autorisaties', 'Open Zaak - Autorisaties');
+
+-- Set up the Autorisatiegegevens
+INSERT INTO vng_api_common_jwtsecret (identifier, secret) VALUES(1, 'open-zaak-autorisaties', 'openZaakAutorisatiesApiSecretKey');
