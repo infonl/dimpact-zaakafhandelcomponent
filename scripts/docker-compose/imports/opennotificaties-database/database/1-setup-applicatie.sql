ea1f0858936b70748de777e5bed3733946ad6dd6
Edgar Vonk <edgar@info.nl>
2024-12-11 15:26:12 +0100

    chore: added more OpenNotificaties configuration

--- scripts/docker-compose/imports/opennotificaties-database/database/1-setup-applicatie.sql
+++ scripts/docker-compose/imports/opennotificaties-database/database/1-setup-applicatie.sql
@@ -2,10 +2,11 @@
 INSERT INTO accounts_user (id, password, last_login, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined) VALUES (1, 'pbkdf2_sha256$260000$gtIe19cI1vW9RzIsRDpriC$o8G6cItI5vXqbGFcXuu0pbullajpvMDc6Hze70mf+jE=', null, true, 'admin', '', '', 'admin@example.com', true, true, '2023-08-08 15:14:56.735552 +00:00');
 
 -- Set up the Autorisatiecomponentconfiguratie
-INSERT INTO authorizations_authorizationsconfig (api_root, component) VALUES('http://openzaak.local:8000/api/v1/', 'ac');
+INSERT INTO authorizations_authorizationsconfig (api_root, component) VALUES('http://host.docker.internal:8001/autorisaties/api/v1/', 'ac');
 
 -- Set up the OpenNotificaties 'self' service
 -- TODO: it seems we need to use 'host.docker.internal' here
+-- TODO: not needed?
 INSERT INTO zgw_consumers_service (label, api_type, api_root, client_id, secret, auth_type, header_key, header_value, oas, nlx, user_id, user_representation, oas_file, client_certificate_id, server_certificate_id, uuid, timeout, api_connection_check_path) VALUES('notificaties-self', 'nrc', 'http://host.docker.internal:8003/api/v1/', 'opennotificaties', 'openNotificatiesApiSecretKey', 'zgw', '', '', 'http://host.docker.internal:8003/api/v1/schema/openapi.yaml', '', '', '', '', NULL, NULL, 'd7d82c34-f6f2-4cf7-9376-bd23e7633a1f', 10, '');
 
 -- Set up the Notificatiescomponentconfiguratie
@@ -14,7 +15,7 @@ UPDATE notifications_api_common_notificationsconfig SET notifications_api_servic
 
 -- Set up the External API credentials
 -- TODO: it seems we need to use 'host.docker.internal' here
-INSERT INTO vng_api_common_apicredential (api_root, client_id, secret, label, user_id, user_representation) VALUES('http://openzaak.local:8000/autorisaties/api/v1/', 'open-zaak-autorisaties', 'openZaakAutorisatiesApiSecretKey', 'Open Zaak - Autorisaties', 'open-zaak-autorisaties', 'Open Zaak - Autorisaties');
+INSERT INTO vng_api_common_apicredential (api_root, client_id, secret, label, user_id, user_representation) VALUES('http://host.docker.internal:8001/autorisaties/api/v1/', 'open-zaak-autorisaties', 'openZaakAutorisatiesApiSecretKey', 'Open Zaak - Autorisaties', 'open-zaak-autorisaties', 'Open Zaak - Autorisaties');
 
 -- Set up the Autorisatiegegevens
 INSERT INTO vng_api_common_jwtsecret (identifier, secret) VALUES('open-zaak-autorisaties', 'openZaakAutorisatiesApiSecretKey');
