b9445c0fb806a4895cb0c54fd7e9e7bee8824542
Edgar Vonk <edgar@info.nl>
2024-12-11 15:50:25 +0100

    chore: added more OpenNotificaties configuration

--- scripts/docker-compose/imports/opennotificaties-database/database/1-setup-applicatie.sql
+++ scripts/docker-compose/imports/opennotificaties-database/database/1-setup-applicatie.sql
@@ -4,11 +4,6 @@ INSERT INTO accounts_user (id, password, last_login, is_superuser, username, fir
 -- Set up the Autorisatiecomponentconfiguratie
 INSERT INTO authorizations_authorizationsconfig (api_root, component) VALUES('http://host.docker.internal:8001/autorisaties/api/v1/', 'ac');
 
--- Set up the OpenNotificaties 'self' service
--- TODO: it seems we need to use 'host.docker.internal' here
--- TODO: not needed?
-INSERT INTO zgw_consumers_service (label, api_type, api_root, client_id, secret, auth_type, header_key, header_value, oas, nlx, user_id, user_representation, oas_file, client_certificate_id, server_certificate_id, uuid, timeout, api_connection_check_path) VALUES('notificaties-self', 'nrc', 'http://host.docker.internal:8003/api/v1/', 'opennotificaties', 'openNotificatiesApiSecretKey', 'zgw', '', '', 'http://host.docker.internal:8003/api/v1/schema/openapi.yaml', '', '', '', '', NULL, NULL, 'd7d82c34-f6f2-4cf7-9376-bd23e7633a1f', 10, '');
-
 -- Set up the Notificatiescomponentconfiguratie
 -- We assume here that a record already exists with id=1 (this is provisioned by OpenNotificaties on startup)
 UPDATE notifications_api_common_notificationsconfig SET notifications_api_service_id=(SELECT id FROM zgw_consumers_service WHERE label = 'notificaties-self'), notification_delivery_max_retries=5, notification_delivery_retry_backoff=3, notification_delivery_retry_backoff_max=48 WHERE id=1;
