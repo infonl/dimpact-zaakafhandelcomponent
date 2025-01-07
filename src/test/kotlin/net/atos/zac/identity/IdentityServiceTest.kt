873b95b02a727e4997ba5484e5ae7291809e927f
edgarvonk <edgar@info.nl>
2025-01-07 18:43:18 +0100

    chore: code improvements

--- src/test/kotlin/net/atos/zac/identity/IdentityServiceTest.kt
+++ src/test/kotlin/net/atos/zac/identity/IdentityServiceTest.kt
@@ -1,20 +1,29 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Lifely
+ * SPDX-License-Identifier: EUPL-1.2+
+ */
+
 package net.atos.zac.identity
 
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
-import net.atos.zac.identity.IdentityService
 import org.keycloak.admin.client.resource.RealmResource
-import org.keycloak.representations.idm.UserRepresentation
 import org.keycloak.representations.idm.createUserRepresentation
 
-class IdentityServiceTest: BehaviorSpec ({
+class IdentityServiceTest : BehaviorSpec({
     val realmResource = mockk<RealmResource>()
     val identityService = IdentityService(realmResource)
 
     Given("Users in the Keycloak realm") {
         val userRepresentations = listOf(
+            createUserRepresentation(
+                username = "dummyUsername3",
+                firstName = "dummyFirstName3",
+                lastName = "dummyLastName3",
+                email = "test3@example.com"
+            ),
             createUserRepresentation(
                 username = "dummyUsername1",
                 firstName = "dummyFirstName1",
@@ -30,11 +39,11 @@ class IdentityServiceTest: BehaviorSpec ({
         )
         every { realmResource.users().list() } returns userRepresentations
 
-        When("the users are listed") {
+        When("the users are listed, sorted by full name") {
             val users = identityService.listUsers()
 
             Then("the users are retrieved from Keycloak") {
-                users.size shouldBe 2
+                users.size shouldBe 3
                 with(users[0]) {
                     id shouldBe "dummyUsername1"
                     firstName shouldBe "dummyFirstName1"
@@ -46,6 +55,12 @@ class IdentityServiceTest: BehaviorSpec ({
                     firstName shouldBe "dummyFirstName2"
                     lastName shouldBe "dummyLastName2"
                     fullName shouldBe "dummyFirstName2 dummyLastName2"
+                }
+                with(users[2]) {
+                    id shouldBe "dummyUsername3"
+                    firstName shouldBe "dummyFirstName3"
+                    lastName shouldBe "dummyLastName3"
+                    fullName shouldBe "dummyFirstName3 dummyLastName3"
                 }
             }
         }
--- src/test/kotlin/org/keycloak/representations/idm/KeycloakRepresentationsIdmFixtures.kt
+++ src/test/kotlin/org/keycloak/representations/idm/KeycloakRepresentationsIdmFixtures.kt
@@ -1,9 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Lifely
+ * SPDX-License-Identifier: EUPL-1.2+
+ */
+
 package org.keycloak.representations.idm
 
 fun createUserRepresentation(
     username: String = "dummyUsername",
-    firstName: String = "dummyFirstName",
-    lastName: String = "dummyLastName",
+    firstName: String? = "dummyFirstName",
+    lastName: String? = "dummyLastName",
     email: String = "dummy@example.com"
 ) = UserRepresentation().apply {
     this.username = username
