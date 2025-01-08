1d3f529eff680662714c569b8b07db50c1ffc504
Hristo Iliev <hristo.iliev@info.nl>
2025-01-08 20:32:20 +0200

    feature: add BSNValidator tests

--- src/main/kotlin/net/atos/zac/util/BSNValidator.kt
+++ src/main/kotlin/net/atos/zac/util/BSNValidator.kt
@@ -11,17 +11,17 @@ object BSNValidator {
 
     fun validateBSN(bsn: String) {
         require(bsn.length == BSN_LENGTH) { "BSN '$bsn' length must be $BSN_LENGTH" }
-        val sum = bsnSum(bsn)
-        require(sum % BSN_MULTIPLE == 0) { "Invalid BSN '$bsn'" }
+        require(bsnSum(bsn) % BSN_MULTIPLE == 0) { "Invalid BSN '$bsn'" }
     }
 
     private fun bsnSum(bsn: String) =
         bsn.mapIndexed { index, bsnChar ->
             require(bsnChar.isDigit()) { "Character on index $index in BSN '$bsn' is not a digit" }
             if (index == BSN_LENGTH - 1) {
-                - bsnChar.digitToInt()
+                -bsnChar.digitToInt()
             } else {
                 (BSN_LENGTH - index) * bsnChar.digitToInt()
             }
         }.sum()
+
 }
---
+++ src/test/kotlin/net/atos/zac/util/BSNValidatorTest.kt
@@ -0,0 +1,85 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Lifely
+ * SPDX-License-Identifier: EUPL-1.2+
+ */
+
+package net.atos.zac.util
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.checkUnnecessaryStub
+import java.lang.IllegalArgumentException
+
+class BSNValidatorTest : BehaviorSpec({
+
+    beforeEach {
+        checkUnnecessaryStub()
+    }
+
+    Given("a valid BSN") {
+        val validBSN = "316245124"
+
+        When("it is validated") {
+            BSNValidator.validateBSN(validBSN)
+
+            Then("no exception is thrown") {}
+        }
+    }
+
+    Given("an invalid BSN") {
+        val invalidBSN = "123456789"
+
+        When("it is validated") {
+            val exception = shouldThrow<IllegalArgumentException> {
+                BSNValidator.validateBSN(invalidBSN)
+            }
+
+            Then("exception is thrown") {
+                exception.message shouldBe "Invalid BSN '$invalidBSN'"
+            }
+        }
+    }
+
+    Given("BSN that is shorter than expected") {
+        val invalidBSN = "12345678"
+
+        When("it is validated") {
+            val exception = shouldThrow<IllegalArgumentException> {
+                BSNValidator.validateBSN(invalidBSN)
+            }
+
+            Then("exception is thrown") {
+                exception.message shouldBe "BSN '$invalidBSN' length must be 9"
+            }
+        }
+    }
+
+    Given("BSN that is longer than expected") {
+        val invalidBSN = "1234567890"
+
+        When("it is validated") {
+            val exception = shouldThrow<IllegalArgumentException> {
+                BSNValidator.validateBSN(invalidBSN)
+            }
+
+            Then("exception is thrown") {
+                exception.message shouldBe "BSN '$invalidBSN' length must be 9"
+            }
+        }
+    }
+
+    Given("BSN that contains non-numeric characters") {
+        val invalidBSN = "123A56789"
+
+        When("it is validated") {
+            val exception = shouldThrow<IllegalArgumentException> {
+                BSNValidator.validateBSN(invalidBSN)
+            }
+
+            Then("exception is thrown") {
+                exception.message shouldBe "Character on index 3 in BSN '$invalidBSN' is not a digit"
+            }
+        }
+    }
+})
