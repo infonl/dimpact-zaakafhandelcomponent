602067e434328f88b6438c55075d60abbacc35b5
edgarvonk <edgar@info.nl>
2025-01-16 21:15:29 +0100

    chore: convert more code to Kotlin

--- src/main/kotlin/net/atos/zac/app/zoeken/model/RestDocumentZoekObject.kt
+++ src/main/kotlin/net/atos/zac/app/zoeken/model/RestDocumentZoekObject.kt
@@ -11,44 +11,43 @@ import net.atos.zac.util.time.DateTimeConverterUtil.convertToLocalDate
 import net.atos.zac.zoeken.model.DocumentIndicatie
 import net.atos.zac.zoeken.model.zoekobject.DocumentZoekObject
 import net.atos.zac.zoeken.model.zoekobject.ZoekObjectType
-import nl.info.zac.util.NoArgConstructor
 import java.time.LocalDate
 import java.util.EnumSet
 
-@NoArgConstructor
+
 data class RestDocumentZoekObject(
     override val id: String? = null,
     override val type: ZoekObjectType? = null,
     override val identificatie: String? = null,
-    var titel: String? = null,
-    var beschrijving: String? = null,
-    var zaaktypeUuid: String? = null,
-    var zaaktypeIdentificatie: String? = null,
-    var zaaktypeOmschrijving: String? = null,
-    var zaakIdentificatie: String? = null,
-    var zaakUuid: String? = null,
-    var zaakRelatie: String? = null,
-    var creatiedatum: LocalDate? = null,
-    var registratiedatum: LocalDate? = null,
-    var ontvangstdatum: LocalDate? = null,
-    var verzenddatum: LocalDate? = null,
-    var ondertekeningDatum: LocalDate? = null,
-    var vertrouwelijkheidaanduiding: String? = null,
-    var auteur: String? = null,
-    var status: String? = null,
-    var formaat: String? = null,
-    var versie: Long = 0,
-    var bestandsnaam: String? = null,
-    var bestandsomvang: Long = 0,
-    var documentType: String? = null,
-    var ondertekeningSoort: String? = null,
-    var indicatieOndertekend: Boolean = false,
-    var inhoudUrl: String? = null,
-    var indicatieVergrendeld: Boolean = false,
-    var vergrendeldDoor: String? = null,
-    var indicaties: EnumSet<DocumentIndicatie>? = null,
-    var rechten: RestDocumentRechten? = null,
-    var indicatieGebruiksrecht: Boolean = false
+    val titel: String? = null,
+    val beschrijving: String? = null,
+    val zaaktypeUuid: String? = null,
+    val zaaktypeIdentificatie: String? = null,
+    val zaaktypeOmschrijving: String? = null,
+    val zaakIdentificatie: String? = null,
+    val zaakUuid: String? = null,
+    val zaakRelatie: String? = null,
+    val creatiedatum: LocalDate? = null,
+    val registratiedatum: LocalDate? = null,
+    val ontvangstdatum: LocalDate? = null,
+    val verzenddatum: LocalDate? = null,
+    val ondertekeningDatum: LocalDate? = null,
+    val vertrouwelijkheidaanduiding: String? = null,
+    val auteur: String? = null,
+    val status: String? = null,
+    val formaat: String? = null,
+    val versie: Long = 0,
+    val bestandsnaam: String? = null,
+    val bestandsomvang: Long = 0,
+    val documentType: String? = null,
+    val ondertekeningSoort: String? = null,
+    val indicatieOndertekend: Boolean = false,
+    val inhoudUrl: String? = null,
+    val indicatieVergrendeld: Boolean = false,
+    val vergrendeldDoor: String? = null,
+    val indicaties: EnumSet<DocumentIndicatie>? = null,
+    val rechten: RestDocumentRechten? = null,
+    val indicatieGebruiksrecht: Boolean = false
 ) : AbstractRestZoekObject(id, type, identificatie)
 
 fun DocumentZoekObject.toRestDocumentZoekObject(documentRechten: DocumentRechten) = RestDocumentZoekObject(
