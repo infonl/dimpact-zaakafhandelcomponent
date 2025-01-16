6025a9169a65b6f2fe01d107fd03fecbd233e1cf
Hristo Iliev <hristo.iliev@info.nl>
2025-01-16 18:23:09 +0200

    fix: use current translation keys

---
+++ src/test/kotlin/net/atos/zac/app/bag/converter/RESTAdreseerbaarObjectConverterTest.kt
@@ -0,0 +1,97 @@
+package net.atos.zac.app.bag.converter
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.checkUnnecessaryStub
+import net.atos.client.bag.model.generated.StatusPlaats
+import net.atos.client.bag.model.generated.StatusVerblijfsobject
+import net.atos.client.bag.model.generated.TypeAdresseerbaarObject
+import net.atos.zac.app.bag.createLigplaatsAdresseerbaarObject
+import net.atos.zac.app.bag.createStandplaatsAdresseerbaarObject
+import net.atos.zac.app.bag.createVerblijfsAdresseerbaarObject
+
+class RESTAdreseerbaarObjectConverterTest : BehaviorSpec({
+    val restAdreseerbaarObjectConverter = RESTAdreseerbaarObjectConverter()
+
+    beforeEach {
+        checkUnnecessaryStub()
+    }
+
+    Given("Ligplaats addressbaar object") {
+        val adresseerbaarObjectIOHal = createLigplaatsAdresseerbaarObject(StatusPlaats.AANGEWEZEN)
+
+        When("converted to rest representation") {
+            val result = restAdreseerbaarObjectConverter.convertToREST(adresseerbaarObjectIOHal)
+
+            Then("it should return the correct data") {
+                with(result) {
+                    typeAdresseerbaarObject shouldBe TypeAdresseerbaarObject.LIGPLAATS
+                    status shouldBe "Plaats aangewezen"
+                    vboDoel shouldBe null
+                    vboOppervlakte shouldBe 0
+                    with(geometry) {
+                        type shouldBe "Polygon"
+                        point shouldBe null
+                        with(polygon!!) {
+                            size shouldBe 1
+                            get(0)[0].latitude shouldBe 0.0
+                            get(0)[0].longitude shouldBe 0.0
+                        }
+                        geometrycollection shouldBe null
+                    }
+                }
+            }
+        }
+    }
+
+    Given("Standplaats addressbaar object") {
+        val adresseerbaarObjectIOHal = createStandplaatsAdresseerbaarObject(StatusPlaats.AANGEWEZEN)
+
+        When("converted to rest representation") {
+            val result = restAdreseerbaarObjectConverter.convertToREST(adresseerbaarObjectIOHal)
+
+            Then("it should return the correct data") {
+                with(result) {
+                    typeAdresseerbaarObject shouldBe TypeAdresseerbaarObject.STANDPLAATS
+                    status shouldBe "Plaats aangewezen"
+                    vboDoel shouldBe null
+                    vboOppervlakte shouldBe 0
+                    with(geometry) {
+                        type shouldBe "Polygon"
+                        point shouldBe null
+                        with(polygon!!) {
+                            size shouldBe 1
+                            get(0)[0].latitude shouldBe 0.0
+                            get(0)[0].longitude shouldBe 0.0
+                        }
+                        geometrycollection shouldBe null
+                    }
+                }
+            }
+        }
+    }
+
+    Given("Verblijfs addressbaar object") {
+        val adresseerbaarObjectIOHal = createVerblijfsAdresseerbaarObject(StatusVerblijfsobject.VERBLIJFSOBJECT_GEVORMD)
+
+        When("converted to rest representation") {
+            val result = restAdreseerbaarObjectConverter.convertToREST(adresseerbaarObjectIOHal)
+
+            Then("it should return the correct data") {
+                with(result) {
+                    typeAdresseerbaarObject shouldBe TypeAdresseerbaarObject.VERBLIJFSOBJECT
+                    status shouldBe "Verblijfsobject gevormd"
+                    vboDoel shouldBe ""
+                    vboOppervlakte shouldBe 0
+                    with(geometry) {
+                        type shouldBe "Point"
+                        polygon shouldBe null
+                        point!!.latitude shouldBe 0.0
+                        point!!.longitude shouldBe 0.0
+                        geometrycollection shouldBe null
+                    }
+                }
+            }
+        }
+    }
+})
