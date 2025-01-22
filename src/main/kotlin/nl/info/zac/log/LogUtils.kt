a6dad2d1bc0582d676714d638e0877d1392ea658
Edgar Vonk <edgar@info.nl>
2025-01-22 17:00:52 +0100

    chore: refactored code a bit

--- src/main/kotlin/nl/info/zac/log/LogUtils.kt
+++ src/main/kotlin/nl/info/zac/log/LogUtils.kt
@@ -9,7 +9,7 @@ import java.util.logging.Level
 import java.util.logging.Logger
 
 /**
- * Simple wapper function around the Java Util Logger class to make it easier to unit test classes that use logging.
+ * Simple wapper function around [Logger.log] to make it easier to unit test classes that use logging.
  */
 fun log(logger: Logger, level: Level, message: String, throwable: Throwable) {
     logger.log(level, message, throwable)
