1f438db76f0ab6fe8b9d083b560bfd19dac8b427
Hristo Iliev <hristo.iliev@info.nl>
2025-01-28 12:25:36 +0200

    fix: set user/password as needed for mail session

--- src/main/kotlin/net/atos/zac/mail/MailMessageBuilder.kt
+++ src/main/kotlin/net/atos/zac/mail/MailMessageBuilder.kt
@@ -6,7 +6,9 @@ package net.atos.zac.mail
 
 import jakarta.mail.Address
 import jakarta.mail.Message
+import jakarta.mail.PasswordAuthentication
 import jakarta.mail.Session
+import jakarta.mail.URLName
 import jakarta.mail.internet.MimeBodyPart
 import jakarta.mail.internet.MimeMessage
 import jakarta.mail.internet.MimeMultipart
@@ -24,32 +26,52 @@ class MailMessageBuilder(
     private var body: String,
     private var attachments: List<Attachment>
 ) {
-    fun build(mailSession: Session) = MimeMessage(mailSession).apply {
-        addHeader("Content-type", "${MediaType.TEXT_HTML}; charset=${StandardCharsets.UTF_8.name()}")
-        addHeader("format", "flowed")
-        addHeader("Content-Transfer-Encoding", "8bit")
+    private fun setupPasswordAuthentication(mailSession: Session) {
+        val userName = System.getenv("SMTP_USERNAME")
+        val password = System.getenv("SMTP_PASSWORD")
+        userName?.let {
+            mailSession.properties.setProperty("mail.smtp.user", userName)
+            mailSession.properties.setProperty("mail.smtp.auth", "true")
+        }
+        password?.let {
+            val authSection = if (userName != null) "$userName@" else ""
+            val smtpServerHost = System.getenv("SMTP_SERVER")
+            mailSession.setPasswordAuthentication(
+                URLName("smtp://$authSection$smtpServerHost"),
+                PasswordAuthentication(userName, password)
+            )
+            mailSession.properties.setProperty("mail.smtp.auth", "true")
+        }
+    }
 
-        setFrom(fromAddress)
-        setRecipients(Message.RecipientType.TO, arrayOf(toAddress))
-        replyTo = replyToAddress?.let { arrayOf(replyToAddress) }
-        subject = mailSubject
-        sentDate = Date()
+    fun build(mailSession: Session) = setupPasswordAuthentication(mailSession).let {
+        MimeMessage(mailSession).apply {
+            addHeader("Content-type", "${MediaType.TEXT_HTML}; charset=${StandardCharsets.UTF_8.name()}")
+            addHeader("format", "flowed")
+            addHeader("Content-Transfer-Encoding", "8bit")
 
-        setContent(
-            MimeMultipart().apply {
-                addBodyPart(
-                    MimeBodyPart().apply {
-                        setText(body, StandardCharsets.UTF_8.name(), "html")
+            setFrom(fromAddress)
+            setRecipients(Message.RecipientType.TO, arrayOf(toAddress))
+            replyTo = replyToAddress?.let { arrayOf(replyToAddress) }
+            subject = mailSubject
+            sentDate = Date()
+
+            setContent(
+                MimeMultipart().apply {
+                    addBodyPart(
+                        MimeBodyPart().apply {
+                            setText(body, StandardCharsets.UTF_8.name(), "html")
+                        }
+                    )
+                    attachments.forEach {
+                        val messageBodyPart = PreencodedMimeBodyPart("base64").apply {
+                            setContent(it.base64Content, it.contentType)
+                            fileName = it.filename
+                        }
+                        addBodyPart(messageBodyPart)
                     }
-                )
-                attachments.forEach {
-                    val messageBodyPart = PreencodedMimeBodyPart("base64").apply {
-                        setContent(it.base64Content, it.contentType)
-                        fileName = it.filename
-                    }
-                    addBodyPart(messageBodyPart)
                 }
-            }
-        )
+            )
+        }
     }
 }
