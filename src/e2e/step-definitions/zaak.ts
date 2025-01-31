b3de7636a098f70b1e772a733a2d89226ff83b6a
Bas de Wit <bas@info.nl>
2025-01-31 11:00:58 +0100

    fix(e2e): select correct for aanvullende informatie nodig voor zaak

--- src/e2e/step-definitions/zaak.ts
+++ src/e2e/step-definitions/zaak.ts
@@ -113,7 +113,8 @@ When(
 
     await this.expect(
       this.page.getByRole('cell', { name: 'Aanvullende informatie' })
-          .toBeVisible({ timeout: FIFTEEN_SECONDS_IN_MS });
+    )
+        .toBeVisible({ timeout: FIFTEEN_SECONDS_IN_MS });
     await checkZaakAssignment.call(this, zaakNumber, user2Profile);
   },
 );
@@ -217,7 +218,9 @@ Then(
     await this.page.goto(`${this.worldParameters.urls.zac}/taken/mijn`);
 
     await this.expect(
-      this.page.getByRole("cell", { name: caseNumber, exact: true }).first(),
+      this.page
+          .getByRole("cell", { name: caseNumber, exact: true })
+          .first(),
     ).toBeVisible({ timeout: FIFTEEN_SECONDS_IN_MS });
 
     await this.expect(
