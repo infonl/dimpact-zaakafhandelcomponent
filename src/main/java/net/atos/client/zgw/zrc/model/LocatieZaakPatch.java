77a482449cea6ceedcba887cb3dfd19cfba7b8ec
Edgar Vonk <edgar@info.nl>
2025-02-02 19:59:21 +0100

    chore: formatting only

--- src/main/java/net/atos/client/zgw/zrc/model/LocatieZaakPatch.java
+++ src/main/java/net/atos/client/zgw/zrc/model/LocatieZaakPatch.java
@@ -1,10 +1,8 @@
 /*
- * SPDX-FileCopyrightText: 2021 Atos
+ * SPDX-FileCopyrightText: 2021 Atos, 2025 Lifely
  * SPDX-License-Identifier: EUPL-1.2+
  */
-
 package net.atos.client.zgw.zrc.model;
-
 
 import jakarta.json.bind.annotation.JsonbNillable;
 
@@ -12,7 +10,6 @@ import jakarta.json.bind.annotation.JsonbNillable;
  * Zaak locatie patch data
  */
 public class LocatieZaakPatch extends Zaak {
-
     private final Geometry zaakgeometrie;
 
     public LocatieZaakPatch(final Geometry zaakgeometrie) {
