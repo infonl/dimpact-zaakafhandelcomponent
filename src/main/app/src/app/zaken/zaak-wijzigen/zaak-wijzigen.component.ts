ad276e7cdca605467d22afbd0129c0f02100ba92
infonl-marcel <marcele@info.nl>
2025-02-11 22:04:41 +0100

    added fields

--- src/main/app/src/app/zaken/zaak-wijzigen/zaak-wijzigen.component.ts
+++ src/main/app/src/app/zaken/zaak-wijzigen/zaak-wijzigen.component.ts
@@ -3,26 +3,17 @@
  * SPDX-License-Identifier: EUPL-1.2+
  */
 
-import {
-  AfterViewInit,
-  Component,
-  Input,
-  OnDestroy,
-  OnInit,
-} from "@angular/core";
+import { Component, Input, OnDestroy, OnInit } from "@angular/core";
 import { MatDrawer } from "@angular/material/sidenav";
 import { FormGroup, Validators } from "@angular/forms";
-import { FormControl } from "@angular/forms";
 import {} from "../../shared/location/location.service";
 import { GeneratedType } from "src/app/shared/utils/generated-types";
 import { MedewerkerGroepFormField } from "src/app/shared/material-form-builder/form-components/medewerker-groep/medewerker-groep-form-field";
 import { FormConfig } from "src/app/shared/material-form-builder/model/form-config";
 import { FormConfigBuilder } from "src/app/shared/material-form-builder/model/form-config-builder";
 import { Observable, Subject } from "rxjs";
-import { Select } from "ol/interaction";
 import { SelectFormField } from "src/app/shared/material-form-builder/form-components/select/select-form-field";
 import { ReferentieTabelService } from "src/app/admin/referentie-tabel.service";
-import { TranslateService } from "@ngx-translate/core";
 import { UtilService } from "src/app/core/service/util.service";
 import { Vertrouwelijkheidaanduiding } from "src/app/informatie-objecten/model/vertrouwelijkheidaanduiding.enum";
 import { MedewerkerGroepFieldBuilder } from "src/app/shared/material-form-builder/form-components/medewerker-groep/medewerker-groep-field-builder";
@@ -33,13 +24,16 @@ import { TextareaFormField } from "src/app/shared/material-form-builder/form-com
 import { AbstractFormField } from "src/app/shared/material-form-builder/model/abstract-form-field";
 import { NavigationService } from "src/app/shared/navigation/navigation.service";
 import { OrderUtil } from "src/app/shared/order/order-util";
+import { DateFormFieldBuilder } from "src/app/shared/material-form-builder/form-components/date/date-form-field-builder";
+import { DateFormField } from "src/app/shared/material-form-builder/form-components/date/date-form-field";
+import { InputFormField } from "src/app/shared/material-form-builder/form-components/input/input-form-field";
 
 @Component({
   selector: "zac-case-details-edit",
   templateUrl: "./zaak-wijzigen.component.html",
   styleUrls: ["./zaak-wijzigen.component.less"],
 })
-export class ZaakWijzigenComponent implements OnInit, AfterViewInit, OnDestroy {
+export class ZaakWijzigenComponent implements OnInit, OnDestroy {
   @Input() sideNav: MatDrawer;
   @Input() readonly: boolean;
   @Input() zaak: GeneratedType<"RestZaak">;
@@ -50,15 +44,19 @@ export class ZaakWijzigenComponent implements OnInit, AfterViewInit, OnDestroy {
   private medewerkerGroepFormField: MedewerkerGroepFormField;
   private communicatiekanalen: Observable<string[]>;
   private communicatiekanaalField: SelectFormField;
+  private startdatum: DateFormField;
+  private einddatumGepland: DateFormField | InputFormField;
+  private uiterlijkeEinddatumAfdoening: DateFormField;
   private vertrouwelijkheidaanduidingField: SelectFormField;
   private vertrouwelijkheidaanduidingen: { label: string; value: string }[];
+  private omschrijving: TextareaFormField;
   private toelichtingField: TextareaFormField;
+  private redenField: TextareaFormField;
   private ngDestroy = new Subject<void>();
 
   constructor(
     private referentieTabelService: ReferentieTabelService,
     private navigation: NavigationService,
-    private translateService: TranslateService,
     private utilService: UtilService,
   ) {}
 
@@ -68,18 +66,15 @@ export class ZaakWijzigenComponent implements OnInit, AfterViewInit, OnDestroy {
     this.initForm();
   }
 
-  ngAfterViewInit(): void {}
-
   private initForm(): void {
     this.formConfig = new FormConfigBuilder()
       .saveText("actie.aanmaken")
       .cancelText("actie.annuleren")
+      .requireUserChanges(true)
       .build();
 
-    this.communicatiekanalen = this.referentieTabelService
-      .listCommunicatiekanalen
-      // this.inboxProductaanvraag != null,
-      ();
+    this.communicatiekanalen =
+      this.referentieTabelService.listCommunicatiekanalen();
 
     this.vertrouwelijkheidaanduidingen = this.utilService.getEnumAsSelectList(
       "vertrouwelijkheidaanduiding",
@@ -94,10 +89,39 @@ export class ZaakWijzigenComponent implements OnInit, AfterViewInit, OnDestroy {
     this.communicatiekanaalField = new SelectFormFieldBuilder(
       this.zaak.communicatiekanaal,
     )
-      // ZaakCreateComponent.KANAAL_E_FORMULIER,
       .id("communicatiekanaal")
       .label("communicatiekanaal")
       .options(this.communicatiekanalen)
+      .validators(Validators.required)
+      .build();
+
+    this.startdatum = new DateFormFieldBuilder(this.zaak.startdatum)
+      .id("startdatum")
+      .label("startdatum")
+      .validators(Validators.required)
+      .build();
+
+    this.einddatumGepland = this.zaak.einddatumGepland
+      ? new DateFormFieldBuilder(this.zaak.einddatumGepland)
+          .id("einddatumGepland")
+          .label("einddatumGepland")
+          .validators(
+            this.zaak.einddatumGepland
+              ? Validators.required
+              : Validators.nullValidator,
+          )
+          .build()
+      : new InputFormFieldBuilder("-")
+          .id("einddatumGepland")
+          .label("einddatumGepland")
+          .disabled()
+          .build();
+
+    this.uiterlijkeEinddatumAfdoening = new DateFormFieldBuilder(
+      this.zaak.uiterlijkeEinddatumAfdoening,
+    )
+      .id("uiterlijkeEinddatumAfdoening")
+      .label("uiterlijkeEinddatumAfdoening")
       .validators(Validators.required)
       .build();
 
@@ -114,7 +138,7 @@ export class ZaakWijzigenComponent implements OnInit, AfterViewInit, OnDestroy {
       .validators(Validators.required)
       .build();
 
-    const omschrijving = new InputFormFieldBuilder(this.zaak.omschrijving)
+    this.omschrijving = new InputFormFieldBuilder(this.zaak.omschrijving)
       .id("omschrijving")
       .label("omschrijving")
       .maxlength(80)
@@ -127,17 +151,38 @@ export class ZaakWijzigenComponent implements OnInit, AfterViewInit, OnDestroy {
       .maxlength(1000)
       .build();
 
+    this.redenField = new InputFormFieldBuilder()
+      .id("reden")
+      .label("reden")
+      .maxlength(80)
+      .validators(Validators.required)
+      .build();
+
     this.formFields = [
-      [this.medewerkerGroepFormField],
-      [this.communicatiekanaalField, this.vertrouwelijkheidaanduidingField],
-      [omschrijving],
+      [this.medewerkerGroepFormField, this.communicatiekanaalField],
+      [
+        this.startdatum,
+        this.einddatumGepland,
+        this.uiterlijkeEinddatumAfdoening,
+      ],
+      [this.vertrouwelijkheidaanduidingField],
+      [this.omschrijving],
       [this.toelichtingField],
+      [this.redenField],
     ];
   }
 
   onFormSubmit(formGroup: FormGroup): void {
     if (formGroup) {
-      this.navigation.back();
+      const changedValues = {};
+      for (const [key, control] of Object.entries(formGroup.controls)) {
+        if (control.dirty) {
+          changedValues[key] = control.value;
+        }
+      }
+
+      console.log("changedValues", changedValues);
+      this.sideNav.close();
     }
   }
 
