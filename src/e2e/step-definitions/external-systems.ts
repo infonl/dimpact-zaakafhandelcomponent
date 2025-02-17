038335108708832e75cf4aa6f630ff9057dd45bc
Dax Riool <dax_riool@hotmail.com>
2025-02-17 13:38:01 +0100

    chore: replaced id selectors with more accurate and user friendly ones

--- src/e2e/step-definitions/external-systems.ts
+++ src/e2e/step-definitions/external-systems.ts
@@ -9,7 +9,7 @@ import { CustomWorld } from "../support/worlds/world";
 
 const ONE_MINUTE_IN_MS = 60_000;
 const TWENTY_SECONDS_IN_MS = 20_000;
-
+const SLOW_LAPTOP_TIMEOUT = 100_000;
 let smartDocumentsWizardPage: Page;
 
 const documentInput = {
@@ -20,21 +20,24 @@ const documentInput = {
 
 When(
   "Employee {string} clicks on Create Document button for the new zaak",
-  { timeout: ONE_MINUTE_IN_MS },
+  { timeout: SLOW_LAPTOP_TIMEOUT },
   async function (this: CustomWorld, user) {
     await this.page.getByText("note_addDocument maken").click();
 
     const sidebar = this.page.locator("div.sidenav-title");
     await sidebar.waitFor({ state: "visible" });
     await sidebar.getByText("Document maken");
-  },
+  }
 );
 
 When(
   "Employee {string} enters and submits the form to start the SmartDocuments wizard",
-  { timeout: ONE_MINUTE_IN_MS },
+  { timeout: SLOW_LAPTOP_TIMEOUT },
   async function (this: CustomWorld, user) {
-    const submitButton = this.page.locator("#opslaan_button");
+    const submitButton = this.page.getByRole("button", {
+      name: /Toevoegen|Add/, // Matches "Toevoegen" OR "Add"
+    });
+
     await submitButton.waitFor({ state: "visible" });
 
     await this.expect(submitButton).toBeDisabled();
@@ -48,54 +51,55 @@ When(
     await this.page.getByLabel("Sjabloon").last().click();
     await this.page.getByRole("option", { name: "OpenZaakTest" }).click();
 
-    const inputTitle = this.page.locator("#title_tekstfield");
+    const inputTitle = this.page.getByLabel(/Titel|Title/i);
     await inputTitle.click();
     await inputTitle.fill(documentInput.title);
     await expect(inputTitle).toHaveValue(documentInput.title);
 
-    const inputDescription = this.page.locator("#description_tekstfield");
+    const inputDescription = this.page.getByLabel(/Beschrijving|Description/i);
     await inputDescription.click();
     await inputDescription.fill(documentInput.description);
     await expect(inputDescription).toHaveValue(documentInput.description);
 
-    const inputAuthor = this.page.locator("#author_tekstfield");
+    const inputAuthor = this.page.getByLabel(/Auteur|Author/i);
     await inputAuthor.click();
     await inputAuthor.fill(documentInput.author);
     await expect(inputAuthor).toHaveValue(documentInput.author);
 
     await this.expect(submitButton).toBeEnabled();
-    await this.page.click("#opslaan_button");
-  },
+    await submitButton.click();
+  }
 );
 
 When(
   "Employee {string} completes the SmartDocuments wizard",
-  { timeout: ONE_MINUTE_IN_MS },
+  { timeout: SLOW_LAPTOP_TIMEOUT },
   async function (this: CustomWorld, user) {
     smartDocumentsWizardPage = await this.page.waitForEvent("popup");
     await this.expect(
-      smartDocumentsWizardPage.getByRole("link", { name: "SmartDocuments" }),
+      smartDocumentsWizardPage.getByRole("link", { name: "SmartDocuments" })
     ).toBeVisible();
 
-    const klaarButton = smartDocumentsWizardPage.locator(
-      "#gwt-debug-wizardEngine_panelControls_nextButton",
-    );
+    const klaarButton = smartDocumentsWizardPage.getByRole("button", {
+      name: /Klaar|Done/, // Matches "Klaar" OR "Done"
+      });
+
     await klaarButton.waitFor({ state: "visible" });
     await klaarButton.click();
-  },
+  }
 );
 
 When(
   "Employee {string} closes the wizard result page",
-  { timeout: ONE_MINUTE_IN_MS },
+  { timeout: SLOW_LAPTOP_TIMEOUT },
   async function (this: CustomWorld, user) {
     const caseNumber = this.testStorage.get("caseNumber");
     const caseNumberLocator = smartDocumentsWizardPage.locator(
-      `text=${caseNumber}`,
+      `text=${caseNumber}`
     );
     await expect(caseNumberLocator).toHaveCount(2);
 
-    const wizardResultDiv = smartDocumentsWizardPage.locator("#wizard-result");
+    const wizardResultDiv = smartDocumentsWizardPage.locator('[role="status"][aria-live="polite"]');
     await wizardResultDiv.waitFor({ state: "attached" });
     await expect(wizardResultDiv).toBeVisible();
 
@@ -103,12 +107,12 @@ When(
     await expect(wizardResultDiv.getByText("succes")).toBeVisible();
 
     await smartDocumentsWizardPage.close();
-  },
+  }
 );
 
 When(
   "Employee {string} views the created document",
-  { timeout: ONE_MINUTE_IN_MS },
+  { timeout: SLOW_LAPTOP_TIMEOUT },
   async function (this: CustomWorld, user) {
     const caseNumber = this.testStorage.get("caseNumber");
     const caseNumberLocator = this.page.locator(`text=${caseNumber}`);
@@ -123,12 +127,12 @@ When(
 
     const anchorLocator = this.page.locator('a[title="Document bekijken"]');
     await anchorLocator.click();
-  },
+  }
 );
 
 Then(
   "Employee {string} sees all added details in the created document meta data",
-  { timeout: ONE_MINUTE_IN_MS },
+  { timeout: SLOW_LAPTOP_TIMEOUT },
   async function (this: CustomWorld, user) {
     const tabPanelLocator = this.page
       .locator('mat-tab-body[role="tabpanel"]')
@@ -137,21 +141,21 @@ Then(
     await expect(tabPanelLocator).toBeVisible();
 
     const documentTitleText = tabPanelLocator.locator(
-      `text=${documentInput.title}`,
+      `text=${documentInput.title}`
     );
     await documentTitleText.waitFor({ state: "attached" });
     await expect(documentTitleText).toBeVisible();
 
     const documnentDescriptionText = tabPanelLocator.locator(
-      `text=${documentInput.description}`,
+      `text=${documentInput.description}`
     );
     await documnentDescriptionText.waitFor({ state: "attached" });
     await expect(documnentDescriptionText).toBeVisible();
 
     const documnentAuthor = tabPanelLocator.locator(
-      `text=${documentInput.author}`,
+      `text=${documentInput.author}`
     );
     await documnentAuthor.waitFor({ state: "attached" });
     await expect(documnentAuthor).toBeVisible();
-  },
+  }
 );
