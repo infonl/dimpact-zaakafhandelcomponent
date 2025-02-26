a2c4da81a1c1eedfabd928a62fc418bd9687df21
Hristo Iliev <hristo.iliev@info.nl>
2025-02-26 18:57:50 +0200

    feature: add JWTTokenGenerator test
    Add JWTTokenGenerator unit test
    
    Solves PZ-5694

---
+++ src/test/kotlin/net/atos/client/util/JWTTokenGeneratorTest.kt
@@ -0,0 +1,27 @@
+package net.atos.client.util
+
+import com.auth0.jwt.JWT
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+
+class JWTTokenGeneratorTest : BehaviorSpec({
+
+    Given("client id and secret and no logged-in user") {
+
+        When("token is generated") {
+            val token = JWTTokenGenerator.generate("clientId", "secret", null)
+
+            Then("it should be a valid JWT token") {
+                println(token)
+                val tokenParts = token.split(" ")
+                tokenParts[0] shouldBe "Bearer"
+                val decodedToken = JWT.decode(tokenParts[1])
+                with(decodedToken) {
+                    issuer shouldBe "clientId"
+                    getHeaderClaim("client_identifier").asString() shouldBe "clientId"
+                }
+            }
+        }
+    }
+})
