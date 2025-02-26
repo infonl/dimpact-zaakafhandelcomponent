ab6e0694da718578fb4ed5bff8eea968cc827095
Edgar Vonk <edgar@info.nl>
2025-02-26 17:36:22 +0100

    fix: fix Open Notificaties in Docker Compose setup (#2780)
    Fix OpenNotificaties in Docker Compose setup for local testing. Includes
    a workaround for initialising the 'ZAC abonnement' in the
    OpenNotificaties database until OpenNotificaties support setting the
    `client_id` field in the YAML-based setup configuration.
    
    Solves PZ-5591

--- scripts/docker-compose/imports/opennotificaties-database/database/1-setup-applicatie.sql
+++ scripts/docker-compose/imports/opennotificaties-database/database/1-setup-applicatie.sql
@@ -1,22 +1,7 @@
--- create superuser to be able to log in to the UI with username admin and password admin
-INSERT INTO accounts_user (id, password, last_login, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined) VALUES (1, 'pbkdf2_sha256$260000$gtIe19cI1vW9RzIsRDpriC$o8G6cItI5vXqbGFcXuu0pbullajpvMDc6Hze70mf+jE=', null, true, 'admin', '', '', 'admin@example.com', true, true, '2023-08-08 15:14:56.735552 +00:00');
-
--- Set up the Autorisatiecomponentconfiguratie
--- Unfortunately it seems that we need to use 'host.docker.internal' here to connect to Open Zaak. Not sure why.
--- Please see our 'testing.md' document on how to set this up.
-INSERT INTO authorizations_authorizationsconfig (api_root, component) VALUES('http://host.docker.internal:8001/autorisaties/api/v1/', 'ac');
-
--- Set up the Notificatiescomponentconfiguratie
--- We assume here that a record already exists with id=1 (this is provisioned by OpenNotificaties on startup)
-UPDATE notifications_api_common_notificationsconfig SET notifications_api_service_id=(SELECT id FROM zgw_consumers_service WHERE label = 'notificaties-self'), notification_delivery_max_retries=5, notification_delivery_retry_backoff=3, notification_delivery_retry_backoff_max=48 WHERE id=1;
-
--- Set up the External API credentials
--- Unfortunately it seems that we need to use 'host.docker.internal' here to connect to Open Zaak. Not sure why.
--- Please see our 'testing.md' document on how to set this up.
-INSERT INTO vng_api_common_apicredential (api_root, client_id, secret, label, user_id, user_representation) VALUES('http://host.docker.internal:8001/autorisaties/api/v1/', 'open-zaak-autorisaties', 'openZaakAutorisatiesApiSecretKey', 'Open Zaak - Autorisaties', 'open-zaak-autorisaties', 'Open Zaak - Autorisaties');
-
--- Set up the Autorisatiegegevens
-INSERT INTO vng_api_common_jwtsecret (identifier, secret) VALUES('open-zaak-autorisaties', 'openZaakAutorisatiesApiSecretKey');
+-- Script to initialize kanalen and abonnement data only in Open Notificaties database
+-- Temporary workaround until Open Notificaties setup configuration files support setting the 'client_id' for abonnementen.
+-- Once that is supported in Open Notificaties, we can remove this script and use extend (../../opennotificaties/setup-configuration/data.yaml) to set up the kanalen and abonnement.
+-- We need to set up the kanalen here as well because the abonnement references the kanalen.
 
 -- Set up the kanalen
 INSERT INTO datamodel_kanaal (uuid, naam, documentatie_link, filters) VALUES('493002ad-e5d5-4747-93b2-1853e78889f5', 'zaaktypen', 'http://open-zaak-zac-dev.westeurope.cloudapp.azure.com/ref/kanalen/#zaaktypen', '{catalogus}');
--- scripts/docker-compose/imports/openzaak-database/database/1-setup-zac-config.sql
+++ scripts/docker-compose/imports/openzaak-database/database/1-setup-zac-config.sql
@@ -4,11 +4,11 @@ INSERT INTO catalogi_catalogus (naam, uuid, domein, rsin, contactpersoon_beheer_
     ('zac', '8225508a-6840-413e-acc9-6422af120db1', 'ALG', '002564440', 'ZAC Test Catalogus', '06-12345678', 'noreply@example.com', '_etag', NULL, '');
 
 INSERT INTO authorizations_applicatie (uuid, client_ids, label, heeft_alle_autorisaties) VALUES (uuid_generate_v4(), '{zac_client}', 'ZAC', true);
-INSERT INTO authorizations_applicatie (uuid, client_ids, label, heeft_alle_autorisaties) VALUES (uuid_generate_v4(), '{open-zaak-autorisaties}', 'Open Zaak - Autorisaties', true);
+INSERT INTO authorizations_applicatie (uuid, client_ids, label, heeft_alle_autorisaties) VALUES (uuid_generate_v4(), '{open-zaak}', 'Open Zaak', true);
 INSERT INTO authorizations_applicatie (uuid, client_ids, label, heeft_alle_autorisaties) VALUES (uuid_generate_v4(), '{open-archiefbeheer}', 'Open Archiefbeheer', true);
-INSERT INTO authorizations_applicatie (uuid, client_ids, label, heeft_alle_autorisaties) VALUES (uuid_generate_v4(), '{opennotificaties}', 'Open notificaties', true);
+INSERT INTO authorizations_applicatie (uuid, client_ids, label, heeft_alle_autorisaties) VALUES (uuid_generate_v4(), '{open-notificaties}', 'Open notificaties', true);
 
 INSERT INTO vng_api_common_jwtsecret (identifier, secret) VALUES ('zac_client', 'openzaakZaakafhandelcomponentClientSecret');
-INSERT INTO vng_api_common_jwtsecret (identifier, secret) VALUES ('open-zaak-autorisaties', 'openZaakAutorisatiesApiSecretKey');
+INSERT INTO vng_api_common_jwtsecret (identifier, secret) VALUES ('open-zaak', 'opennotificatiesOpenzaakSecret');
 INSERT INTO vng_api_common_jwtsecret (identifier, secret) VALUES ('open-archiefbeheer', 'openArchiefbeheerApiSecretKey');
-INSERT INTO vng_api_common_jwtsecret (identifier, secret) VALUES ('opennotificaties', 'openNotificatiesApiSecretKey');
+INSERT INTO vng_api_common_jwtsecret (identifier, secret) VALUES ('open-notificaties', 'opennotificatiesAutorisatieApiSecret');
