41b53c5510a0e8544646675a194cb8b923b23bbb
sander boer <mail@sanderboer.nl>
2025-02-26 11:10:55 +0100

    feat(ZaakafhandelParameters): validate `behandelaar` (#2772)
    Make sure the selected `behandelaar` is in the `group`.
    
    Solves PZ-5067

--- src/main/kotlin/nl/info/zac/exception/ErrorCode.kt
+++ src/main/kotlin/nl/info/zac/exception/ErrorCode.kt
@@ -40,5 +40,6 @@ enum class ErrorCode(val value: String) {
     ERROR_CODE_CASE_HAS_OPEN_SUBCASES("msg.error.case.has.open.subcases"),
     ERROR_CODE_CASE_HAS_LOCKED_INFORMATION_OBJECTS("msg.error.case.has.locked.information.objects"),
     ERROR_CODE_SEARCH_INDEXING("msg.error.search.indexing.exception"),
-    ERROR_CODE_SEARCH_SEARCH("msg.error.search.search.exception")
+    ERROR_CODE_SEARCH_SEARCH("msg.error.search.search.exception"),
+    ERROR_CODE_USER_NOT_IN_GROUP("msg.error.user.not.in.group"),
 }
