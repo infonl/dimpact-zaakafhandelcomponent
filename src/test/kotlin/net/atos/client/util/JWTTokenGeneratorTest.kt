bf634a3ea30a14d5bf015315bac2ccfc6c744082
Hristo Iliev <hristo.iliev@info.nl>
2025-02-26 19:58:22 +0200

    fix: spotless

--- src/test/kotlin/net/atos/client/util/JWTTokenGeneratorTest.kt
+++ src/test/kotlin/net/atos/client/util/JWTTokenGeneratorTest.kt
@@ -8,14 +8,25 @@ package net.atos.client.util
 import com.auth0.jwt.JWT
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.shouldNotBe
+import io.mockk.checkUnnecessaryStub
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import net.atos.zac.authentication.LoggedInUser
+import net.atos.zac.identity.model.getFullName
 
 class JWTTokenGeneratorTest : BehaviorSpec({
 
+    beforeEach {
+        checkUnnecessaryStub()
+    }
+
     Given("client id and secret and no logged-in user") {
+        val clientId = "clientId"
+        val clientSecret = "clientSecret"
 
         When("token is generated") {
-            val token = JWTTokenGenerator.generate("clientId", "secret", null)
+            val token = JWTTokenGenerator.generate(clientId, clientSecret, null)
 
             Then("it should be a valid JWT token") {
                 println(token)
@@ -23,8 +34,35 @@ class JWTTokenGeneratorTest : BehaviorSpec({
                 tokenParts[0] shouldBe "Bearer"
                 val decodedToken = JWT.decode(tokenParts[1])
                 with(decodedToken) {
-                    issuer shouldBe "clientId"
-                    getHeaderClaim("client_identifier").asString() shouldBe "clientId"
+                    issuer shouldBe clientId
+                    getHeaderClaim("client_identifier").asString() shouldBe clientId
+                }
+            }
+        }
+    }
+
+    Given("client credentials and logged-in user") {
+        val clientId = "clientId"
+        val clientSecret = "clientSecret"
+        val loggedInUser = mockk<LoggedInUser>()
+
+        every { loggedInUser.id } returns "id"
+        mockkStatic(LoggedInUser::getFullName)
+        every { loggedInUser.getFullName() } returns "fullName"
+
+        When("token is generated") {
+            val token = JWTTokenGenerator.generate(clientId, clientSecret, loggedInUser)
+
+            Then("it should be a valid JWT token") {
+                println(token)
+                val tokenParts = token.split(" ")
+                tokenParts[0] shouldBe "Bearer"
+                val decodedToken = JWT.decode(tokenParts[1])
+                with(decodedToken) {
+                    issuer shouldBe clientId
+                    getHeaderClaim("client_identifier").asString() shouldBe clientId
+                    getClaim("user_id").asString() shouldBe "id"
+                    getClaim("user_representation").asString() shouldBe "fullName"
                 }
             }
         }
