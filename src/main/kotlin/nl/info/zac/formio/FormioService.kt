dc81b08271d1d2888770a88f037425fa77841c30
Edgar Vonk <edgar@info.nl>
2025-03-18 15:57:06 +0100

    chore: moved code; added unit test

--- src/main/kotlin/nl/info/zac/formio/FormioService.kt
+++ src/main/kotlin/nl/info/zac/formio/FormioService.kt
@@ -1,10 +1,7 @@
 /*
- *
- *  * SPDX-FileCopyrightText: 2025 Lifely
- *  * SPDX-License-Identifier: EUPL-1.2+
- *
+ * SPDX-FileCopyrightText: 2025 Lifely
+ * SPDX-License-Identifier: EUPL-1.2+
  */
-
 package nl.info.zac.formio
 
 import jakarta.enterprise.context.ApplicationScoped
