1928d94a2b05b136b4012c223c6aab31526f4006
Edgar Vonk <edgar@info.nl>
2025-03-18 15:54:26 +0100

    chore: moved code; added unit test

--- src/main/kotlin/nl/info/zac/app/admin/FormioFormulierenRestService.kt
+++ src/main/kotlin/nl/info/zac/app/admin/FormioFormulierenRestService.kt
@@ -2,8 +2,7 @@
  * SPDX-FileCopyrightText: 2024 Dimpact
  * SPDX-License-Identifier: EUPL-1.2+
  */
-
-package net.atos.zac.app.admin
+package nl.info.zac.app.admin
 
 import jakarta.inject.Inject
 import jakarta.inject.Singleton
