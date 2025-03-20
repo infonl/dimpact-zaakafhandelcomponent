7c2f5463c5f519df0f0d62d661886fe98b95391d
Hristo Iliev <hsiliev@gmail.com>
2025-03-20 18:09:39 +0200

    fix: make ADDITIONAL_ALLOWED_FILE_TYPES optional (#2929)
    ADDITIONAL_ALLOWED_FILE_TYPES with no value causes ZAC to fail to start
    now
    
    Solves PZ-5951
    
    ---------
    
    Co-authored-by: Hristo Iliev <hristo.iliev@info.nl>

--- src/test/kotlin/nl/info/zac/configuratie/ConfiguratieServiceTest.kt
+++ src/test/kotlin/nl/info/zac/configuratie/ConfiguratieServiceTest.kt
@@ -16,6 +16,7 @@ import nl.info.client.zgw.ztc.ZtcClientService
 import nl.info.client.zgw.ztc.model.CatalogusListParameters
 import nl.info.client.zgw.ztc.model.generated.Catalogus
 import java.net.URI
+import java.util.Optional
 import java.util.UUID
 
 class ConfiguratieServiceTest : BehaviorSpec({
@@ -23,7 +24,7 @@ class ConfiguratieServiceTest : BehaviorSpec({
     val ztcClientService = mockk<ZtcClientService>()
     val catalogus = mockk<Catalogus>()
 
-    val additionalAllowedFileTypes = ""
+    val additionalAllowedFileTypes = Optional.of("dummyFileType1,dummyFileType2")
     val zgwApiClientMpRestUrl = "https://example.com:1111"
     val contextUrl = "https://example.com:2222"
     val gemeenteCode = "gemeenteCode"
@@ -83,6 +84,14 @@ class ConfiguratieServiceTest : BehaviorSpec({
                 informatieobjectTonenUrl.toString() shouldBe "$contextUrl/informatie-objecten/$uuid"
             }
         }
+
+        When("additional allowed file types are requested") {
+            val fileTypes = configurationService.readAdditionalAllowedFileTypes()
+
+            Then("Correct list is returned") {
+                fileTypes shouldBe listOf("dummyFileType1", "dummyFileType2")
+            }
+        }
     }
 
     Given("An invalid bron organisatie BSN") {
@@ -109,6 +118,39 @@ class ConfiguratieServiceTest : BehaviorSpec({
                         catalogusDomein
                     )
                 }
+            }
+        }
+    }
+
+    Given("An empty additional file list") {
+        val catalogusUri = "https://example.com/catalogus"
+        every { catalogus.url } returns URI(catalogusUri)
+        every { ztcClientService.readCatalogus(any<CatalogusListParameters>()) } returns catalogus
+
+        val bronOrganisatie = "123443210"
+        val verantwoordelijkeOrganisatie = "316245124"
+        val catalogusDomein = "ALG"
+
+        val configurationService = ConfiguratieService(
+            entityManager,
+            ztcClientService,
+            Optional.empty(),
+            zgwApiClientMpRestUrl,
+            contextUrl,
+            gemeenteCode,
+            gemeenteNaam,
+            gemeenteMail,
+            bpmnSupport,
+            bronOrganisatie,
+            verantwoordelijkeOrganisatie,
+            catalogusDomein
+        )
+
+        When("a list of additional allowed file types are requested") {
+            val fileTypes = configurationService.readAdditionalAllowedFileTypes()
+
+            Then("an empty list is returned") {
+                fileTypes.size shouldBe 0
             }
         }
     }
