a7a8b2818157716d5fd7412ada71603a00927bf4
Sander <mail@sanderboer.nl>
2025-03-24 00:10:55 +0100

    input can have a different display value than actual value

--- src/main/app/src/app/shared/form/input/input.ts
+++ src/main/app/src/app/shared/form/input/input.ts
@@ -16,14 +16,21 @@ import { FormHelper } from "../helpers";
 export class ZacInput<
   Form extends Record<string, AbstractControl>,
   Key extends keyof Form,
+  Option extends Form[Key]["value"],
+  DisplayValue extends keyof Option | ((option: Option) => string),
 > implements OnInit
 {
   @Input({ required: true }) key!: Key;
   @Input({ required: true }) form!: FormGroup<Form>;
   @Input({ transform: booleanAttribute }) readonly = false;
   @Input() type: "text" | "number" = "text";
+  /**
+   * When a `displayValue` is declared the `input` will be hidden, and it will use an overlay for the value.
+   * The input will also be put in an `readonly` mode.
+   */
+  @Input() displayValue?: DisplayValue;
 
-  protected control?: AbstractControl<string>;
+  protected control?: AbstractControl<Option>;
   protected maxlength?: number | null;
 
   constructor(private readonly translateService: TranslateService) {}
@@ -32,6 +39,19 @@ export class ZacInput<
     this.control = this.form.get(String(this.key))!;
     this.maxlength = FormHelper.getValidatorValue("maxLength", this.control);
   }
+
+  protected getDisplayValue = (option?: Option) => {
+    if (!option) return null;
+
+    switch (typeof this.displayValue) {
+      case "undefined":
+        return String(option);
+      case "function":
+        return this.displayValue(option);
+      default:
+        return String(option[this.displayValue as keyof Option]);
+    }
+  };
 
   protected get required() {
     return this.control?.hasValidator(Validators.required) ?? false;
