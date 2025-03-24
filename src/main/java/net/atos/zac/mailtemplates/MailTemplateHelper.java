a3c3d4a466dcaceb5abbd59927dc8e0ec0be446f
Edgar Vonk <edgar@info.nl>
2025-03-24 16:54:23 +0100

    chore: converted old javax.annotation usages to jakarta.annotation

--- src/main/java/net/atos/zac/mailtemplates/MailTemplateHelper.java
+++ src/main/java/net/atos/zac/mailtemplates/MailTemplateHelper.java
@@ -40,8 +40,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 import java.util.regex.Pattern;
 
-import javax.annotation.Nullable;
-
+import jakarta.annotation.Nullable;
 import jakarta.inject.Inject;
 
 import org.apache.commons.lang3.StringUtils;
