72230972de5fa44ebceafe98c1cb934b722e33b0
Edgar Vonk <edgar@info.nl>
2025-04-02 11:19:29 +0200

    fix: generating ZAC API HTML docs no longer works (#2996)
    Removed obsolete and no longer working Swagger Gradle plugin and created
    a simple Node based Gradle Npm Redoc task to generate the API docs. PS:
    our ZAC OpenAPI spec itself is invalid because of duplicate
    operationIds. For another PR..
    
    Solves PZ-6138

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,8 @@ import com.github.gradle.node.npm.task.NpmTask
 import io.gitlab.arturbosch.detekt.Detekt
 import io.smallrye.openapi.api.OpenApiConfig.DuplicateOperationIdBehavior
 import io.smallrye.openapi.api.OpenApiConfig.OperationIdStrategy
+import org.gradle.api.plugins.JavaBasePlugin.BUILD_TASK_NAME
+import org.gradle.api.plugins.JavaBasePlugin.DOCUMENTATION_GROUP
 import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
 import java.util.Locale
 
@@ -22,7 +24,6 @@ plugins {
     alias(libs.plugins.gradle.node)
     alias(libs.plugins.taskinfo)
     alias(libs.plugins.openapi)
-    alias(libs.plugins.swagger.generator)
     alias(libs.plugins.detekt)
     alias(libs.plugins.spotless)
     alias(libs.plugins.allopen)
@@ -145,8 +146,6 @@ dependencies {
         // and these transitive dependencies may cause conflicts with the RESTEasy version provided by WildFly
         exclude(group = "org.jboss.resteasy")
     }
-
-    swaggerUI(libs.swagger.ui)
 
     // enable detekt formatting rules. see: https://detekt.dev/docs/rules/formatting/
     detektPlugins(libs.detekt.formatting)
@@ -283,12 +282,6 @@ smallryeOpenApi {
     // https://github.com/smallrye/smallrye-open-api/issues/2230
     duplicateOperationIdBehavior.set(DuplicateOperationIdBehavior.FAIL)
     outputFileTypeFilter.set("YAML")
-}
-
-swaggerSources {
-    register("zaakafhandelcomponent") {
-        setInputFile(file("$rootDir/build/generated/openapi/META-INF/openapi/openapi.yaml"))
-    }
 }
 
 configure<SpotlessExtension> {
@@ -469,8 +462,6 @@ tasks {
     getByName("spotlessJson").mustRunAfter("npmRunLint")
     getByName("spotlessLess").mustRunAfter("npmRunLint")
 
-    getByName("generateSwaggerUIZaakafhandelcomponent").setDependsOn(listOf("generateOpenApiSpec"))
-
     getByName("compileItestKotlin") {
         dependsOn("copyJacocoAgentForItest")
         mustRunAfter("buildDockerImage")
@@ -516,7 +507,7 @@ tasks {
     // specify a specific non-overlapping output directory per task so that
     // Gradle can cache the outputs for these tasks.
     withType<GenerateTask> {
-        group = "build"
+        group = BUILD_TASK_NAME
         generatorName.set("java")
         generateApiTests.set(false)
         generateApiDocumentation.set(false)
@@ -620,11 +611,9 @@ tasks {
         description = "Generates Java client code for the DRC API"
         inputSpec.set("$rootDir/src/main/resources/api-specs/zgw/drc-openapi.yaml")
         outputDir.set("$rootDir/src/generated/zgw/drc/java")
-
         // this OpenAPI spec contains a schema validation error: `schema: null`
         // so we disable the schema validation for this spec until this is fixed in a future version of this spec
         validateSpec.set(false)
-
         modelPackage.set("nl.info.client.zgw.drc.model.generated")
     }
 
@@ -800,6 +789,20 @@ tasks {
         inputs.file(wildflyResources.file("configure-wildfly.cli"))
         inputs.file(wildflyResources.file("deploy-zaakafhandelcomponent.cli"))
         outputs.dir(layout.projectDirectory.dir("target"))
+    }
+
+    register<Exec>("generateZacApiDocs") {
+        description = "Generate ZAC HTML API documentation from OpenAPI spec."
+        dependsOn("generateOpenApiSpec")
+        group = DOCUMENTATION_GROUP
+        commandLine(
+            "npx",
+            "@redocly/cli",
+            "build-docs",
+            file("$rootDir/build/generated/openapi/META-INF/openapi/openapi.yaml"),
+            "-o",
+            file("build/generated/zac-api-docs/index.html")
+        )
     }
 
     register<Maven>("cleanMaven") {
