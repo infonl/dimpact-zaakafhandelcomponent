91087c650d26d554d70cef6622530cbacf2a0028
Hristo Iliev <hristo.iliev@info.nl>
2025-04-08 15:17:49 +0300

    fix: spotless formatting

--- src/main/app/src/app/formulieren/formio-wrapper/formio-wrapper.component.spec.ts
+++ src/main/app/src/app/formulieren/formio-wrapper/formio-wrapper.component.spec.ts
@@ -2,40 +2,38 @@
  * SPDX-FileCopyrightText: 2025 Lifely
  * SPDX-License-Identifier: EUPL-1.2+
  */
-import {FormioWrapperComponent} from "./formio-wrapper.component";
-import {TestBed} from "@angular/core/testing";
+import { TestBed } from "@angular/core/testing";
+import { FormioWrapperComponent } from "./formio-wrapper.component";
 
 describe(FormioWrapperComponent.name, () => {
   let component: FormioWrapperComponent;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [
-        FormioWrapperComponent,
-      ],
+      providers: [FormioWrapperComponent],
     }).compileComponents();
 
     component = TestBed.inject(FormioWrapperComponent);
-  })
+  });
 
   describe(FormioWrapperComponent.prototype.onChange.name, () => {
     it(`should emit events that has data`, () => {
-      const event = { data: 'value' };
-      const listener = jest.spyOn(component.formChange, "emit")
+      const event = { data: "value" };
+      const listener = jest.spyOn(component.formChange, "emit");
 
-      component.onChange(event)
+      component.onChange(event);
 
       expect(listener).toHaveBeenCalledTimes(1);
       expect(listener).toHaveBeenCalledWith(event);
-    })
+    });
 
     it(`should filter out events without data`, () => {
-      const event = { key: 'value' };
-      const listener = jest.spyOn(component.formChange, "emit")
+      const event = { key: "value" };
+      const listener = jest.spyOn(component.formChange, "emit");
 
-      component.onChange(event)
+      component.onChange(event);
 
       expect(listener).toHaveBeenCalledTimes(0);
-    })
-  })
-})
+    });
+  });
+});
