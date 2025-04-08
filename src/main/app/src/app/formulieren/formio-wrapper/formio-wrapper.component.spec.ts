185605c33465158e3b2623eb509ff4cc126a35e4
Hristo Iliev <hristo.iliev@info.nl>
2025-04-08 15:10:45 +0300

    feature: add tests for filtering out events without data

---
+++ src/main/app/src/app/formulieren/formio-wrapper/formio-wrapper.component.spec.ts
@@ -0,0 +1,41 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Lifely
+ * SPDX-License-Identifier: EUPL-1.2+
+ */
+import {FormioWrapperComponent} from "./formio-wrapper.component";
+import {TestBed} from "@angular/core/testing";
+
+describe(FormioWrapperComponent.name, () => {
+  let component: FormioWrapperComponent;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        FormioWrapperComponent,
+      ],
+    }).compileComponents();
+
+    component = TestBed.inject(FormioWrapperComponent);
+  })
+
+  describe(FormioWrapperComponent.prototype.onChange.name, () => {
+    it(`should emit events that has data`, () => {
+      const event = { data: 'value' };
+      const listener = jest.spyOn(component.formChange, "emit")
+
+      component.onChange(event)
+
+      expect(listener).toHaveBeenCalledTimes(1);
+      expect(listener).toHaveBeenCalledWith(event);
+    })
+
+    it(`should filter out events without data`, () => {
+      const event = { key: 'value' };
+      const listener = jest.spyOn(component.formChange, "emit")
+
+      component.onChange(event)
+
+      expect(listener).toHaveBeenCalledTimes(0);
+    })
+  })
+})
