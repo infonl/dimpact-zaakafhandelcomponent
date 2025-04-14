1e00be436b782e5e6372192e2dc6e8015cb0b444
Hristo Iliev <hristo.iliev@info.nl>
2025-04-14 12:28:20 +0300

    fix: wording

--- build.gradle.kts
+++ build.gradle.kts
@@ -589,7 +589,7 @@ tasks {
 
     register<GenerateTask>("generateKlantenClient") {
         description = "Generates Java client code for the Klanten API"
-        // disabled because the generated Java code that is not a working OpenKlanten client
+        // disabled because the generated Java code is not a working OpenKlanten client
         isEnabled = false
 
         // To generate a new version of the client:
