75b053d15e5021700c0cf910bbc674587ca84d11
DaxRiool <dax_riool@hotmail.com>
2025-04-30 17:02:26 +0200

    fix: changed the filename setter due to comments

--- src/main/app/src/app/informatie-objecten/informatie-object-add/informatie-object-add.component.ts
+++ src/main/app/src/app/informatie-objecten/informatie-object-add/informatie-object-add.component.ts
@@ -292,9 +292,8 @@ export class InformatieObjectAddComponent implements AfterViewInit, OnDestroy {
 
     this.subscriptions.push(
       inhoudField.formControl.valueChanges.subscribe((file: File) => {
-        let fileName = file?.name?.replace(/\.[^/.]+$/, "") || "";
-        fileName = fileName.substring(0, 100);
-        titel.formControl.setValue(fileName);
+        const fileName = file?.name?.replace(/\.[^/.]+$/, "").substring(0, 100) ?? "";
+        titel.formControl.setValue(fileName)
       }),
     );
   }
