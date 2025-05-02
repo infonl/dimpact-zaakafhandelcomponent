ac42179f4a5d28852376e46ad2fac96224e45f4b
Sander <mail@sanderboer.nl>
2025-05-02 11:19:59 +0200

    added sql migration

--- src/main/kotlin/nl/info/zac/app/admin/model/RestBetrokkeneKoppelingen.kt
+++ src/main/kotlin/nl/info/zac/app/admin/model/RestBetrokkeneKoppelingen.kt
@@ -8,6 +8,6 @@ package nl.info.zac.app.admin.model
 class RestBetrokkeneKoppelingen {
     var id: Long? = null
     var zaakafhandelParameters: RestZaakafhandelParameters? = null
-    var brpKoppelen = true
-    var kvkKoppelen = true
+    var brpKoppelen = false
+    var kvkKoppelen = false
 }
