d7202766b4dd3d1a187511915af0254428e95a50
Sander <mail@sanderboer.nl>
2025-05-02 11:19:59 +0200

    added sql migration

--- src/main/java/net/atos/zac/admin/model/ZaakafhandelParameters.java
+++ src/main/java/net/atos/zac/admin/model/ZaakafhandelParameters.java
@@ -15,7 +15,17 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
