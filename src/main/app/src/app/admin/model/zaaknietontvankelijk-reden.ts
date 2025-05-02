6f1a0422bcc0d5e561f094e068f32a958feb7e28
Sander <mail@sanderboer.nl>
2025-05-02 15:43:30 +0200

    refactor(parameter-edit): fully type-safe

--- src/main/app/src/app/admin/model/zaaknietontvankelijk-reden.ts
+++ src/main/app/src/app/admin/model/zaaknietontvankelijk-reden.ts
@@ -1,8 +1,9 @@
 /*
- * SPDX-FileCopyrightText: 2022 Atos
+ * SPDX-FileCopyrightText: 2022 Atos, 2025 Lifely
  * SPDX-License-Identifier: EUPL-1.2+
  */
 
+import { GeneratedType } from "../../shared/utils/generated-types";
 import { ZaakbeeindigReden } from "./zaakbeeindig-reden";
 
 export class ZaaknietontvankelijkReden extends ZaakbeeindigReden {
@@ -14,11 +15,7 @@ export class ZaaknietontvankelijkReden extends ZaakbeeindigReden {
     super();
   }
 
-  static getInstance() {
-    return this.ZNOR;
-  }
-
-  static is(reden: ZaakbeeindigReden): boolean {
-    return reden.id === this.ZNOR.id;
+  static is(reden?: GeneratedType<"RESTZaakbeeindigReden">): boolean {
+    return reden?.id === this.ZNOR.id;
   }
 }
