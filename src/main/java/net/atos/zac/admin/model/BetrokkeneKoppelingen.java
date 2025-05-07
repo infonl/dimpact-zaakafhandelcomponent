59df87bd0863a8c1e45299e4895e0e4b5c5fdfb8
Sander <mail@sanderboer.nl>
2025-05-07 09:00:03 +0200

    chore(BetrokkeneKoppelingen): remove `.*` import

--- src/main/java/net/atos/zac/admin/model/BetrokkeneKoppelingen.java
+++ src/main/java/net/atos/zac/admin/model/BetrokkeneKoppelingen.java
@@ -5,9 +5,16 @@
 
 package net.atos.zac.admin.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
-
 import net.atos.zac.util.FlywayIntegrator;
 
 @Entity
