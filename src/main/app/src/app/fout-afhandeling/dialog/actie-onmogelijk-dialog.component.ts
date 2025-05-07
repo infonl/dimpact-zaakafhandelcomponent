e17acba88205803904c0c2f912899895945b56d8
infonl-marcel <marcele@info.nl>
2025-05-07 10:23:21 +0200

    pr feedback

--- src/main/app/src/app/fout-afhandeling/dialog/actie-onmogelijk-dialog.component.ts
+++ src/main/app/src/app/fout-afhandeling/dialog/actie-onmogelijk-dialog.component.ts
@@ -3,18 +3,15 @@
  * SPDX-License-Identifier: EUPL-1.2+
  */
 
-import { Component, Inject } from "@angular/core";
-import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import { Component } from "@angular/core";
+import { MatDialogRef } from "@angular/material/dialog";
 
 @Component({
   templateUrl: "actie-onmogelijk-dialog.component.html",
   styleUrls: ["./dialog.component.less"],
 })
 export class ActieOnmogelijkDialogComponent {
-  constructor(
-    public dialogRef: MatDialogRef<ActieOnmogelijkDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: string,
-  ) {}
+  constructor(public dialogRef: MatDialogRef<ActieOnmogelijkDialogComponent>) {}
 
   close(): void {
     this.dialogRef.close();
