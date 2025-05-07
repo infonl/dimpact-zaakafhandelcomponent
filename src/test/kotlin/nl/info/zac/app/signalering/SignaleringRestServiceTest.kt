34e05b6ed4b4d4a7e23c1b91eb36c2f126c8fcbf
Edgar Vonk <edgar@info.nl>
2025-05-07 14:58:05 +0200

    feat: add authorisation for group signalering instellingen (#3282)
    Added authorisation checks on 'beheren' permission for listing and
    updating group signalering instellingen.
    
    Solves PZ-6652

--- src/test/kotlin/nl/info/zac/app/signalering/SignaleringRestServiceTest.kt
+++ src/test/kotlin/nl/info/zac/app/signalering/SignaleringRestServiceTest.kt
@@ -8,9 +8,13 @@ package nl.info.zac.app.signalering
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import io.mockk.checkUnnecessaryStub
 import io.mockk.every
 import io.mockk.mockk
 import jakarta.enterprise.inject.Instance
+import net.atos.zac.policy.PolicyService
+import net.atos.zac.policy.exception.PolicyException
 import net.atos.zac.signalering.model.SignaleringType
 import nl.info.zac.app.shared.RestPageParameters
 import nl.info.zac.app.signalering.converter.RestSignaleringInstellingenConverter
@@ -18,20 +22,28 @@ import nl.info.zac.app.signalering.exception.SignaleringException
 import nl.info.zac.app.zaak.model.createRESTZaakOverzicht
 import nl.info.zac.authentication.LoggedInUser
 import nl.info.zac.identity.IdentityService
+import nl.info.zac.identity.model.createGroup
 import nl.info.zac.signalering.SignaleringService
+import nl.info.zac.signalering.model.createRestSignaleringInstellingen
+import nl.info.zac.signalering.model.createSignaleringInstellingen
 
 class SignaleringRestServiceTest : BehaviorSpec({
     val signaleringService = mockk<SignaleringService>()
     val identityService = mockk<IdentityService>()
+    val policyService = mockk<PolicyService>()
     val restSignaleringInstellingenConverter = mockk<RestSignaleringInstellingenConverter>()
     val loggedInUserInstance = mockk<Instance<LoggedInUser>>()
-
     val signaleringRestService = SignaleringRestService(
-        signaleringService,
-        identityService,
-        restSignaleringInstellingenConverter,
-        loggedInUserInstance
+        signaleringService = signaleringService,
+        identityService = identityService,
+        policyService = policyService,
+        restSignaleringInstellingenConverter = restSignaleringInstellingenConverter,
+        loggedInUserInstance = loggedInUserInstance
     )
+
+    beforeEach {
+        checkUnnecessaryStub()
+    }
 
     Given("zaken signaleringen for ZAAK_OP_NAAM") {
         val signaleringType = SignaleringType.Type.ZAAK_OP_NAAM
@@ -62,6 +74,100 @@ class SignaleringRestServiceTest : BehaviorSpec({
 
             Then("exception is thrown") {
                 exception.message shouldBe "Requested page 123 must be <= 1"
+            }
+        }
+    }
+
+    Given("An existing group and existing group signalering instellingen and a user with 'beheren' permissions") {
+        val groupId = "fakeGroupId"
+        val group = createGroup()
+        val signaleringInstellingen = listOf(createSignaleringInstellingen())
+        val restSignaleringInstellingen = listOf(createRestSignaleringInstellingen())
+
+        every { policyService.readOverigeRechten().beheren } returns true
+        every { identityService.readGroup(groupId) } returns group
+        every { signaleringService.listInstellingenInclusiefMogelijke(any()) } returns signaleringInstellingen
+        every { restSignaleringInstellingenConverter.convert(signaleringInstellingen) } returns restSignaleringInstellingen
+
+        When("listGroupSignaleringInstellingen is called") {
+            val result = signaleringRestService.listGroupSignaleringInstellingen(groupId)
+
+            Then("it should return the list of signalering instellingen for the group") {
+                result shouldBe restSignaleringInstellingen
+            }
+        }
+    }
+
+    Given("An existing group and a user with 'beheren' permissions") {
+        val groupId = "valid-group-id"
+        val group = createGroup()
+        val signaleringInstellingen = createSignaleringInstellingen()
+        val restSignaleringInstellingen = createRestSignaleringInstellingen()
+
+        every { policyService.readOverigeRechten().beheren } returns true
+        every { identityService.readGroup(groupId) } returns group
+        every {
+            restSignaleringInstellingenConverter.convert(restSignaleringInstellingen, group)
+        } returns signaleringInstellingen
+        every { signaleringService.createUpdateOrDeleteInstellingen(signaleringInstellingen) } returns signaleringInstellingen
+
+        When("updateGroupSignaleringInstellingen is called") {
+            val result = signaleringRestService.updateGroupSignaleringInstellingen(groupId, restSignaleringInstellingen)
+
+            Then("it should update the signalering instellingen for the group and return the updated instellingen") {
+                result shouldBe signaleringInstellingen
+            }
+        }
+    }
+
+    Given("A non-existing group") {
+        val groupId = "invalid-group-id"
+        val restSignaleringInstellingen = createRestSignaleringInstellingen()
+        every { policyService.readOverigeRechten().beheren } returns true
+        every { identityService.readGroup(groupId) } throws IllegalArgumentException("Group not found")
+
+        When("listGroupSignaleringInstellingen is called") {
+            val exception = shouldThrow<IllegalArgumentException> {
+                signaleringRestService.listGroupSignaleringInstellingen(groupId)
+            }
+
+            Then("it should throw an IllegalArgumentException") {
+                exception.message shouldBe "Group not found"
+            }
+        }
+
+        When("updateGroupSignaleringInstellingen is called") {
+            val exception = shouldThrow<IllegalArgumentException> {
+                signaleringRestService.updateGroupSignaleringInstellingen(groupId, restSignaleringInstellingen)
+            }
+
+            Then("it should throw an IllegalArgumentException") {
+                exception.message shouldBe "Group not found"
+            }
+        }
+    }
+
+    Given("The user does not have the required 'beheren' permissions") {
+        val groupId = "fakeGroupId"
+        val restSignaleringInstellingen = createRestSignaleringInstellingen()
+        every { policyService.readOverigeRechten().beheren } returns false
+
+        When("listGroupSignaleringInstellingen is called") {
+            val exception = shouldThrow<PolicyException> {
+                signaleringRestService.listGroupSignaleringInstellingen(groupId)
+            }
+
+            Then("it should throw a PolicyException") {
+                exception shouldNotBe null
+            }
+        }
+
+        When("updateGroupSignaleringInstellingen is called") {
+            val exception = shouldThrow<PolicyException> {
+                signaleringRestService.updateGroupSignaleringInstellingen(groupId, restSignaleringInstellingen)
+            }
+            Then("it should throw a PolicyException") {
+                exception shouldNotBe null
             }
         }
     }
