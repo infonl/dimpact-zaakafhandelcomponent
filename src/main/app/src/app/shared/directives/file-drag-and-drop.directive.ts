47ed642dc5087d056899837e9a3771548b63b1b3
Sander <mail@sanderboer.nl>
2025-05-08 16:58:22 +0200

    chore(file-drag-and-drop.directive): make type safe

--- src/main/app/src/app/shared/directives/file-drag-and-drop.directive.ts
+++ src/main/app/src/app/shared/directives/file-drag-and-drop.directive.ts
@@ -19,25 +19,25 @@ export class FileDragAndDropDirective {
   @Output() fileDropped = new EventEmitter<FileList>();
   @HostBinding("style.border") private border = "solid transparent";
 
-  @HostListener("dragover", ["$event"]) public onDragOver(evt): any {
+  @HostListener("dragover", ["$event"]) public onDragOver(evt: DragEvent) {
     evt.preventDefault();
     evt.stopPropagation();
     this.border = "dotted #FF4D2A";
   }
 
-  @HostListener("dragleave", ["$event"]) public onDragLeave(evt): any {
+  @HostListener("dragleave", ["$event"]) public onDragLeave(evt: DragEvent) {
     evt.preventDefault();
     evt.stopPropagation();
     this.border = "solid transparent";
   }
 
-  @HostListener("drop", ["$event"]) public ondrop(evt): any {
+  @HostListener("drop", ["$event"]) public ondrop(evt: DragEvent) {
     evt.preventDefault();
     evt.stopPropagation();
     this.border = "solid transparent";
-    const files = evt.dataTransfer.files;
-    if (files.length > 0) {
-      this.fileDropped.emit(files);
-    }
+    const files = evt.dataTransfer?.files;
+    if (!files?.length) return
+    
+    this.fileDropped.emit(files);
   }
 }
