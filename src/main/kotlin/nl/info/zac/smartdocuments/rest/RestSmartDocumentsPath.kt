c7edd44242f8e097fe4879c8fdcade3a5c8d42e2
Hristo Iliev <hristo.iliev@info.nl>
2025-05-19 21:15:26 +0300

    fix: integration tests

--- src/main/kotlin/nl/info/zac/smartdocuments/rest/RestSmartDocumentsPath.kt
+++ src/main/kotlin/nl/info/zac/smartdocuments/rest/RestSmartDocumentsPath.kt
@@ -4,11 +4,13 @@
  */
 package nl.info.zac.smartdocuments.rest
 
+import jakarta.validation.constraints.NotEmpty
 import nl.info.zac.util.AllOpen
 import nl.info.zac.util.NoArgConstructor
 
 @NoArgConstructor
 @AllOpen
 data class RestSmartDocumentsPath(
+    @field:NotEmpty
     var path: List<String>
 )
