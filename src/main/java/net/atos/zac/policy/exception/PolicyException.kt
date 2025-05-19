29f1cd2fb8ddceb1aa51233ca79e73aaa45160d3
Edgar Vonk <edgar@info.nl>
2025-05-19 15:18:24 +0200

    chore: convert Policy service to Kotlin

--- src/main/java/net/atos/zac/policy/exception/PolicyException.kt
+++ src/main/java/net/atos/zac/policy/exception/PolicyException.kt
@@ -1,9 +1,7 @@
 /*
- * SPDX-FileCopyrightText: 2022 Atos
+ * SPDX-FileCopyrightText: 2022 Atos, 2025 INFO.nl
  * SPDX-License-Identifier: EUPL-1.2+
  */
+package net.atos.zac.policy.exception
 
-package net.atos.zac.policy.exception;
-
-public class PolicyException extends RuntimeException {
-}
+class PolicyException : RuntimeException()
