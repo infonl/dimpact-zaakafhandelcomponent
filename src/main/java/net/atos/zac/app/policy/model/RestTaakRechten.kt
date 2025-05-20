94506912ce53ca23e53999a5ef15b6d2545a4518
Edgar Vonk <edgar@info.nl>
2025-05-20 10:33:56 +0200

    chore: migrate REST policy classes to Kotlin

--- src/main/java/net/atos/zac/app/policy/model/RestTaakRechten.kt
+++ src/main/java/net/atos/zac/app/policy/model/RestTaakRechten.kt
@@ -4,6 +4,12 @@
  */
 package net.atos.zac.app.policy.model
 
+import nl.info.zac.util.NoArgConstructor
+
+// No-arg constructor is needed because somewhere we
+// create an empty instance of this class.
+// We should probably fix this at some point.
+@NoArgConstructor
 data class RestTaakRechten(
     val lezen: Boolean,
     val wijzigen: Boolean,
