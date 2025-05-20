c81cd23d42dedb5759d331d39a6a50157a828f4a
Edgar Vonk <edgar@info.nl>
2025-05-20 13:50:25 +0200

    chore: updated docs

--- src/main/kotlin/nl/info/zac/app/policy/model/RestTaakRechten.kt
+++ src/main/kotlin/nl/info/zac/app/policy/model/RestTaakRechten.kt
@@ -7,9 +7,10 @@ package nl.info.zac.app.policy.model
 import nl.info.zac.policy.output.TaakRechten
 import nl.info.zac.util.NoArgConstructor
 
-// No-arg constructor is needed because somewhere we
-// create an empty instance of this class.
-// We should probably fix this at some point.
+// No-arg constructor is needed because instances of this class are currently
+// deserialized because they are part of incoming ZAC API requests in [TaskRestService.kt].
+// This is because currently the same classes are used for all CRUD operations in the ZAC API.
+// We should refactor this at some point.
 @NoArgConstructor
 data class RestTaakRechten(
     val lezen: Boolean,
