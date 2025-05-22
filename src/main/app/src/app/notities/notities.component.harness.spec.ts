fdadd59178da2433fd5b20f3c82bf9e8da7b1934
infonl-marcel <marcele@info.nl>
2025-05-22 17:18:10 +0200

    harnessessessed

--- src/main/app/src/app/notities/notities.component.harness.spec.ts
+++ src/main/app/src/app/notities/notities.component.harness.spec.ts
@@ -1,22 +1,18 @@
 /*
- * SPDX-FileCopyrightText: 2024 INFO.nl
+ * SPDX-FileCopyrightText: 2025 INFO.nl
  * SPDX-License-Identifier: EUPL-1.2+
  */
 
-import {
-  provideHttpClient,
-  withInterceptorsFromDi,
-} from "@angular/common/http";
+import { ComponentHarness, HarnessLoader } from "@angular/cdk/testing";
+import { TestbedHarnessEnvironment } from "@angular/cdk/testing/testbed";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 import { TranslateModule, TranslateService } from "@ngx-translate/core";
-import { Observable, of } from "rxjs";
+import { of } from "rxjs";
 import { IdentityService } from "../identity/identity.service";
 import { MaterialModule } from "../shared/material/material.module";
 import { PipesModule } from "../shared/pipes/pipes.module";
 import { GeneratedType } from "../shared/utils/generated-types";
-import { Notitie } from "./model/notitie";
 import { NotitiesComponent } from "./notities.component";
 import { NotitieService } from "./notities.service";
 
@@ -31,10 +27,10 @@ const mockIdentityService = {
   },
 };
 const mockNotitieService = {
-  listNotities(): Observable<Notitie[]> {
+  listNotities() {
     return of([]);
   },
-  updateNotitie(notitie: Notitie) {
+  updateNotitie(notitie: any) {
     return of(notitie);
   },
 };
@@ -48,9 +44,19 @@ const mockTranslateService = {
   onDefaultLangChange: of({}),
 };
 
-describe("NotitiesComponent", () => {
-  let component: NotitiesComponent;
+class NotitiesHarness extends ComponentHarness {
+  static hostSelector = ".notitie-container";
+
+  async isTextareaVisible(): Promise<boolean> {
+    const textareas = await this.locatorForAll("textarea")();
+    return textareas.length > 0;
+  }
+}
+
+describe("NotitiesComponent Harness", () => {
   let fixture: ComponentFixture<NotitiesComponent>;
+  let component: NotitiesComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [NotitiesComponent],
@@ -64,28 +70,30 @@ describe("NotitiesComponent", () => {
         { provide: IdentityService, useValue: mockIdentityService },
         { provide: NotitieService, useValue: mockNotitieService },
         { provide: TranslateService, useValue: mockTranslateService },
-        provideHttpClient(withInterceptorsFromDi()),
       ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NotitiesComponent);
     component = fixture.componentInstance;
-    component.notitieRechten = { lezen: true, wijzigen: true };
+  });
+
+  it("should show textarea when wijzigen is true", async () => {
+    component.notitieRechten = { lezen: false, wijzigen: true };
     fixture.detectChanges();
+
+    const loader: HarnessLoader = TestbedHarnessEnvironment.loader(fixture);
+    const harness = await loader.getHarness(NotitiesHarness);
+
+    expect(await harness.isTextareaVisible()).toBe(true);
   });
 
-  it("should create", () => {
-    expect(component).toBeTruthy();
-  });
+  it("should not show textarea when wijzigen is false", async () => {
+    component.notitieRechten = { lezen: false, wijzigen: false };
+    fixture.detectChanges();
 
-  it("should set new text and current username on notitie edit", () => {
-    const notitie = new Notitie();
-    notitie.tekst = "some text";
-    notitie.gebruikersnaamMedewerker = "some other user";
-    notitie.id = 1;
-    const someOtherText = "some other text";
-    component.updateNotitie(notitie, someOtherText);
-    expect(notitie.gebruikersnaamMedewerker).toEqual(currentUser.id);
-    expect(notitie.tekst).toEqual(someOtherText);
+    const loader: HarnessLoader = TestbedHarnessEnvironment.loader(fixture);
+    const harness = await loader.getHarness(NotitiesHarness);
+
+    expect(await harness.isTextareaVisible()).toBe(false);
   });
 });
