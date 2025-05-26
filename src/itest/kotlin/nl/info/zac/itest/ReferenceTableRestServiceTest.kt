c359389d6fe4c465e1c5467c2980f1d590e93c39
Sander <mail@sanderboer.nl>
2025-05-27 01:05:42 +0200

    fix reference table test

--- src/itest/kotlin/nl/info/zac/itest/ReferenceTableRestServiceTest.kt
+++ src/itest/kotlin/nl/info/zac/itest/ReferenceTableRestServiceTest.kt
@@ -16,6 +16,10 @@ import nl.info.zac.itest.config.ItestConfiguration.REFERENCE_TABLE_ADVIES_CODE
 import nl.info.zac.itest.config.ItestConfiguration.REFERENCE_TABLE_ADVIES_NAME
 import nl.info.zac.itest.config.ItestConfiguration.REFERENCE_TABLE_AFZENDER_CODE
 import nl.info.zac.itest.config.ItestConfiguration.REFERENCE_TABLE_AFZENDER_NAME
+import nl.info.zac.itest.config.ItestConfiguration.REFERENCE_TABLE_BRP_DOELBINDING_RAADPLEEG_WAARDE_CODE
+import nl.info.zac.itest.config.ItestConfiguration.REFERENCE_TABLE_BRP_DOELBINDING_RAADPLEEG_WAARDE_NAAM
+import nl.info.zac.itest.config.ItestConfiguration.REFERENCE_TABLE_BRP_DOELBINDING_ZOEK_WAARDE_CODE
+import nl.info.zac.itest.config.ItestConfiguration.REFERENCE_TABLE_BRP_DOELBINDING_ZOEK_WAARDE_NAAM
 import nl.info.zac.itest.config.ItestConfiguration.REFERENCE_TABLE_COMMUNICATIEKANAAL_CODE
 import nl.info.zac.itest.config.ItestConfiguration.REFERENCE_TABLE_COMMUNICATIEKANAAL_NAME
 import nl.info.zac.itest.config.ItestConfiguration.REFERENCE_TABLE_DOMEIN_CODE
@@ -81,15 +85,31 @@ class ReferenceTableRestServiceTest : BehaviorSpec({
                                 "naam": "$REFERENCE_TABLE_SERVER_ERROR_ERROR_PAGINA_TEKST_NAME", 
                                 "systeem": true, 
                                 "aantalWaarden": 0
-                            }
+                            },
+                            {
+                                "aantalWaarden": 15,
+                                "code": "$REFERENCE_TABLE_BRP_DOELBINDING_RAADPLEEG_WAARDE_CODE",
+                                "id": 7,
+                                "naam": "$REFERENCE_TABLE_BRP_DOELBINDING_RAADPLEEG_WAARDE_NAAM",
+                                "systeem": true,
+                                "waarden": []
+                              },
+                              {
+                                "aantalWaarden": 4,
+                                "code": "$REFERENCE_TABLE_BRP_DOELBINDING_ZOEK_WAARDE_CODE",
+                                "id": 6,
+                                "naam": "$REFERENCE_TABLE_BRP_DOELBINDING_ZOEK_WAARDE_NAAM",
+                                "systeem": true,
+                                "waarden": []
+                              },
                         ]
                         """.trimIndent()
                     )
                 }
                 with(JSONArray(responseBody)) {
-                    communicationChannelReferenceTableId = getJSONObject(2).getInt("id")
-                    domeinReferenceTableId = getJSONObject(3).getInt("id")
-                    serverErrorTextErrorReferenceTableId = getJSONObject(4).getInt("id")
+                    communicationChannelReferenceTableId = getJSONObject(4).getInt("id")
+                    domeinReferenceTableId = getJSONObject(5).getInt("id")
+                    serverErrorTextErrorReferenceTableId = getJSONObject(6).getInt("id")
                 }
             }
         }
