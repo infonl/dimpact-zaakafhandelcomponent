93170aed9344fac954ef9b67c11e032c22e8fabf
Edgar Vonk <edgar@info.nl>
2025-06-02 18:28:34 +0200

    chore: convert some custom ZRC model classes to Kotlin (#3515)
    Convert some custom ZRC model classes to Kotlin.
    
    Solves PZ-7093

---
+++ src/main/kotlin/nl/info/client/zgw/zrc/model/RelevanteZaak.kt
@@ -0,0 +1,26 @@
+/*
+ * SPDX-FileCopyrightText: 2021 Atos, 2025 INFO.nl
+ * SPDX-License-Identifier: EUPL-1.2+
+ */
+package nl.info.client.zgw.zrc.model
+
+import jakarta.json.bind.annotation.JsonbCreator
+import jakarta.json.bind.annotation.JsonbProperty
+import net.atos.client.zgw.zrc.model.AardRelatie
+import nl.info.zac.util.AllOpen
+import nl.info.zac.util.NoArgConstructor
+import java.net.URI
+
+@NoArgConstructor
+@AllOpen
+data class RelevanteZaak @JsonbCreator constructor(
+    /**
+     * URL-referentie naar de ZAAK.
+     */
+    @param:JsonbProperty("url") val url: URI,
+
+    /**
+     * 'Benamingen van de aard van de relaties van andere zaken tot (onderhanden) zaken.'
+     */
+    @param:JsonbProperty("aardRelatie") val aardRelatie: AardRelatie
+)
