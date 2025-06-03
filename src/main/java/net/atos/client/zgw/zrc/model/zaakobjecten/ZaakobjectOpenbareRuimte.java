a228f8ac5d18847580ce23e218e1b76757c9e71b
Edgar Vonk <edgar@info.nl>
2025-06-03 13:09:31 +0200

    chore: use generated Objecttype ZRC client class (#3519)
    Use generated Objecttype ZRC client class.
    
    Solves PZ-7102
    
    Co-authored-by: sander boer <mail@sanderboer.nl>

--- src/main/java/net/atos/client/zgw/zrc/model/zaakobjecten/ZaakobjectOpenbareRuimte.java
+++ src/main/java/net/atos/client/zgw/zrc/model/zaakobjecten/ZaakobjectOpenbareRuimte.java
@@ -1,13 +1,13 @@
 /*
- * SPDX-FileCopyrightText: 2023 Atos
+ * SPDX-FileCopyrightText: 2023 Atos, 2025 INFO.nl
  * SPDX-License-Identifier: EUPL-1.2+
  */
-
 package net.atos.client.zgw.zrc.model.zaakobjecten;
 
 import java.net.URI;
 
-import net.atos.client.zgw.zrc.model.Objecttype;
+import nl.info.client.zgw.zrc.model.generated.ObjectTypeEnum;
+
 
 /**
  * ZaakobjectOpenbareRuimte
@@ -24,7 +24,7 @@ public class ZaakobjectOpenbareRuimte extends ZaakobjectMetObjectIdentificatie<O
      * Constructor with required attributes
      */
     public ZaakobjectOpenbareRuimte(final URI zaak, final URI bagobjectURI, final ObjectOpenbareRuimte objectOpenbareRuimte) {
-        super(zaak, bagobjectURI, Objecttype.OPENBARE_RUIMTE, objectOpenbareRuimte);
+        super(zaak, bagobjectURI, ObjectTypeEnum.OPENBARE_RUIMTE, objectOpenbareRuimte);
     }
 
     @Override
