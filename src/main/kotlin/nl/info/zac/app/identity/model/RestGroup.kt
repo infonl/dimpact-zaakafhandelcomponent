5125ef46505962e557a32d21c6c13c6cd9ef5aa5
Edgar Vonk <edgar@info.nl>
2025-06-10 11:18:51 +0200

    chore: generate constants for annotation validations in generated Java code using the OpenAPI Generator framework (#3572)
    Generate constants for annotation validations in generated Java code
    using the OpenAPI Generator framework and use those in our own code.
    
    Solves PZ-7198

--- src/main/kotlin/nl/info/zac/app/identity/model/RestGroup.kt
+++ src/main/kotlin/nl/info/zac/app/identity/model/RestGroup.kt
@@ -5,6 +5,8 @@
 package nl.info.zac.app.identity.model
 
 import jakarta.validation.constraints.Size
+import nl.info.client.zgw.zrc.model.generated.OrganisatorischeEenheidIdentificatie.IDENTIFICATIE_MAX_LENGTH
+import nl.info.client.zgw.zrc.model.generated.OrganisatorischeEenheidIdentificatie.NAAM_MAX_LENGTH
 import nl.info.zac.identity.model.Group
 import nl.info.zac.util.AllOpen
 import nl.info.zac.util.NoArgConstructor
@@ -14,18 +16,18 @@ import nl.info.zac.util.NoArgConstructor
 data class RestGroup(
     /**
      * Since this is used for the 'identificatie' field in
-     * [net.atos.client.zgw.zrc.model.OrganisatorischeEenheid]
+     * [nl.info.client.zgw.zrc.model.generated.OrganisatorischeEenheidIdentificatie],
      * we need to make sure it adheres to the same constraints.
      */
-    @field:Size(max = 24)
+    @field:Size(max = IDENTIFICATIE_MAX_LENGTH)
     var id: String,
 
     /**
      * Since this is used for the 'naam' field in
-     * [net.atos.client.zgw.zrc.model.OrganisatorischeEenheid]
+     * [nl.info.client.zgw.zrc.model.generated.OrganisatorischeEenheidIdentificatie],
      * we need to make sure it adheres to the same constraints.
      */
-    @field:Size(max = 50)
+    @field:Size(max = NAAM_MAX_LENGTH)
     var naam: String
 )
 
