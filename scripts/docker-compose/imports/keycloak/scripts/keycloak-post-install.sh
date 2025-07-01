51a66c0c66ffd27cabea4fd7fcb5ac1b0e6d9c64
Edgar Vonk <edgar@info.nl>
2025-07-01 15:18:05 +0200

    fix: set sslRequired to false for the Keycloak master realm in our Docker Compose environment to fix the intermittent issue that the master realm is unreachable locally with a "HTTPS required" error

--- scripts/docker-compose/imports/keycloak/scripts/keycloak-post-install.sh
+++ scripts/docker-compose/imports/keycloak/scripts/keycloak-post-install.sh
@@ -5,6 +5,8 @@
 # SPDX-License-Identifier: EUPL-1.2+
 #
 
+set -euo pipefail
+
 echo -e "Setting the 'sslRequired' parameter to 'NONE' for the Keycloak master realm so that SSL is not required under any circumstances"
 
 /opt/keycloak/bin/kcadm.sh config credentials --server http://keycloak:8080 --realm master --user admin --password admin
