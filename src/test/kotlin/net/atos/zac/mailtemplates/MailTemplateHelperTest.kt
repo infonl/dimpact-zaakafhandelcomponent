8e580bf1b0da8e8ffda9f510239bd5594cc0c415
Edgar Vonk <edgar@info.nl>
2025-07-10 11:35:15 +0200

    chore: added unit test

--- src/test/kotlin/net/atos/zac/mailtemplates/MailTemplateHelperTest.kt
+++ src/test/kotlin/net/atos/zac/mailtemplates/MailTemplateHelperTest.kt
@@ -11,6 +11,9 @@ import io.mockk.every
 import io.mockk.mockk
 import nl.info.client.brp.BrpClientService
 import nl.info.client.kvk.KvkClientService
+import nl.info.client.kvk.model.createResultaatItem
+import nl.info.client.zgw.model.createNietNatuurlijkPersoonIdentificatie
+import nl.info.client.zgw.model.createRolNietNatuurlijkPersoon
 import nl.info.client.zgw.model.createZaak
 import nl.info.client.zgw.model.createZaakStatus
 import nl.info.client.zgw.shared.ZGWApiService
@@ -22,6 +25,7 @@ import nl.info.zac.configuratie.ConfiguratieService
 import nl.info.zac.identity.IdentityService
 import java.net.URI
 import java.time.LocalDate
+import java.util.Optional
 
 class MailTemplateHelperTest : BehaviorSpec({
     val brpClientService = mockk<BrpClientService>()
@@ -56,15 +60,53 @@ class MailTemplateHelperTest : BehaviorSpec({
         every { zrcClientService.readStatus(zaak.status) } returns zaakStatus
         every { ztcClientService.readStatustype(zaakStatus.statustype) } returns statusType
 
-        When("I call the getMailTemplate method") {
+        When("the variables are resolved with a text containing placeholders") {
             val resolvedText = mailTemplateHelper.resolveVariabelen(
                 "fakeText, {ZAAK_NUMMER}, {ZAAK_URL}, {ZAAK_TYPE}, {ZAAK_STATUS}, {ZAAK_STARTDATUM}",
                 zaak
             )
 
-            Then("I should get the correct mail template") {
+            Then("the variables in the provided text should be replaced by the correct values from the zaak") {
                 resolvedText shouldBe "fakeText, ${zaak.identificatie}, $zaakTonenURL, ${zaakType.omschrijving}, " +
                     "${statusType.omschrijving}, 12-10-2021"
+            }
+        }
+    }
+
+    Given("A zaak with an initiator of role niet-natuurlijk persoon with a vestigingnummer") {
+        val zaakType = createZaakType()
+        val zaak = createZaak(
+            zaakTypeURI = zaakType.url,
+            status = URI("https://example.com/fakeStatus"),
+            startDate = LocalDate.of(2021, 10, 12)
+        )
+        val zaakStatus = createZaakStatus()
+        val statusType = createStatusType(omschrijving = "fakeStatusTypeDescription")
+        val zaakTonenURL = URI("https://example.com/fakeURL")
+        val vestigingsnummer = "123456789"
+        val rolNietNatuurlijkPersoon = createRolNietNatuurlijkPersoon(
+            nietNatuurlijkPersoonIdentificatie = createNietNatuurlijkPersoonIdentificatie(
+                vestigingsnummer = vestigingsnummer
+            )
+        )
+        val resultaatItem = createResultaatItem()
+        every { ztcClientService.readZaaktype(zaak.zaaktype) } returns createZaakType()
+        every { configuratieService.zaakTonenUrl(zaak.identificatie) } returns zaakTonenURL
+        every { zrcClientService.readStatus(zaak.status) } returns zaakStatus
+        every { ztcClientService.readStatustype(zaakStatus.statustype) } returns statusType
+        every { zgwApiService.findInitiatorRoleForZaak(zaak) } returns rolNietNatuurlijkPersoon
+        // note that this is not correct; instead of a rechtspersoon we should search for a vestiging
+        // this will be fixed in a follow-up pull request
+        every { kvkClientService.findRechtspersoon(vestigingsnummer) } returns Optional.of(resultaatItem)
+
+        When("the variables are resolved with a text containing a placeholder for the zaak initiator") {
+            val resolvedText = mailTemplateHelper.resolveVariabelen(
+                "fakeText, {ZAAK_INITIATOR}",
+                zaak
+            )
+
+            Then("the variables in the provided text should be replaced by the correct values from the zaak") {
+                resolvedText shouldBe "fakeText, ${resultaatItem.naam}"
             }
         }
     }
