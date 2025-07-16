ef7ba649f4170c98960dbdb8e40d9dbd79e04868
Edgar Vonk <edgar@info.nl>
2025-07-16 17:29:03 +0200

    feat: attempt to fix objects DB init scripts - work in progress

--- scripts/docker-compose/imports/objects-api/init/init.sh
+++ scripts/docker-compose/imports/objects-api/init/init.sh
@@ -13,4 +13,3 @@ sleep 10
 #
 
 python /app/src/manage.py loaddata demodata
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python /app/src/manage.py shell
