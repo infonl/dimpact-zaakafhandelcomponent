7df6d009ae266d26b69709bdf0a46a47c2d8f77c
Edgar Vonk <edgar@info.nl>
2025-07-23 11:35:48 +0200

    chore: use BedrijfType enum in KVK client

--- src/main/kotlin/nl/info/client/kvk/model/BedrijfType.kt
+++ src/main/kotlin/nl/info/client/kvk/model/BedrijfType.kt
@@ -4,25 +4,13 @@
  */
 package nl.info.client.kvk.model
 
-import jakarta.json.bind.annotation.JsonbTypeSerializer
-import jakarta.json.bind.serializer.JsonbSerializer
-import jakarta.json.bind.serializer.SerializationContext
-import jakarta.json.stream.JsonGenerator
-
 /**
  * The company type as specified by the KVK ZOEKEN API.
- * Used in requests to the KVK ZOEKEN API only.
  */
-@JsonbTypeSerializer(BedrijfTypeSerializer::class)
 enum class BedrijfType(val value: String) {
     HOOFDVESTIGING("hoofdvestiging"),
     NEVENVESTIGING("nevenvestiging"),
-    RECHTSPERSOON("rechtspersoon")
-}
+    RECHTSPERSOON("rechtspersoon");
 
-class BedrijfTypeSerializer : JsonbSerializer<BedrijfType> {
-    override fun serialize(bedrijfType: BedrijfType, generator: JsonGenerator, ctx: SerializationContext) {
-        generator.write(bedrijfType.value)
-    }
+    override fun toString(): String = value
 }
-
