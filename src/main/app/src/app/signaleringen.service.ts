6bca649ab9e7c04d9b4cff183bd11ddf0b9d54bb
Sander <mail@sanderboer.nl>
2025-07-28 16:21:22 +0200

    refactor(signaleringen-service): make use of the `GeneratedTypes`

--- src/main/app/src/app/signaleringen.service.ts
+++ src/main/app/src/app/signaleringen.service.ts
@@ -3,84 +3,56 @@
  * SPDX-License-Identifier: EUPL-1.2+
  */
 
-import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { BehaviorSubject, Observable } from "rxjs";
-import { catchError, switchMap } from "rxjs/operators";
-import { FoutAfhandelingService } from "./fout-afhandeling/fout-afhandeling.service";
-import { Resultaat } from "./shared/model/resultaat";
+import { BehaviorSubject } from "rxjs";
+import { switchMap } from "rxjs/operators";
+import { PutBody, ZacHttpClient } from "./shared/http/zac-http-client";
 import { GeneratedType } from "./shared/utils/generated-types";
-import { ZaakOverzichtDashboard } from "./zaken/model/zaak-overzicht-dashboard";
 
 @Injectable({
   providedIn: "root",
 })
 export class SignaleringenService {
-  private basepath = "/rest/signaleringen";
-
-  private latestSignaleringSubject: BehaviorSubject<void> =
-    new BehaviorSubject<void>(null);
+  private latestSignaleringSubject = new BehaviorSubject<null>(null);
   latestSignalering$ = this.latestSignaleringSubject.pipe(
-    switchMap(() => this.http.get<string>(`${this.basepath}/latest`)),
+    switchMap(() => this.zacHttpClient.GET("/rest/signaleringen/latest")),
   );
 
-  constructor(
-    private http: HttpClient,
-    private foutAfhandelingService: FoutAfhandelingService,
-  ) {}
+  constructor(private readonly zacHttpClient: ZacHttpClient) {}
 
-  updateSignaleringen(): void {
-    this.latestSignaleringSubject.next();
+  updateSignaleringen() {
+    this.latestSignaleringSubject.next(null);
   }
 
   listDashboardSignaleringTypen() {
-    return this.http
-      .get<GeneratedType<"Type">[]>(`${this.basepath}/typen/dashboard`)
-      .pipe(
-        catchError((err) => this.foutAfhandelingService.foutAfhandelen(err)),
-      );
+    return this.zacHttpClient.GET("/rest/signaleringen/typen/dashboard");
   }
 
-  listZakenSignalering(params: {
-    signaleringType: GeneratedType<"Type">;
-    page: number;
-    rows: number;
-  }): Observable<Resultaat<ZaakOverzichtDashboard>> {
-    const { signaleringType, page, rows } = params;
-    return this.http
-      .put<Resultaat<ZaakOverzichtDashboard>>(
-        `${this.basepath}/zaken/${signaleringType}`,
-        {
-          page,
-          rows,
-        },
-      )
-      .pipe(
-        catchError((err) => this.foutAfhandelingService.foutAfhandelen(err)),
-      );
+  listZakenSignalering(
+    type: GeneratedType<"Type">,
+    body: PutBody<"/rest/signaleringen/zaken/{type}">,
+  ) {
+    return this.zacHttpClient.PUT("/rest/signaleringen/zaken/{type}", body, {
+      path: { type },
+    });
   }
 
   listTakenSignalering(
     signaleringType: GeneratedType<"RestSignaleringInstellingen">["type"],
   ) {
-    return this.http
-      .get<
-        GeneratedType<"RestSignaleringTaskSummary">[]
-      >(`${this.basepath}/taken/${signaleringType}`)
-      .pipe(
-        catchError((err) => this.foutAfhandelingService.foutAfhandelen(err)),
-      );
+    return this.zacHttpClient.GET("/rest/signaleringen/taken/{type}", {
+      path: { type: signaleringType },
+    });
   }
 
   listInformatieobjectenSignalering(
     signaleringType: GeneratedType<"RestSignaleringInstellingen">["type"],
   ) {
-    return this.http
-      .get<
-        GeneratedType<"RestEnkelvoudigInformatieobject">[]
-      >(`${this.basepath}/informatieobjecten/${signaleringType}`)
-      .pipe(
-        catchError((err) => this.foutAfhandelingService.foutAfhandelen(err)),
-      );
+    return this.zacHttpClient.GET(
+      "/rest/signaleringen/informatieobjecten/{type}",
+      {
+        path: { type: signaleringType },
+      },
+    );
   }
 }
