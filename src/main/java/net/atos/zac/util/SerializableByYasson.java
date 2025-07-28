1f3882e4e4b97383c4ae61320f2db2201730e5af
Edgar Vonk <edgar@info.nl>
2025-07-28 13:35:07 +0200

    chore: convert OPA admin client to Kotlin

--- src/main/java/net/atos/zac/util/SerializableByYasson.java
+++ src/main/java/net/atos/zac/util/SerializableByYasson.java
@@ -7,7 +7,8 @@ package net.atos.zac.util;
 /**
  * A workaround for a bug in the Yasson 3 library which is included in WildFly related to using
  * generic types in JSON-B deserialisation. See:
- * <a href="https://github.com/eclipse-ee4j/yasson/issues/599">Issue with @JsonbCreator constructor with Generic Type in Yasson 3.0.0 and higher</a>.
+ * <a href="https://github.com/eclipse-ee4j/yasson/issues/599">Issue with @JsonbCreator constructor with Generic Type in Yasson 3.0.0 and
+ * higher</a>.
  * Details of the workaround: <a href="https://github.com/smallrye/smallrye-graphql/issues/1819#issuecomment-1549588537">issue comment</a>
  * The workaround is:
  * "T needs to implement an interface or extends an abstract class. A fake is fine, it is not added to the schema.
