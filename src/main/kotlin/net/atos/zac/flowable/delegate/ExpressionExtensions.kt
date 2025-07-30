9d1961d77de5f7e1f47f2845834a24969fa4d9a1
Hristo Iliev <hsiliev@gmail.com>
2025-07-30 14:54:27 +0300

    Update src/main/kotlin/net/atos/zac/flowable/delegate/ExpressionExtensions.kt
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/kotlin/net/atos/zac/flowable/delegate/ExpressionExtensions.kt
+++ src/main/kotlin/net/atos/zac/flowable/delegate/ExpressionExtensions.kt
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2025 INFO.nl
+ * SPDX-License-Identifier: EUPL-1.2+
+ */
+
 package net.atos.zac.flowable.delegate
 
 import org.flowable.common.engine.api.delegate.Expression
