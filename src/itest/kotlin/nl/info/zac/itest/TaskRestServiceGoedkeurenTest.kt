7b14660c8483b9ac4b87fd88e8ad72082e8e957d
edgarvonk <edgar@info.nl>
2025-08-12 20:12:36 +0200

    chore: added creating a document

--- src/itest/kotlin/nl/info/zac/itest/TaskRestServiceGoedkeurenTest.kt
+++ src/itest/kotlin/nl/info/zac/itest/TaskRestServiceGoedkeurenTest.kt
@@ -13,15 +13,40 @@ import nl.info.zac.itest.client.ItestHttpClient
 import nl.info.zac.itest.client.ZacClient
 import nl.info.zac.itest.config.ItestConfiguration.ACTIE_INTAKE_AFRONDEN
 import nl.info.zac.itest.config.ItestConfiguration.DATE_TIME_2000_01_01
+import nl.info.zac.itest.config.ItestConfiguration.DOCUMENT_3_IDENTIFICATION
+import nl.info.zac.itest.config.ItestConfiguration.DOCUMENT_FILE_TITLE
+import nl.info.zac.itest.config.ItestConfiguration.DOCUMENT_STATUS_IN_BEWERKING
+import nl.info.zac.itest.config.ItestConfiguration.DOCUMENT_VERTROUWELIJKHEIDS_AANDUIDING_OPENBAAR
+import nl.info.zac.itest.config.ItestConfiguration.DOCUMENT_VERTROUWELIJKHEIDS_AANDUIDING_VERTROUWELIJK
+import nl.info.zac.itest.config.ItestConfiguration.INFORMATIE_OBJECT_TYPE_BIJLAGE_OMSCHRIJVING
+import nl.info.zac.itest.config.ItestConfiguration.INFORMATIE_OBJECT_TYPE_BIJLAGE_UUID
+import nl.info.zac.itest.config.ItestConfiguration.PDF_MIME_TYPE
 import nl.info.zac.itest.config.ItestConfiguration.TEST_GROUP_A_DESCRIPTION
 import nl.info.zac.itest.config.ItestConfiguration.TEST_GROUP_A_ID
+import nl.info.zac.itest.config.ItestConfiguration.TEST_PDF_FILE_NAME
 import nl.info.zac.itest.config.ItestConfiguration.TEST_SPEC_ORDER_AFTER_ZAAK_UPDATED
+import nl.info.zac.itest.config.ItestConfiguration.TEST_TXT_FILE_NAME
+import nl.info.zac.itest.config.ItestConfiguration.TEST_USER_1_NAME
+import nl.info.zac.itest.config.ItestConfiguration.TEXT_MIME_TYPE
 import nl.info.zac.itest.config.ItestConfiguration.ZAAKTYPE_INDIENEN_AANSPRAKELIJKSTELLING_DOOR_DERDEN_BEHANDELEN_UUID
 import nl.info.zac.itest.config.ItestConfiguration.ZAC_API_URI
+import nl.info.zac.itest.config.ItestConfiguration.enkelvoudigInformatieObjectUUID
+import nl.info.zac.itest.config.ItestConfiguration.zaakProductaanvraag1Uuid
+import nl.info.zac.itest.util.shouldEqualJsonIgnoringExtraneousFields
 import nl.info.zac.itest.util.sleepForOpenZaakUniqueConstraint
+import okhttp3.Headers
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
 import org.json.JSONArray
 import org.json.JSONObject
+import java.io.File
 import java.net.HttpURLConnection.HTTP_NO_CONTENT
+import java.net.HttpURLConnection.HTTP_OK
+import java.net.URLDecoder
+import java.time.LocalDate
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 import java.util.UUID
 
 /**
@@ -37,6 +62,7 @@ class TaskRestServiceGoedkeurenTest : BehaviorSpec({
 
     Given("A zaak has been created that has finished the intake phase with the status 'admissible'") {
         lateinit var zaakUUID: UUID
+        lateinit var enkelvoudigInformatieObjectUUID: UUID
         val intakeId: Int
         zacClient.createZaak(
             zaakTypeUUID = ZAAKTYPE_INDIENEN_AANSPRAKELIJKSTELLING_DOOR_DERDEN_BEHANDELEN_UUID,
@@ -73,7 +99,61 @@ class TaskRestServiceGoedkeurenTest : BehaviorSpec({
             logger.info { "Response: ${body.string()}" }
             code shouldBe HTTP_NO_CONTENT
         }
-        // TODO: add a document
+        When(
+            """
+            the create enkelvoudig informatie object with file upload endpoint is called for the zaak with a TXT file
+            """
+        ) {
+            val endpointUrl =
+                "$ZAC_API_URI/informatieobjecten/informatieobject/$zaakUUID/$zaakUUID"
+            val file = Thread.currentThread().contextClassLoader.getResource(TEST_TXT_FILE_NAME).let {
+                File(URLDecoder.decode(it!!.path, Charsets.UTF_8))
+            }
+            val requestBody =
+                MultipartBody.Builder()
+                    .setType(MultipartBody.FORM)
+                    .addFormDataPart("bestandsnaam", TEST_TXT_FILE_NAME)
+                    .addFormDataPart("titel", DOCUMENT_FILE_TITLE)
+                    .addFormDataPart("bestandsomvang", file.length().toString())
+                    .addFormDataPart("formaat", TEXT_MIME_TYPE)
+                    .addFormDataPart(
+                        "file",
+                        TEST_TXT_FILE_NAME,
+                        file.asRequestBody(TEXT_MIME_TYPE.toMediaType())
+                    )
+                    .addFormDataPart("informatieobjectTypeUUID", INFORMATIE_OBJECT_TYPE_BIJLAGE_UUID)
+                    .addFormDataPart(
+                        "vertrouwelijkheidaanduiding",
+                        DOCUMENT_VERTROUWELIJKHEIDS_AANDUIDING_OPENBAAR
+                    )
+                    .addFormDataPart("status", DOCUMENT_STATUS_IN_BEWERKING)
+                    .addFormDataPart(
+                        "creatiedatum",
+                        DateTimeFormatter.ofPattern(
+                            "yyyy-MM-dd'T'HH:mm+01:00"
+                        ).format(ZonedDateTime.now())
+                    )
+                    .addFormDataPart("auteur", TEST_USER_1_NAME)
+                    .addFormDataPart("taal", "dut")
+                    .build()
+            val response = itestHttpClient.performPostRequest(
+                url = endpointUrl,
+                headers = Headers.headersOf(
+                    "Accept",
+                    "application/json",
+                    "Content-Type",
+                    "multipart/form-data"
+                ),
+                requestBody = requestBody
+            )
+
+            Then("the response should be OK and contain information for the created document") {
+                val responseBody = response.body.string()
+                logger.info { "$endpointUrl response: $responseBody" }
+                response.code shouldBe HTTP_OK
+                enkelvoudigInformatieObjectUUID = UUID.fromString(JSONObject(responseBody).getString("uuid"))
+            }
+        }
         // TODO: start the 'Goedkeuren' task
         // TODO: complete the 'Goedkeuren' task by approving the document
     }
