41f8d8ff5799a2c563897eaf97280006170f9ec9
Sander <mail@sanderboer.nl>
2025-08-18 20:35:57 +0200

    refactor(formio): make use of the `GeneratedTypes`

--- src/main/app/src/app/admin/formio-formulieren.service.ts
+++ src/main/app/src/app/admin/formio-formulieren.service.ts
@@ -3,48 +3,29 @@
  * SPDX-License-Identifier: EUPL-1.2+
  */
 
-import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { catchError } from "rxjs/operators";
-import { FoutAfhandelingService } from "../fout-afhandeling/fout-afhandeling.service";
-import { FormioFormulier } from "./model/formio-formulier";
-import { FormioFormulierContent } from "./model/formio-formulier-content";
+import { PostBody, ZacHttpClient } from "../shared/http/zac-http-client";
 
 @Injectable({
   providedIn: "root",
 })
 export class FormioFormulierenService {
-  private basepath = "/rest/formio-formulieren";
+  constructor(private readonly zacHttpClient: ZacHttpClient) {}
 
-  constructor(
-    private http: HttpClient,
-    private foutAfhandelingService: FoutAfhandelingService,
-  ) {}
-
-  listFormioFormulieren(): Observable<FormioFormulier[]> {
-    return this.http
-      .get<FormioFormulier[]>(`${this.basepath}`)
-      .pipe(
-        catchError((err) => this.foutAfhandelingService.foutAfhandelen(err)),
-      );
+  listFormioFormulieren() {
+    return this.zacHttpClient.GET("/rest/formio-formulieren");
   }
 
   uploadFormioFormulier(
-    formioFormulierContent: FormioFormulierContent,
+    body: PostBody<"/rest/formio-formulieren">,
   ): Observable<void> {
-    return this.http
-      .post<void>(`${this.basepath}`, formioFormulierContent)
-      .pipe(
-        catchError((err) => this.foutAfhandelingService.foutAfhandelen(err)),
-      );
+    return this.zacHttpClient.POST("/rest/formio-formulieren", body);
   }
 
-  deleteFormioFormulier(formioFormulier: FormioFormulier): Observable<void> {
-    return this.http
-      .delete<void>(`${this.basepath}/${formioFormulier.id}`)
-      .pipe(
-        catchError((err) => this.foutAfhandelingService.foutAfhandelen(err)),
-      );
+  deleteFormioFormulier(id: number) {
+    return this.zacHttpClient.DELETE("/rest/formio-formulieren/{id}", {
+      path: { id },
+    });
   }
 }
