590ae26cbba3e2999d38e5f3868eb68d926f3d0b
ismail ozgen <ismail@info.nl>
2025-08-18 11:02:12 +0300

    feat: Integrate per-zaaktype PABC roles into IAM & initial use in createZaak authorization
    This change is the first step in migrating from Keycloak-based global functional roles to PABC-based application roles defined per zaaktype.
    
    Solves [PZ-7826]

--- src/itest/kotlin/nl/info/zac/itest/config/ProjectConfig.kt
+++ src/itest/kotlin/nl/info/zac/itest/config/ProjectConfig.kt
@@ -58,7 +58,7 @@ class ProjectConfig : AbstractProjectConfig() {
         "ADDITIONAL_ALLOWED_FILE_TYPES" to ADDITIONAL_ALLOWED_FILE_TYPES,
         "BAG_API_CLIENT_MP_REST_URL" to "$BAG_MOCK_BASE_URI/lvbag/individuelebevragingen/v2/",
         "FEATURE_FLAG_BPMN_SUPPORT" to "true",
-        "FEATURE_FLAG_PABC_INTEGRATION" to "true",
+        "FEATURE_FLAG_PABC_INTEGRATION" to "false",
         "KVK_API_CLIENT_MP_REST_URL" to KVK_MOCK_BASE_URI,
         "OFFICE_CONVERTER_CLIENT_MP_REST_URL" to OFFICE_CONVERTER_BASE_URI,
         "PABC_API_CLIENT_MP_REST_URL" to "$PABC_CLIENT_BASE_URI/pabc/api/v1/",
