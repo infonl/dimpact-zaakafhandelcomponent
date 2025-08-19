1465bc44a9a05ac94e3d9aa4ad9c86866a5f5e80
Edgar Vonk <edgar@info.nl>
2025-08-19 14:47:56 +0200

    chore: no longer log ZGW validation errors inside a Flowable context as exceptions with log level ERROR

--- src/main/kotlin/net/atos/zac/flowable/cmmn/exception/CmmnExceptions.kt
+++ src/main/kotlin/net/atos/zac/flowable/cmmn/exception/CmmnExceptions.kt
@@ -4,6 +4,20 @@
  */
 package net.atos.zac.flowable.cmmn.exception
 
+import net.atos.client.zgw.shared.exception.ZgwValidationErrorException
+import org.flowable.common.engine.api.FlowableException
+
 class OpenTaskItemNotFoundException(override val message: String) : RuntimeException(message)
 
 class CaseDefinitionNotFoundException(override val message: String) : RuntimeException(message)
+
+/**
+ * Custom Flowable exception that wraps ZGW validation errors. It is used to throw ZGW validation errors from inside
+ * a Flowable context so that for these exceptions the Flowable command context will log them at INFO log level
+ * instead of the default ERROR log level.
+ */
+class FlowableZgwValidationErrorException(
+    override val message: String,
+    override val cause: ZgwValidationErrorException
+) :
+    FlowableException(message, cause)
