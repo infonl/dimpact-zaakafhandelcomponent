af061601440f6733878325e3ddbc54f772a58d04
Hristo Iliev <hristo.iliev@info.nl>
2025-08-25 15:53:49 +0300

    Revert "feature: make zaaktype_omschrijving not nullable"
    This reverts commit c9502ca2962f90788e3b6a6788dbc88a39e6b161.

--- src/main/resources/schemas/V76__add_product_aanvraag_type_zaaktype_omscrijving_to_zaaktype_bpmn_process.sql
+++ src/main/resources/schemas/V76__add_product_aanvraag_type_zaaktype_omscrijving_to_zaaktype_bpmn_process.sql
@@ -6,10 +6,3 @@
 ALTER TABLE ${schema}.zaaktype_bpmn_process_definition
     ADD COLUMN zaaktype_omschrijving VARCHAR,
     ADD COLUMN productaanvraagtype VARCHAR;
-
-UPDATE ${schema}.zaaktype_bpmn_process_definition
-    SET zaaktype_omschrijving = 'na'
-    WHERE zaaktype_omschrijving IS NULL;
-
-ALTER TABLE ${schema}.zaaktype_bpmn_process_definition
-    ALTER COLUMN zaaktype_omschrijving SET NOT NULL;
