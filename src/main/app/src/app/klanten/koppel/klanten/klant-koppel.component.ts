8b48e6cce5735f9c1b4b86ad210b505301b48d4a
Sander <mail@sanderboer.nl>
2025-09-03 09:19:25 +0200

    typescript fixes

--- src/main/app/src/app/klanten/koppel/klanten/klant-koppel.component.ts
+++ src/main/app/src/app/klanten/koppel/klanten/klant-koppel.component.ts
@@ -102,10 +102,10 @@ import { KlantKoppelInitiator } from "./klant-koppel-initiator.component";
 })
 export class KlantKoppelComponent {
   @Input() initiator = false;
-  @Input() zaaktypeUUID: string;
-  @Input() sideNav: MatDrawer;
-  @Input() allowPersoon: boolean;
-  @Input() allowBedrijf: boolean;
+  @Input() zaaktypeUUID?: string | null = null;
+  @Input({ required: true }) sideNav!: MatDrawer;
+  @Input() allowPersoon?: boolean;
+  @Input() allowBedrijf?: boolean;
   @Output() klantGegevens = new EventEmitter<KlantGegevens>();
 
   context = input.required<string>();
