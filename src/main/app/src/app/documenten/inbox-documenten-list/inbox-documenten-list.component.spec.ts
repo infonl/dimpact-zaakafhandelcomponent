afebb28cbde9964f35a76f8baf888685233a92a9
Dax Riool <76945092+DaxRiool@users.noreply.github.com>
2025-09-11 14:34:30 +0200

    Update src/main/app/src/app/documenten/inbox-documenten-list/inbox-documenten-list.component.spec.ts
    header update
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/app/src/app/documenten/inbox-documenten-list/inbox-documenten-list.component.spec.ts
+++ src/main/app/src/app/documenten/inbox-documenten-list/inbox-documenten-list.component.spec.ts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2025 INFO.nl
+ * SPDX-License-Identifier: EUPL-1.2+
+ */
+
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { EventEmitter } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
