9943e8ca331b9140d7295e826077d19c45e562eb
Sander <mail@sanderboer.nl>
2025-09-16 16:40:34 +0200

    update expectations

--- src/itest/kotlin/nl/info/zac/itest/config/ItestConfiguration.kt
+++ src/itest/kotlin/nl/info/zac/itest/config/ItestConfiguration.kt
@@ -137,7 +137,7 @@ object ItestConfiguration {
     const val TOTAL_COUNT_INDEXED_ZAKEN = 13
     const val TOTAL_COUNT_INDEXED_ZAKEN_AFGEROND = 2
     const val TOTAL_COUNT_INDEXED_TASKS = 4
-    const val TOTAL_COUNT_INDEXED_DOCUMENTS = 11
+    const val TOTAL_COUNT_INDEXED_DOCUMENTS = 13
 
     /**
      * Test person that exists in both the BRP and the Klanten API databases
