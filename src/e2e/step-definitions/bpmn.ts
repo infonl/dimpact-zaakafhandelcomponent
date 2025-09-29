bd5d98fc6772c06c83a0aa5591165d0997c84a97
Hristo Iliev <hsiliev@gmail.com>
2025-09-29 13:47:16 +0300

    fix: searchbox selector for BPMN e2e tests (#4426)
    e2e tests fail to find the list of documents attached to a task/zaak
    
    Solves PZ-8789

--- src/e2e/step-definitions/bpmn.ts
+++ src/e2e/step-definitions/bpmn.ts
@@ -37,7 +37,9 @@ Then(
       this.page.getByRole("button", { name: "Create" }),
     ).toBeVisible();
     await expect(
-      this.page.getByRole("textbox", { name: "Select one or more documents" }),
+      this.page.getByRole("searchbox", {
+        name: "Select one or more documents",
+      }),
     ).toBeVisible();
     await expect(this.page.getByLabel("Communication channel")).toBeVisible();
   },
@@ -169,10 +171,16 @@ When(
     await this.page.getByLabel("User").click();
     await this.page.getByLabel("User").selectOption("functioneelbeheerder2");
     await this.page
-      .getByRole("textbox", { name: "Select one or more documents" })
+      .getByRole("searchbox", { name: "Select one or more documents" })
       .fill("");
-    await this.page.getByLabel("Test form").getByText("file A").click();
-    await this.page.getByLabel("Test form").getByText("file B").click();
+    await this.page
+      .getByLabel("Test form")
+      .getByText("file A", { exact: true })
+      .click();
+    await this.page
+      .getByLabel("Test form")
+      .getByText("file B", { exact: true })
+      .click();
     await this.page.getByLabel("Communication channel").selectOption("E-mail");
     await this.page.getByLabel("Select result").click();
     await this.page.getByLabel("Select result").selectOption("Verleend");
