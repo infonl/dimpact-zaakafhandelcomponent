07c703a261d2aceff4131cddb09371aef2cebada
Sander <mail@sanderboer.nl>
2025-10-09 14:56:35 +0200

    spotless

--- src/main/app/src/app/taken/taak-view/taak-view.component.spec.ts
+++ src/main/app/src/app/taken/taak-view/taak-view.component.spec.ts
@@ -13,7 +13,6 @@ import {
 import { provideHttpClientTesting } from "@angular/common/http/testing";
 import { ComponentRef } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { FormControl } from "@angular/forms";
 import { MatFormFieldHarness } from "@angular/material/form-field/testing";
 import { MatInputHarness } from "@angular/material/input/testing";
 import { MatSidenav } from "@angular/material/sidenav";
@@ -216,7 +215,7 @@ describe(TaakViewComponent.name, () => {
       jest
         .spyOn(taakFormulierenService, "getAngularHandleFormBuilder")
         .mockResolvedValue([
-          [{ type: "input", key: "question" }, new FormControl("")],
+          { type: "input", key: "question" },
         ]);
     });
 
