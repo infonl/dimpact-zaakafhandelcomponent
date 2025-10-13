44e88b2c1878e1b070b96543aa10f366d06c153f
Hristo Iliev <hsiliev@gmail.com>
2025-10-13 14:08:01 +0300

    fix: remove logging

--- src/main/kotlin/nl/info/zac/configuratie/BrpConfiguration.kt
+++ src/main/kotlin/nl/info/zac/configuratie/BrpConfiguration.kt
@@ -10,7 +10,6 @@ import nl.info.zac.util.AllOpen
 import nl.info.zac.util.NoArgConstructor
 import org.eclipse.microprofile.config.inject.ConfigProperty
 import java.util.Optional
-import java.util.logging.Logger
 import kotlin.jvm.optionals.getOrNull
 
 @ApplicationScoped
@@ -36,21 +35,17 @@ class BrpConfiguration @Inject constructor(
     val processingRegisterDefault: Optional<String>
 ) {
     companion object {
-        private val LOG = Logger.getLogger(BrpConfiguration::class.java.name)
         val SUPPORTED_PROTOCOLLERING_PROVIDERS = arrayOf("iConnect", "2Secure")
     }
 
     fun isBrpProtocolleringEnabled(): Boolean = originOIN.isPresent
 
-    fun readBrpProtocolleringProvider(): String {
-        LOG.warning { "BRP_PROTOCOLLERING environment variable value: ${auditLogProvider.getOrNull()}" }
-        LOG.warning { "Environment variables: ${System.getenv().entries.joinToString { "${it.key}: ${it.value}" }}" }
-        return requireNotNull(auditLogProvider.getOrNull().takeIf { it in SUPPORTED_PROTOCOLLERING_PROVIDERS }) {
+    fun readBrpProtocolleringProvider(): String =
+        requireNotNull(auditLogProvider.getOrNull().takeIf { it in SUPPORTED_PROTOCOLLERING_PROVIDERS }) {
             if (auditLogProvider.isPresent) {
                 "Invalid environment variable 'BRP_PROTOCOLLERING' value '$auditLogProvider'. Supported: " + SUPPORTED_PROTOCOLLERING_PROVIDERS.joinToString()
             } else {
                 "Missing environment variable 'BRP_PROTOCOLLERING'. Supported: " + SUPPORTED_PROTOCOLLERING_PROVIDERS.joinToString()
             }
         }
-    }
 }
