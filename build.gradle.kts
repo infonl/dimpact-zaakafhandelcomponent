3d10e351af3d62ea5c170e127b67cba11bbc86d5
Bas de Wit <52208549+bas-info-nl@users.noreply.github.com>
2025-10-15 11:47:48 +0200

    fix owasp-dependencycheck plugin alias referece

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ plugins {
     alias(libs.plugins.openapi)
     alias(libs.plugins.detekt)
     alias(libs.plugins.spotless)
-    alias(libs.plugins.owasp.dependencycheck)
+    alias(libs.plugins.owasp-dependencycheck)
     alias(libs.plugins.allopen)
     alias(libs.plugins.noarg)
 }
