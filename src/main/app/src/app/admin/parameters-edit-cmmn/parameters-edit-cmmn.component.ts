da924a2e04a4d5c510a20e3ea5a7f17562b8d120
infonl-marcel <82890006+infonl-marcel@users.noreply.github.com>
2025-10-20 14:32:51 +0200

    feat(app): File renames to indicate its code is related to CMMN (#4558)
    FE - File renames to indicate its code is related to CMMN
    
    Solves PZ-9021

--- src/main/app/src/app/admin/parameters-edit-cmmn/parameters-edit-cmmn.component.ts
+++ src/main/app/src/app/admin/parameters-edit-cmmn/parameters-edit-cmmn.component.ts
@@ -41,8 +41,8 @@ import { SmartDocumentsFormComponent } from "./smart-documents-form/smart-docume
 
 @Component({
   selector: "zac-parameters-edit-cmmn",
-  templateUrl: "./parameter-edit.component.html",
-  styleUrls: ["./parameter-edit.component.less"],
+  templateUrl: "./parameters-edit-cmmn.component.html",
+  styleUrls: ["./parameters-edit-cmmn.component.less"],
 })
 export class ParametersEditCmmnComponent implements OnDestroy, AfterViewInit {
   @Input({ required: true }) showFirstStep: boolean = false;
