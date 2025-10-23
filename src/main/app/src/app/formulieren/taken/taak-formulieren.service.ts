a471c67575811205531e467685fb0c86596e6439
infonl-marcel <82890006+infonl-marcel@users.noreply.github.com>
2025-10-23 13:28:43 +0200

    feat(app): Revert to previous Aanvullende informatie task form (#4597)
    FE - Revert to previous Aanvullende informatie task form
    
    Solves PZ-9148

--- src/main/app/src/app/formulieren/taken/taak-formulieren.service.ts
+++ src/main/app/src/app/formulieren/taken/taak-formulieren.service.ts
@@ -5,6 +5,7 @@
 
 import { inject, Injectable } from "@angular/core";
 import { TranslateService } from "@ngx-translate/core";
+import { KlantenService } from "src/app/klanten/klanten.service";
 import { InformatieObjectenService } from "../../informatie-objecten/informatie-objecten.service";
 import { MailtemplateService } from "../../mailtemplate/mailtemplate.service";
 import { FormField } from "../../shared/form/form";
@@ -14,6 +15,7 @@ import { ZakenService } from "../../zaken/zaken.service";
 import { AanvullendeInformatieFormulier } from "./model/aanvullende-informatie";
 import { Advies } from "./model/advies";
 import { DefaultTaakformulier } from "./model/default-taakformulier";
+import { AanvullendeInformatieDeprecated } from "./model/deprecated/aanvullende-informatie";
 import { DocumentVerzendenPost } from "./model/document-verzenden-post";
 import { ExternAdviesMail } from "./model/extern-advies-mail";
 import { ExternAdviesVastleggen } from "./model/extern-advies-vastleggen";
@@ -25,6 +27,8 @@ import { TaakFormulierBuilder } from "./taak-formulier-builder";
 })
 export class TaakFormulierenService {
   private readonly goedkeurenFormulier = inject(GoedkeurenFormulier);
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   private readonly aanvullendeInformatieFormulier = inject(
     AanvullendeInformatieFormulier,
   );
@@ -33,6 +37,7 @@ export class TaakFormulierenService {
     private readonly informatieObjectenService: InformatieObjectenService,
     private readonly takenService: TakenService,
     private readonly zakenService: ZakenService,
+    private readonly klantenService: KlantenService,
     private readonly mailtemplateService: MailtemplateService,
   ) {}
 
@@ -43,8 +48,8 @@ export class TaakFormulierenService {
     switch (formulierDefinitie) {
       case "GOEDKEUREN":
         return this.goedkeurenFormulier.requestForm(zaak);
-      case "AANVULLENDE_INFORMATIE":
-        return this.aanvullendeInformatieFormulier.requestForm(zaak);
+      // case "AANVULLENDE_INFORMATIE":
+      //   return this.aanvullendeInformatieFormulier.requestForm(zaak);
       default:
         throw new Error(
           `Onbekende formulierDefinitie for Angular form: ${formulierDefinitie}`,
@@ -54,13 +59,14 @@ export class TaakFormulierenService {
 
   public async getAngularHandleFormBuilder(
     taak: GeneratedType<"RestTask">,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     zaak: GeneratedType<"RestZaak">,
   ): Promise<FormField[]> {
     switch (taak.formulierDefinitieId) {
       case "GOEDKEUREN":
         return this.goedkeurenFormulier.handleForm(taak);
-      case "AANVULLENDE_INFORMATIE":
-        return this.aanvullendeInformatieFormulier.handleForm(taak, zaak);
+      // case "AANVULLENDE_INFORMATIE":
+      //   return this.aanvullendeInformatieFormulier.handleForm(taak, zaak);
       default:
         throw new Error(
           `${taak.formulierDefinitie}: Onbekende formulierDefinitie for Angular`,
@@ -80,8 +86,15 @@ export class TaakFormulierenService {
           ),
         );
       case "AANVULLENDE_INFORMATIE":
-        throw new Error(
-          `${formulierDefinitie} is DEPRECATED, use Angular form`,
+        return new TaakFormulierBuilder(
+          new AanvullendeInformatieDeprecated(
+            this.translate,
+            this.takenService,
+            this.informatieObjectenService,
+            this.mailtemplateService,
+            this.klantenService,
+            this.zakenService,
+          ),
         );
       case "ADVIES":
         return new TaakFormulierBuilder(
