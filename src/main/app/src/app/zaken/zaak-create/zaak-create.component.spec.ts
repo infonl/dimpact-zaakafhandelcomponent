edb49e36d801c47e8790b4ee8639a14b54f3a881
infonl-marcel <marcele@info.nl>
2025-11-11 13:08:08 +0100

    extended harness spec

--- src/main/app/src/app/zaken/zaak-create/zaak-create.component.spec.ts
+++ src/main/app/src/app/zaken/zaak-create/zaak-create.component.spec.ts
@@ -33,10 +33,16 @@ import { NavigationService } from "../../shared/navigation/navigation.service";
 import { GeneratedType } from "../../shared/utils/generated-types";
 import { ZakenService } from "../zaken.service";
 import { ZaakCreateComponent } from "./zaak-create.component";
+import { MatAutocompleteHarness } from "@angular/material/autocomplete/testing";
+import { MatInputHarness } from "@angular/material/input/testing";
+import { FoutAfhandelingService } from "src/app/fout-afhandeling/fout-afhandeling.service";
+import { input } from "@angular/core";
+import { BpmnConfigurationService } from "src/app/admin/bpmn-configuration.service";
 
 describe(ZaakCreateComponent.name, () => {
   let identityService: IdentityService;
   let zakenService: ZakenService;
+  let bpmnConfigurationService: BpmnConfigurationService;
   let fixture: ComponentFixture<ZaakCreateComponent>;
   let loader: HarnessLoader;
   let component: ZaakCreateComponent;
@@ -46,6 +52,7 @@ describe(ZaakCreateComponent.name, () => {
       declarations: [ZaakCreateComponent, ZacInput],
       providers: [
         ZakenService,
+        BpmnConfigurationService,
         NavigationService,
         KlantenService,
         ReferentieTabelService,
@@ -65,13 +72,17 @@ describe(ZaakCreateComponent.name, () => {
         MatLabel,
         FormsModule,
         ReactiveFormsModule,
+        NoopAnimationsModule,
       ],
     }).compileComponents();
 
     identityService = TestBed.inject(IdentityService);
-    jest
-      .spyOn(identityService, "listGroups")
-      .mockReturnValue(of([{ id: "test-group-id", naam: "test group" }]));
+    jest.spyOn(identityService, "listGroups").mockReturnValue(
+      of([
+        { id: "test-cmmn-group-id", naam: "test group CMMN" },
+        { id: "test-bpmn-group-id", naam: "test group BPMN" },
+      ]),
+    );
     jest
       .spyOn(identityService, "listUsersInGroup")
       .mockReturnValue(of([{ id: "test-user-id", naam: "test user" }]));
@@ -80,25 +91,171 @@ describe(ZaakCreateComponent.name, () => {
     jest.spyOn(zakenService, "listZaaktypesForCreation").mockReturnValue(
       of([
         fromPartial<GeneratedType<"RestZaaktype">>({
-          uuid: "test-zaaktype-1",
-          omschrijving: "test-description-1",
+          uuid: "test-cmmn-zaaktype-1",
+          omschrijving: "test-cmmn-description-1",
+          zaakafhandelparameters: {
+            defaultGroepId: "test-cmmn-group-id",
+            defaultBehandelaarId: "test-user-id",
+          },
         }),
         fromPartial<GeneratedType<"RestZaaktype">>({
-          uuid: "test-zaaktype-2",
-          omschrijving: "test-description-2",
+          uuid: "test-cmmn-zaaktype-2",
+          omschrijving: "test-cmmn-description-2",
+        }),
+        fromPartial<GeneratedType<"RestZaaktype">>({
+          uuid: "test-bpmn-zaaktype-1",
+          omschrijving: "test-bpmn-description-1",
+          zaakafhandelparameters: { defaultGroepId: "test-bpmn-group-id" },
         }),
       ]),
     );
+
+    bpmnConfigurationService = TestBed.inject(BpmnConfigurationService);
+    jest
+      .spyOn(bpmnConfigurationService, "listbpmnProcessConfigurations")
+      .mockReturnValue(
+        of([
+          fromPartial<GeneratedType<"RestZaaktypeBpmnConfiguration">>({
+            bpmnProcessDefinitionKey: "bpmn-process-1",
+            zaaktypeUuid: "test-bpmn-zaaktype-1",
+          }),
+        ]),
+      );
+
     fixture = TestBed.createComponent(ZaakCreateComponent);
     component = fixture.componentInstance;
     loader = TestbedHarnessEnvironment.loader(fixture);
   });
 
   describe(ZaakCreateComponent.prototype.caseTypeSelected.name, () => {
-    // TODO validate that we can use the `MatAutocompleteHarness` to write this test
-    it.todo(`should set the default group`);
+    beforeAll(() => {
+      // disable animations for the tests
+      (Element.prototype as any).animate = () => {
+        const listeners: Record<string, Function[]> = {};
 
-    it.todo(`should set the default case worker`);
+        const player = {
+          play: () => {},
+          pause: () => {},
+          cancel: () => {},
+          finish: () => {
+            (listeners["finish"] || []).forEach((cb) => cb());
+          },
+          addEventListener: (name: string, cb: Function) => {
+            (listeners[name] ||= []).push(cb);
+          },
+          removeEventListener: (name: string, cb: Function) => {
+            listeners[name] = (listeners[name] || []).filter((fn) => fn !== cb);
+          },
+          finished: Promise.resolve(),
+        };
+
+        return player;
+      };
+    });
+
+    beforeEach(() => {
+      const router = TestBed.inject(Router);
+      jest.spyOn(router, "navigate").mockImplementation(async () => true);
+    });
+
+    it("should handle CMMN case type selection", async () => {
+      let inputs = await loader.getAllHarnesses(MatInputHarness);
+      expect(inputs.length).toEqual(8);
+      expect(zakenService.listZaaktypesForCreation).toHaveBeenCalled();
+
+      // Select case type
+      await inputs[0].focus();
+      await inputs[0].setValue("cmmn");
+
+      let autocomplete = await loader.getHarness(
+        MatAutocompleteHarness.with({
+          selector: '[ng-reflect-name="zaaktype"]',
+        }),
+      );
+      let options = await autocomplete.getOptions();
+      expect(options.length).toEqual(2);
+
+      await options[0].click();
+      let value = await inputs[0].getValue();
+      expect(value).toBeTruthy();
+
+      // select group
+      expect(identityService.listGroups).toHaveBeenCalled();
+      await inputs[4].focus();
+      await inputs[4].setValue("cmmn");
+
+      autocomplete = await loader.getHarness(
+        MatAutocompleteHarness.with({
+          selector: '[ng-reflect-name="groep"]',
+        }),
+      );
+      options = await autocomplete.getOptions();
+      expect(options.length).toEqual(1);
+
+      await options[0].click();
+      value = await inputs[0].getValue();
+      expect(value).toBeTruthy();
+
+      // select employee
+      expect(identityService.listUsersInGroup).toHaveBeenCalled();
+      await inputs[5].focus();
+      await inputs[5].setValue("test");
+
+      autocomplete = await loader.getHarness(
+        MatAutocompleteHarness.with({
+          selector: '[ng-reflect-name="behandelaar"]',
+        }),
+      );
+      options = await autocomplete.getOptions();
+      expect(options.length).toEqual(1);
+
+      await options[0].click();
+      value = await inputs[0].getValue();
+      expect(value).toBeTruthy();
+    });
+
+    it("should handle BPMN case type selection", async () => {
+      let inputs = await loader.getAllHarnesses(MatInputHarness);
+      expect(inputs.length).toEqual(8);
+      expect(zakenService.listZaaktypesForCreation).toHaveBeenCalled();
+
+      // Select case type
+      await inputs[0].focus();
+      await inputs[0].setValue("bpmn");
+
+      let autocomplete = await loader.getHarness(
+        MatAutocompleteHarness.with({
+          selector: '[ng-reflect-name="zaaktype"]',
+        }),
+      );
+      let options = await autocomplete.getOptions();
+      expect(options.length).toEqual(1);
+
+      await options[0].click();
+      let value = await inputs[0].getValue();
+      expect(value).toBeTruthy();
+
+      // select group
+      expect(identityService.listGroups).toHaveBeenCalled();
+      await inputs[4].focus();
+      await inputs[4].setValue("bpmn");
+
+      autocomplete = await loader.getHarness(
+        MatAutocompleteHarness.with({
+          selector: '[ng-reflect-name="groep"]',
+        }),
+      );
+      options = await autocomplete.getOptions();
+      expect(options.length).toEqual(1);
+
+      await options[0].click();
+      value = await inputs[0].getValue();
+      expect(value).toBeTruthy();
+
+      // select employee
+      expect(identityService.listUsersInGroup).toHaveBeenCalledTimes(0);
+      expect(await inputs[5].isDisabled()).toBe(true);
+    });
 
     it.todo(`should set the confidentiality notice`);
   });
