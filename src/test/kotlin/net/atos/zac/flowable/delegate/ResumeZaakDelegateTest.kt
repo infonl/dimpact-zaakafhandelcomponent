16d029d5f03efaffd78a37aaf1a0ec6781271582
Hristo Iliev <hsiliev@gmail.com>
2025-11-12 14:58:36 +0200

    feature: add resume date to resume BPMN delegate
    resume date is optional
    
    Solves PZ-7724

--- src/test/kotlin/net/atos/zac/flowable/delegate/ResumeZaakDelegateTest.kt
+++ src/test/kotlin/net/atos/zac/flowable/delegate/ResumeZaakDelegateTest.kt
@@ -7,6 +7,7 @@ package net.atos.zac.flowable.delegate
 
 import io.kotest.core.spec.style.BehaviorSpec
 import io.mockk.checkUnnecessaryStub
+import io.mockk.clearMocks
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkStatic
@@ -18,6 +19,7 @@ import nl.info.client.zgw.zrc.ZrcClientService
 import nl.info.zac.shared.helper.SuspensionZaakHelper
 import org.flowable.common.engine.impl.el.JuelExpression
 import org.flowable.engine.delegate.DelegateExecution
+import java.time.ZonedDateTime
 
 class ResumeZaakDelegateTest : BehaviorSpec({
     val delegateExecution = mockk<DelegateExecution>()
@@ -26,6 +28,7 @@ class ResumeZaakDelegateTest : BehaviorSpec({
     val suspensionZaakHelper = mockk<SuspensionZaakHelper>()
     val zaak = createZaak()
     val reason = "test"
+    val date = ZonedDateTime.now()
 
     beforeEach {
         checkUnnecessaryStub()
@@ -47,24 +50,49 @@ class ResumeZaakDelegateTest : BehaviorSpec({
         val hervattenRedenExpression = mockk<JuelExpression>()
         every { hervattenRedenExpression.getValue(delegateExecution) } returns reason
 
-        every { suspensionZaakHelper.resumeZaak(zaak, reason) } returns zaak
+        every { suspensionZaakHelper.resumeZaak(zaak, reason, any()) } returns zaak
 
-        val suspendZaakDelegate = ResumeZaakDelegate().apply {
-            hervattenReden = hervattenRedenExpression
-        }
-
-        When("zaak is resumed from service task") {
+        When("zaak is resumed from service task with no 'hervattenDatum'") {
+            val suspendZaakDelegate = ResumeZaakDelegate().apply {
+                hervattenReden = hervattenRedenExpression
+            }
             suspendZaakDelegate.execute(delegateExecution)
 
-            Then("expression is resolved") {
+            Then("expressions resolution is attempted") {
                 verify(exactly = 1) {
                     hervattenRedenExpression.getValue(delegateExecution)
                 }
             }
 
-            And("the zaak resume is called") {
+            And("the zaak resume is called with correct parameters") {
                 verify(exactly = 1) {
-                    suspensionZaakHelper.resumeZaak(zaak, reason)
+                    suspensionZaakHelper.resumeZaak(zaak, reason, any())
+                }
+            }
+        }
+
+        When("zaak is resumed from service task with 'hervattenDatum'") {
+            clearMocks(hervattenRedenExpression, answers = false)
+
+            val hervattenDatumExpression = mockk<JuelExpression>()
+            every { hervattenDatumExpression.getValue(delegateExecution) } returns date.toString()
+
+            val suspendZaakDelegate = ResumeZaakDelegate().apply {
+                hervattenReden = hervattenRedenExpression
+                hervattenDatum = hervattenDatumExpression
+            }
+            suspendZaakDelegate.execute(delegateExecution)
+
+            Then("expressions resolution is attempted") {
+                verify(exactly = 1) {
+                    hervattenRedenExpression.getValue(delegateExecution)
+                    hervattenDatumExpression.getValue(delegateExecution)
+                }
+            }
+
+            And("the zaak resume is called with correct parameters") {
+                verify(exactly = 1) {
+                    suspensionZaakHelper.resumeZaak(zaak, reason, date)
                 }
             }
         }
