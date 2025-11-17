ee414db53be4721e5c83f2c639db4549df524ea0
Sander <mail@sanderboer.nl>
2025-11-17 17:39:18 +0100

    test(app.component): ensure deleting of previous user session

---
+++ src/main/app/src/app/app.component.spec.ts
@@ -0,0 +1,46 @@
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from "@angular/common/http";
+import { provideHttpClientTesting } from "@angular/common/http/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { TranslateModule } from "@ngx-translate/core";
+import { provideQueryClient } from "@tanstack/angular-query-experimental";
+import { testQueryClient } from "../../setupJest";
+import { AppComponent } from "./app.component";
+import { IdentityService } from "./identity/identity.service";
+
+describe(AppComponent.name, () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<typeof component>;
+  let identityService: IdentityService;
+  const removeQueriesSpy = jest.spyOn(testQueryClient, "removeQueries");
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      providers: [
+        IdentityService,
+        provideHttpClient(withInterceptorsFromDi()),
+        provideHttpClientTesting(),
+        provideQueryClient(testQueryClient),
+      ],
+      imports: [TranslateModule.forRoot()],
+    });
+
+    identityService = TestBed.inject(IdentityService);
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should delete the logged in user session on init", () => {
+
+    expect(removeQueriesSpy).toHaveBeenCalledWith(
+      expect.objectContaining({
+        queryKey: identityService.readLoggedInUser().queryKey,
+      }),
+    );
+  });
+});
