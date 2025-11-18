94a08eab73c713274d953ed9edcae25e257ad046
DaxRiool <dax_riool@hotmail.com>
2025-11-18 10:40:17 +0100

    removed comment from code

--- src/main/kotlin/nl/info/client/zgw/zrc/util/ZaakUtil.kt
+++ src/main/kotlin/nl/info/client/zgw/zrc/util/ZaakUtil.kt
@@ -10,13 +10,6 @@ fun Zaak.isOpen() = archiefnominatie == null
 
 fun Zaak.isOpgeschort() = opschorting != null && opschorting.getIndicatie()
 
-/**
-* We use opschorting reden field here as this is an easy way to know if a zaak was suspended in the past.
- * Another approach would be to parse the zaak history/audit log, but that requires a lot of custom logic.
- *
- * A new flag to handle this "suspended in the past" case was requested with:
- * https://github.com/open-zaak/open-zaak/issues/1920
-**/
 fun Zaak.isEerderOpgeschort() = opschorting?.eerdereOpschorting == true
 
 fun Zaak.isVerlengd() = verlenging?.getDuur() != null
