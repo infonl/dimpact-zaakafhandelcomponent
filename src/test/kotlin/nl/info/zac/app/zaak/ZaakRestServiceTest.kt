2c869b097f7b2902167ada300d02855951d68697
Edgar Vonk <edgar@info.nl>
2025-11-19 12:52:50 +0100

    chore: added backend field validation for update zaak endpoint

--- src/test/kotlin/nl/info/zac/app/zaak/ZaakRestServiceTest.kt
+++ src/test/kotlin/nl/info/zac/app/zaak/ZaakRestServiceTest.kt
@@ -64,7 +64,6 @@ import nl.info.client.zgw.zrc.model.generated.GeoJSONGeometry
 import nl.info.client.zgw.zrc.model.generated.MedewerkerIdentificatie
 import nl.info.client.zgw.zrc.model.generated.OrganisatorischeEenheidIdentificatie
 import nl.info.client.zgw.zrc.model.generated.Zaak
-import nl.info.client.zgw.zrc.model.generated.Zaak.OMSCHRIJVING_MAX_LENGTH
 import nl.info.client.zgw.ztc.ZtcClientService
 import nl.info.client.zgw.ztc.model.createRolType
 import nl.info.client.zgw.ztc.model.createZaakType
