c6f926db02ea57f783518b39450cf5e85ea25714
sander boer <mail@sanderboer.nl>
2025-11-26 12:32:33 +0100

    Update src/main/app/src/app/shared/material-form-builder/model/abstract-form-control-field.spec.ts
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/app/src/app/shared/material-form-builder/model/abstract-form-control-field.spec.ts
+++ src/main/app/src/app/shared/material-form-builder/model/abstract-form-control-field.spec.ts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2025 INFO.nl
+ * SPDX-License-Identifier: EUPL-1.2+
+ */
+
 import { AbstractFormControlField } from "./abstract-form-control-field";
 import { FieldType } from "./field-type.enum";
 
