e804301637ef12307c7a90ca1464b283923b553c
edgarvonk <edgar@info.nl>
2025-12-02 18:23:19 +0100

    chore: fixed and added more unit tests for document conversion button in informatie-object-view.component.ts

--- src/itest/kotlin/nl/info/zac/itest/ConfigurationRestServiceTest.kt
+++ src/itest/kotlin/nl/info/zac/itest/ConfigurationRestServiceTest.kt
@@ -11,10 +11,12 @@ import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import nl.info.zac.itest.client.ItestHttpClient
 import nl.info.zac.itest.client.authenticate
+import nl.info.zac.itest.config.ItestConfiguration
 import nl.info.zac.itest.config.ItestConfiguration.BRP_PROTOCOLLERING_ICONNECT
 import nl.info.zac.itest.config.ItestConfiguration.CONFIG_GEMEENTE_CODE
 import nl.info.zac.itest.config.ItestConfiguration.CONFIG_GEMEENTE_NAAM
 import nl.info.zac.itest.config.ItestConfiguration.CONFIG_MAX_FILE_SIZE_IN_MB
+import nl.info.zac.itest.config.ItestConfiguration.FEATURE_FLAG_PABC_INTEGRATION
 import nl.info.zac.itest.config.ItestConfiguration.ZAC_API_URI
 import nl.info.zac.itest.config.RAADPLEGER_DOMAIN_TEST_1
 import nl.info.zac.itest.util.shouldEqualJsonIgnoringOrder
@@ -162,11 +164,11 @@ class ConfigurationRestServiceTest : BehaviorSpec({
                 url = "$ZAC_API_URI/configuratie/feature-flags/pabc-integration"
             )
 
-            Then("'true' is returned because PABC integration is enabled for the integration tests") {
+            Then("'true' is returned if the PABC integration flag is enabled, 'false' otherwise") {
                 response.code shouldBe HTTP_OK
                 val responseBody = response.bodyAsString
                 logger.info { "Response: $responseBody" }
-                responseBody shouldEqualJson "true"
+                responseBody shouldEqualJson if (FEATURE_FLAG_PABC_INTEGRATION) "true" else "false"
             }
         }
         When("the audit log provider is retrieved") {
