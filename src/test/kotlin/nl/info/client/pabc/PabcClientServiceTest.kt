94679739d36405add2647efbe44ace1f2ceee9c5
Edgar Vonk <edgar@info.nl>
2025-12-09 09:17:42 +0100

    chore: updated PABC OpenAPI specs (#4868)
    Updated PABC OpenAPI specs so that we can use the new PABC API features.
    
    Solves PZ-9674

--- src/test/kotlin/nl/info/client/pabc/PabcClientServiceTest.kt
+++ src/test/kotlin/nl/info/client/pabc/PabcClientServiceTest.kt
@@ -12,49 +12,52 @@ import io.mockk.mockk
 import io.mockk.slot
 import nl.info.client.pabc.model.createApplicationRolesResponse
 import nl.info.client.pabc.model.generated.GetApplicationRolesRequest
+import java.util.UUID
 
 class PabcClientServiceTest : BehaviorSpec({
-
     val pabcClient = mockk<PabcClient>()
     val pabcClientService = PabcClientService(pabcClient)
-
-    val roles = listOf(
-        "fakeRole1",
-        "fakeRole2",
-        "fakeRole3"
-    )
-
-    val mockResponse = createApplicationRolesResponse()
 
     beforeEach {
         checkUnnecessaryStub()
     }
 
-    Given("invoke client with list of functional roles") {
-        val requestSlot = slot<GetApplicationRolesRequest>()
-        every {
-            pabcClient.getApplicationRolesPerEntityType(capture(requestSlot))
-        } returns mockResponse
+    Context("Getting application roles") {
+        Given("A PABC application roles response for a certain PABC request") {
+            val applicationId = UUID.randomUUID()
+            val applicationRolesResponse = createApplicationRolesResponse(
+                applicationId = applicationId,
+            )
+            val getApplicationRolesRequestSlot = slot<GetApplicationRolesRequest>()
+            every {
+                pabcClient.getApplicationRolesPerEntityType(capture(getApplicationRolesRequestSlot))
+            } returns applicationRolesResponse
 
-        When("getApplicationRoles is called") {
-            val result = pabcClientService.getApplicationRoles(roles)
-
-            Then("it should invoke the client with the given roles") {
-                result shouldBe mockResponse
-                requestSlot.isCaptured shouldBe true
-                requestSlot.captured.functionalRoleNames shouldBe listOf(
-                    "fakeRole1",
-                    "fakeRole2",
-                    "fakeRole3"
+            When("getApplicationRoles is called for a list of functional roles") {
+                val result = pabcClientService.getApplicationRoles(
+                    listOf(
+                        "fakeRole1",
+                        "fakeRole2",
+                        "fakeRole3"
+                    )
                 )
 
-                val responseModel = result.results[0]
-                responseModel.entityType.id shouldBe "zaaktype_test_1"
-                responseModel.entityType.name shouldBe "Test zaaktype 1"
-                responseModel.entityType.type shouldBe "zaaktype"
+                Then("it should invoke the client with the given roles") {
+                    result shouldBe applicationRolesResponse
+                    getApplicationRolesRequestSlot.isCaptured shouldBe true
+                    getApplicationRolesRequestSlot.captured.functionalRoleNames shouldBe listOf(
+                        "fakeRole1",
+                        "fakeRole2",
+                        "fakeRole3"
+                    )
 
-                responseModel.applicationRoles.forEach {
-                    it.application shouldBe "zaakafhandelcomponent"
+                    val responseModel = result.results[0]
+                    responseModel.entityType.id shouldBe "zaaktype_test_1"
+                    responseModel.entityType.name shouldBe "Test zaaktype 1"
+                    responseModel.entityType.type shouldBe "zaaktype"
+                    responseModel.applicationRoles.forEach {
+                        it.applicationId shouldBe applicationId
+                    }
                 }
             }
         }
