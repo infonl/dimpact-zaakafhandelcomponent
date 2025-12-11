627d0c053279772f2c7486a79714858a5cdf6a2b
DaxRiool <dax_riool@hotmail.com>
2025-12-11 15:28:23 +0100

    fixed test

--- src/main/app/src/app/zoeken/zoek/zoek.component.spec.ts
+++ src/main/app/src/app/zoeken/zoek/zoek.component.spec.ts
@@ -1,67 +1,60 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { ZoekComponent } from "./zoek.component";
-import { MatSidenav } from "@angular/material/sidenav";
-import {EventEmitter, NO_ERRORS_SCHEMA} from "@angular/core";
-import { GeneratedType } from "../../shared/utils/generated-types";
-import { ZoekVeld } from "../model/zoek-veld";
-import { TranslateModule } from "@ngx-translate/core";
 import { provideHttpClient } from "@angular/common/http";
 import { provideHttpClientTesting } from "@angular/common/http/testing";
+import { EventEmitter, NO_ERRORS_SCHEMA } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { MatPaginator, PageEvent } from "@angular/material/paginator";
+import { TranslateModule } from "@ngx-translate/core";
 import { Subject } from "rxjs";
-import {MatPaginator, PageEvent} from "@angular/material/paginator";
+import { ZoekComponent } from "./zoek.component";
 
 describe("ZoekComponent", () => {
-    let component: ZoekComponent;
-    let fixture: ComponentFixture<ZoekComponent>;
+  let component: ZoekComponent;
+  let fixture: ComponentFixture<ZoekComponent>;
 
-    const mockPaginator: Pick<MatPaginator, 'page' | 'pageIndex' | 'pageSize' | 'length'> = {
-        page: new EventEmitter<PageEvent>(),
-        pageIndex: 0,
-        pageSize: 10,
-        length: 0,
-    };
+  const mockPaginator: Pick<
+    MatPaginator,
+    "page" | "pageIndex" | "pageSize" | "length"
+  > = {
+    page: new EventEmitter<PageEvent>(),
+    pageIndex: 0,
+    pageSize: 10,
+    length: 0,
+  };
 
-    const mockSidenav: Pick<MatSidenav, 'open' | 'close' | 'openedStart'> = {
-        open: jest.fn(),
-        close: jest.fn(),
-        openedStart: new Subject<void>(),
-    };
+  const mockSidenav = {
+    open: jest.fn(),
+    close: jest.fn(),
+    openedStart: new Subject<void>(),
+  };
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TranslateModule.forRoot()],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+      declarations: [ZoekComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
 
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            imports: [TranslateModule.forRoot()],
-            declarations: [ZoekComponent],
-            providers: [
-                { provide: MatSidenav, useValue: mockSidenav },
-                provideHttpClient(),
-                provideHttpClientTesting(),
-            ],
-            schemas: [NO_ERRORS_SCHEMA],
-        }).compileComponents();
+    fixture = TestBed.createComponent(ZoekComponent);
+    component = fixture.componentInstance;
 
-        fixture = TestBed.createComponent(ZoekComponent);
-        component = fixture.componentInstance;
-
-        Object.defineProperty(component, "paginator", { get: () => () => mockPaginator });
-        Object.defineProperty(component, "zoekenSideNav", { get: () => () => mockSidenav });
-
-        fixture.detectChanges();
+    // Mock before detectChanges so template bindings work
+    Object.defineProperty(component, "paginator", {
+      get: () => () => mockPaginator,
+    });
+    Object.defineProperty(component, "zoekenSideNav", {
+      get: () => () => mockSidenav,
     });
 
-    it("should create", () => {
-        expect(component).toBeTruthy();
-    });
+    component.zoekResultaat.resultaten = [{ type: "DOCUMENT" }];
 
-    it("should pass sidenav instance to child components", () => {
-        component.zoekResultaat.resultaten = [
-            { type: "ZAAK" } as GeneratedType<"AbstractRestZoekObjectExtendsAbstractRestZoekObject">,
-            { type: "TAAK" } as GeneratedType<"AbstractRestZoekObjectExtendsAbstractRestZoekObject">,
-            { type: "DOCUMENT" } as GeneratedType<"AbstractRestZoekObjectExtendsAbstractRestZoekObject">,
-        ];
+    fixture.detectChanges();
+  });
 
-        fixture.detectChanges();
+  it("should render DOCUMENT search object with a sidenav", () => {
+    const documentElem: HTMLElement | null =
+      fixture.nativeElement.querySelector("zac-document-zoek-object");
 
-        expect(component.zoekenSideNav()).toBe(mockSidenav);
-    });
+    expect(documentElem).toBeTruthy();
+  });
 });
