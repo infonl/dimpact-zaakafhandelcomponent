46d487343e8fb3ec454e14b239680a7aad36d4c9
Edgar Vonk <edgar@info.nl>
2025-12-23 15:24:57 +0100

    chore: delete unused class

--- src/test/kotlin/net/atos/client/util/JWTTokenGeneratorTest.kt
+++ src/test/kotlin/net/atos/client/util/JWTTokenGeneratorTest.kt
@@ -15,51 +15,52 @@ import nl.info.zac.authentication.LoggedInUser
 import nl.info.zac.identity.model.getFullName
 
 class JWTTokenGeneratorTest : BehaviorSpec({
-
     beforeEach {
         checkUnnecessaryStub()
     }
 
-    Given("client id and secret and no logged-in user") {
-        val clientId = "clientId"
-        val clientSecret = "clientSecret"
+    Context("Generating a JWT token") {
+        Given("client id and secret and no logged-in user") {
+            val clientId = "clientId"
+            val clientSecret = "clientSecret"
 
-        When("token is generated") {
-            val token = JWTTokenGenerator.generate(clientId, clientSecret, null)
+            When("token is generated") {
+                val token = JWTTokenGenerator.generate(clientId, clientSecret, null)
 
-            Then("it should be a valid JWT token") {
-                val tokenParts = token.split(" ")
-                tokenParts[0] shouldBe "Bearer"
-                val decodedToken = JWT.decode(tokenParts[1])
-                with(decodedToken) {
-                    issuer shouldBe clientId
-                    getHeaderClaim("client_identifier").asString() shouldBe clientId
+                Then("it should be a valid JWT token") {
+                    val tokenParts = token.split(" ")
+                    tokenParts[0] shouldBe "Bearer"
+                    val decodedToken = JWT.decode(tokenParts[1])
+                    with(decodedToken) {
+                        issuer shouldBe clientId
+                        getHeaderClaim("client_identifier").asString() shouldBe clientId
+                    }
                 }
             }
         }
-    }
 
-    Given("client credentials and logged-in user") {
-        val clientId = "clientId"
-        val clientSecret = "clientSecret"
-        val loggedInUser = mockk<LoggedInUser>()
+        Given("client credentials and logged-in user") {
+            val clientId = "clientId"
+            val clientSecret = "clientSecret"
+            val loggedInUser = mockk<LoggedInUser>()
 
-        every { loggedInUser.id } returns "id"
-        mockkStatic(LoggedInUser::getFullName)
-        every { loggedInUser.getFullName() } returns "fullName"
+            every { loggedInUser.id } returns "id"
+            mockkStatic(LoggedInUser::getFullName)
+            every { loggedInUser.getFullName() } returns "fullName"
 
-        When("token is generated") {
-            val token = JWTTokenGenerator.generate(clientId, clientSecret, loggedInUser)
+            When("token is generated") {
+                val token = JWTTokenGenerator.generate(clientId, clientSecret, loggedInUser)
 
-            Then("it should be a valid JWT token") {
-                val tokenParts = token.split(" ")
-                tokenParts[0] shouldBe "Bearer"
-                val decodedToken = JWT.decode(tokenParts[1])
-                with(decodedToken) {
-                    issuer shouldBe clientId
-                    getHeaderClaim("client_identifier").asString() shouldBe clientId
-                    getClaim("user_id").asString() shouldBe "id"
-                    getClaim("user_representation").asString() shouldBe "fullName"
+                Then("it should be a valid JWT token") {
+                    val tokenParts = token.split(" ")
+                    tokenParts[0] shouldBe "Bearer"
+                    val decodedToken = JWT.decode(tokenParts[1])
+                    with(decodedToken) {
+                        issuer shouldBe clientId
+                        getHeaderClaim("client_identifier").asString() shouldBe clientId
+                        getClaim("user_id").asString() shouldBe "id"
+                        getClaim("user_representation").asString() shouldBe "fullName"
+                    }
                 }
             }
         }
