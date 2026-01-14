94573bc4c8178b362b2b7a52936d6e079a649a9d
infonl-marcel <marcele@info.nl>
2026-01-14 14:14:45 +0100

    fixed RuntimeError: NG0100: ExpressionChangedAfterItHasBeenCheckedError: Expression has changed after it was checke

--- src/main/app/src/app/core/loading/loading.component.ts
+++ src/main/app/src/app/core/loading/loading.component.ts
@@ -1,19 +1,31 @@
 /*
- * SPDX-FileCopyrightText: 2021 Atos, 2025 INFO.nl
- * SPDX-License-Identifier: EUPL-1.2+
- */
+  ~ SPDX-FileCopyrightText: 2021 Atos, 2025 INFO.nl
+  ~ SPDX-License-Identifier: EUPL-1.2+
+  */
 
-import { Component, inject } from "@angular/core";
-import { QueryClient } from "@tanstack/angular-query-experimental";
+import { Component, computed, inject } from "@angular/core";
+import {
+  QueryClient,
+  injectIsMutating,
+  injectIsFetching,
+} from "@tanstack/angular-query-experimental";
 import { UtilService } from "../service/util.service";
 
 @Component({
   selector: "zac-loading",
   templateUrl: "./loading.component.html",
-  styleUrls: ["./loading.component.less"],
   standalone: false,
 })
 export class LoadingComponent {
   protected readonly utilService = inject(UtilService);
   protected readonly queryClient = inject(QueryClient);
+
+  protected readonly mutatingCount = injectIsMutating();
+  protected readonly fetchingCount = injectIsFetching();
+
+  protected readonly isMutating = computed(() => this.mutatingCount() > 0);
+  protected readonly isFetching = computed(() => this.fetchingCount() > 0);
+  protected readonly isLoading = computed(
+    () => this.utilService.loading() || this.isFetching(),
+  );
 }
