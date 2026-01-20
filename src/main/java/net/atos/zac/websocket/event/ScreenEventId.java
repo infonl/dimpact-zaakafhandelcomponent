9e8579403f77cd8a7a92442796b07c178bdbfb42
Edgar Vonk <edgar@info.nl>
2026-01-20 13:59:28 +0100

    chore: fixed compiler warnings

--- src/main/java/net/atos/zac/websocket/event/ScreenEventId.java
+++ src/main/java/net/atos/zac/websocket/event/ScreenEventId.java
@@ -6,7 +6,34 @@ package net.atos.zac.websocket.event;
 
 import org.jspecify.annotations.NonNull;
 
+import java.util.Objects;
+
 public record ScreenEventId(String resource, String detail) {
+    /**
+     * Override the default equals to explicitly exclude the 'detail' field,
+     * because screen event IDs are considered equal if their 'resource' fields are equal.
+     *
+     * @param object the object to compare with
+     * @return true if equal, false otherwise
+     */
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (!(object instanceof ScreenEventId other)) return false;
+        return resource.equals(other.resource);
+    }
+
+    /**
+     * Override the default equals to explicitly exclude the 'detail' field,
+     * because screen event IDs are considered equal if their 'resource' fields are equal.
+     *
+     * @return hash code based on the 'resource' field
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(resource);
+    }
+
     /**
      * String representation of the ScreenEventId.
      * This string format is used in the ZAC WebSocket code and should not be changed.
