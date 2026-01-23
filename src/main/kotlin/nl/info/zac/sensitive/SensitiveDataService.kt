b2e649880a8f620fff171b20a6c7f12354ef435b
Hristo Iliev <hsiliev@gmail.com>
2026-01-23 10:00:31 +0200

    feature: replace map with cache

--- src/main/kotlin/nl/info/zac/sensitive/SensitiveDataService.kt
+++ src/main/kotlin/nl/info/zac/sensitive/SensitiveDataService.kt
@@ -4,22 +4,38 @@
  */
 package nl.info.zac.sensitive
 
+import com.github.benmanes.caffeine.cache.Cache
+import com.github.benmanes.caffeine.cache.Caffeine
 import jakarta.enterprise.context.ApplicationScoped
 import nl.info.zac.util.AllOpen
 import nl.info.zac.util.NoArgConstructor
 import java.util.UUID
-import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.TimeUnit
+import java.util.logging.Logger
 
 @ApplicationScoped
 @AllOpen
 @NoArgConstructor
 class SensitiveDataService {
-    val storage = ConcurrentHashMap<UUID, String>()
+    companion object {
+        const val STORAGE_SIZE = 10000L
+        const val EXPIRATION_TIME_HOURS = 12L
+
+        private val LOG = Logger.getLogger(SensitiveDataService::class.java.name)
+
+        val storage: Cache<UUID, String?> = Caffeine.newBuilder()
+            .maximumSize(STORAGE_SIZE)
+            .expireAfterAccess(EXPIRATION_TIME_HOURS, TimeUnit.HOURS)
+            .recordStats()
+            .removalListener { key: UUID?, _: String?, cause ->
+                LOG.fine("Removing sensitive storage data with key $key, because of: $cause")
+            }.build()
+    }
 
     fun put(data: String): UUID =
         UUID.randomUUID().also {
-            storage[it] = data
+            storage.put(it, data)
         }
 
-    fun get(key: UUID): String? = storage[key]
+    fun get(key: UUID): String? = storage.getIfPresent(key)
 }
