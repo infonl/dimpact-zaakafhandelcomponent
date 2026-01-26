3aefb02ad2bdd9e4cb74c588c8d43ec2b258975d
edgarvonk <edgar@info.nl>
2026-01-26 20:38:18 +0100

    chore: converted OfficeConverterClientService to Kotlin

--- src/main/java/net/atos/client/officeconverter/OfficeConverterClient.kt
+++ src/main/java/net/atos/client/officeconverter/OfficeConverterClient.kt
@@ -1,26 +1,23 @@
 /*
- * SPDX-FileCopyrightText: 2023 Atos
+ * SPDX-FileCopyrightText: 2023 Atos, 2026 INFO.nl
  * SPDX-License-Identifier: EUPL-1.2+
  */
+package net.atos.client.officeconverter
 
-package net.atos.client.officeconverter;
-
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
-
-import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
-import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataOutput;
+import jakarta.ws.rs.Consumes
+import jakarta.ws.rs.POST
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.Produces
+import jakarta.ws.rs.core.MediaType
+import jakarta.ws.rs.core.Response
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataOutput
 
 @Path("conversion")
 @RegisterRestClient(configKey = "Office-Converter-Client")
-public interface OfficeConverterClient {
-
+interface OfficeConverterClient {
     @POST
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Produces(MediaType.APPLICATION_OCTET_STREAM)
-    Response convert(final MultipartFormDataOutput file);
+    fun convert(file: MultipartFormDataOutput): Response
 }
--- src/main/java/net/atos/client/officeconverter/OfficeConverterClientService.kt
+++ src/main/java/net/atos/client/officeconverter/OfficeConverterClientService.kt
@@ -1,44 +1,28 @@
 /*
- * SPDX-FileCopyrightText: 2023 Atos
+ * SPDX-FileCopyrightText: 2023 Atos, 2026 INFO.nl
  * SPDX-License-Identifier: EUPL-1.2+
  */
+package net.atos.client.officeconverter
 
-package net.atos.client.officeconverter;
-
-import java.io.ByteArrayInputStream;
-
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
-
-import org.eclipse.microprofile.rest.client.inject.RestClient;
-import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataOutput;
+import jakarta.enterprise.context.ApplicationScoped
+import jakarta.inject.Inject
+import jakarta.ws.rs.core.MediaType.APPLICATION_OCTET_STREAM_TYPE
+import org.eclipse.microprofile.rest.client.inject.RestClient
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataOutput
+import java.io.ByteArrayInputStream
 
 @ApplicationScoped
-public class OfficeConverterClientService {
-    private OfficeConverterClient officeConverterClient;
-
-    /**
-     * No-arg constructor for CDI.
-     */
-    public OfficeConverterClientService() {
-    }
-
-    @Inject
-    public OfficeConverterClientService(
-            @RestClient final OfficeConverterClient officeConverterClient
-    ) {
-        this.officeConverterClient = officeConverterClient;
-    }
-
-    public ByteArrayInputStream convertToPDF(final ByteArrayInputStream document, final String filename) {
-        final MultipartFormDataOutput multipartFormDataOutput = new MultipartFormDataOutput();
-        multipartFormDataOutput.addFormData("file", document, MediaType.APPLICATION_OCTET_STREAM_TYPE, filename);
-        final Response response = officeConverterClient.convert(multipartFormDataOutput);
-        if (!response.bufferEntity()) {
-            throw new RuntimeException("Content of PDF converter could not be buffered.");
+class OfficeConverterClientService @Inject constructor(
+    @RestClient private val officeConverterClient: OfficeConverterClient
+){
+    fun convertToPDF(document: ByteArrayInputStream, filename: String): ByteArrayInputStream {
+        val multipartFormDataOutput = MultipartFormDataOutput().apply {
+            addFormData("file", document, APPLICATION_OCTET_STREAM_TYPE, filename)
         }
-        return (ByteArrayInputStream) response.getEntity();
+        val response = officeConverterClient.convert(multipartFormDataOutput)
+        if (!response.bufferEntity()) {
+            throw RuntimeException("Content of PDF converter could not be buffered.")
+        }
+        return response.entity as ByteArrayInputStream
     }
 }
