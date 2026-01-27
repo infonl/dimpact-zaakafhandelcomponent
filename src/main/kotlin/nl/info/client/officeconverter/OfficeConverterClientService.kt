068f9079ad4e926cc4a829c463fd3e4cdf75fcd1
Edgar Vonk <edgar@info.nl>
2026-01-27 09:57:35 +0100

    chore: convert officeconverterclientservice to Kotlin (#5178)
    Converted officeconverterclientservice to Kotlin. Added a unit test.
    
    Solves PZ-10136

---
+++ src/main/kotlin/nl/info/client/officeconverter/OfficeConverterClientService.kt
@@ -0,0 +1,33 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Atos, 2026 INFO.nl
+ * SPDX-License-Identifier: EUPL-1.2+
+ */
+package nl.info.client.officeconverter
+
+import jakarta.enterprise.context.ApplicationScoped
+import jakarta.inject.Inject
+import jakarta.ws.rs.core.MediaType.APPLICATION_OCTET_STREAM_TYPE
+import nl.info.client.officeconverter.exception.MessageEntityDataCouldNotBeBufferedException
+import nl.info.zac.util.AllOpen
+import nl.info.zac.util.NoArgConstructor
+import org.eclipse.microprofile.rest.client.inject.RestClient
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataOutput
+import java.io.ByteArrayInputStream
+
+@ApplicationScoped
+@NoArgConstructor
+@AllOpen
+class OfficeConverterClientService @Inject constructor(
+    @RestClient private val officeConverterClient: OfficeConverterClient
+) {
+    fun convertToPDF(document: ByteArrayInputStream, filename: String): ByteArrayInputStream {
+        val multipartFormDataOutput = MultipartFormDataOutput().apply {
+            addFormData("file", document, APPLICATION_OCTET_STREAM_TYPE, filename)
+        }
+        val response = officeConverterClient.convert(multipartFormDataOutput)
+        if (!response.bufferEntity()) {
+            throw MessageEntityDataCouldNotBeBufferedException("Content of PDF converter could not be buffered.")
+        }
+        return response.entity as ByteArrayInputStream
+    }
+}
