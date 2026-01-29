d7184d72e7c0aff5c99778414bd65130ff704cdc
Edgar Vonk <edgar@info.nl>
2026-01-29 11:08:07 +0100

    docs: added some code docs to integration test helper functions

--- src/itest/kotlin/nl/info/zac/itest/client/ItestHttpClient.kt
+++ src/itest/kotlin/nl/info/zac/itest/client/ItestHttpClient.kt
@@ -208,7 +208,7 @@ class ItestHttpClient {
      * @param requestBodyAsString The JSON body of the POST request as a string.
      * @param testUser Optional [TestUser] to authenticate to Keycloak before performing the request.
      * If provided, the user will be logged out afterwards.
-     * @return A [ResponseContent] containing the response body, headers, and status code
+     * @return A [ResponseContent] containing the response body, headers, and status code.
      */
     fun performJSONPostRequest(
         url: String,
