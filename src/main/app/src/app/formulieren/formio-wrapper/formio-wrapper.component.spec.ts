2fd081a74a1a290464baded70af1806ad49ec040
infonl-marcel <marcele@info.nl>
2026-02-02 13:45:11 +0100

    fix component test

--- src/main/app/src/app/formulieren/formio-wrapper/formio-wrapper.component.spec.ts
+++ src/main/app/src/app/formulieren/formio-wrapper/formio-wrapper.component.spec.ts
@@ -2,18 +2,35 @@
  * SPDX-FileCopyrightText: 2025 INFO.nl
  * SPDX-License-Identifier: EUPL-1.2+
  */
+import { ElementRef } from "@angular/core";
 import { TestBed } from "@angular/core/testing";
+import { FormioBootstrapLoaderService } from "./formio-bootstrap-loader.service";
 import { FormioWrapperComponent } from "./formio-wrapper.component";
 
 describe(FormioWrapperComponent.name, () => {
   let component: FormioWrapperComponent;
+  let bootstrapLoader: FormioBootstrapLoaderService;
 
   beforeEach(() => {
+    const mockElementRef: Pick<ElementRef, "nativeElement"> = {
+      nativeElement: {
+        shadowRoot: null,
+      },
+    };
+
     TestBed.configureTestingModule({
-      providers: [FormioWrapperComponent],
+      providers: [
+        FormioWrapperComponent,
+        { provide: ElementRef, useValue: mockElementRef },
+      ],
     }).compileComponents();
 
     component = TestBed.inject(FormioWrapperComponent);
+    bootstrapLoader = TestBed.inject(FormioBootstrapLoaderService);
+
+    jest
+      .spyOn(bootstrapLoader, "getBootstrapStyleSheet")
+      .mockResolvedValue(new CSSStyleSheet());
   });
 
   describe(FormioWrapperComponent.prototype.onChange.name, () => {
