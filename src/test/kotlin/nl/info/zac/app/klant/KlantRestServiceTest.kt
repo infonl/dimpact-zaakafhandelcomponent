b65860435a6028656e8b69c73b67f7296312ac5b
Edgar Vonk <edgar@info.nl>
2026-02-09 17:16:13 +0100

    chore: fixed unit test

--- src/test/kotlin/nl/info/zac/app/klant/KlantRestServiceTest.kt
+++ src/test/kotlin/nl/info/zac/app/klant/KlantRestServiceTest.kt
@@ -325,7 +325,7 @@ class KlantRestServiceTest : BehaviorSpec({
                     "a NullPointerException should be thrown since the logged-in user is required to retrieve a person"
                 ) {
                     exception.message shouldBe "Cannot invoke \"nl.info.zac.authentication.LoggedInUser.getId()\" because " +
-                            "the return value of \"jakarta.enterprise.inject.Instance.get()\" is null"
+                        "the return value of \"jakarta.enterprise.inject.Instance.get()\" is null"
                 }
             }
         }
