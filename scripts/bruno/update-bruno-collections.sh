798393471f177343038453cfbf4740d1c9a5bca6
Edgar Vonk <edgar@info.nl>
2026-02-19 10:44:15 +0100

    chore: add Bruno collection for ZAC backend API

---
+++ scripts/bruno/update-bruno-collections.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+#
+# SPDX-FileCopyrightText: 2026 INFO.nl
+# SPDX-License-Identifier: EUPL-1.2+
+#
+
+help()
+{
+   echo "Creates various Bruno collections from their corresponding OpenAPI specifications."
+   echo "These OpenAPI specifications need to be available for this script to work."
+   echo "A normal ZAC Gradle build will generate the ZAC OpenAPI specification."
+   echo "OpenAPI specs for other collections are available in the ZAC codebase directly."
+   echo "This script required the Bruno CLI to be installed and available in the system PATH."
+}
+
+# Generate ZAC API Bruno collection
+bru import openapi \
+  --source ../../build/generated/openapi/META-INF/openapi/openapi.yaml \
+  --output-file collections/zac-backend-api-bruno-collection. \
+  --collection-name "ZAC Backend API"
