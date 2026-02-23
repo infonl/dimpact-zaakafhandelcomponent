283c29544fa5013c270567d2eb2c671151ffd7f8
infonl-marcel <marcele@info.nl>
2026-02-23 15:11:47 +0100

    styling

--- src/main/app/src/app/fout-afhandeling/fout-afhandeling.service.ts
+++ src/main/app/src/app/fout-afhandeling/fout-afhandeling.service.ts
@@ -136,9 +136,7 @@ export class FoutAfhandelingService {
     this.foutmelding = err.message;
     if (err.error instanceof ErrorEvent) {
       // client-side error
-      this.foutmelding = this.translateService.instant(
-        "dialoog.error.body.fout",
-      );
+      this.foutmelding = "dialoog.error.body.fout";
       this.bericht = err.error.message;
       void this.router.navigate(["/fout"]);
       return of();
@@ -150,9 +148,7 @@ export class FoutAfhandelingService {
         window.location.reload();
         return throwError(() => "User logged out");
       }
-      this.foutmelding = this.translateService.instant(
-        "dialoog.error.body.loggedout",
-      );
+      this.foutmelding = "dialoog.error.body.loggedout";
       this.bericht = "";
       void this.router.navigate(["/fout"]);
       return of();
