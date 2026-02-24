40274f933a85be9f52a216ff63a5c549b9dc98da
infonl-marcel <marcele@info.nl>
2026-02-24 12:27:36 +0100

    spotlessed

--- src/main/app/src/app/informatie-objecten/informatie-objecten.service.ts
+++ src/main/app/src/app/informatie-objecten/informatie-objecten.service.ts
@@ -227,7 +227,9 @@ export class InformatieObjectenService {
     return `${this.basepath}/informatieobject/${uuid}/download`;
   }
 
-  getZIPDownload(body: PostBody<"/rest/informatieobjecten/download/zip">): Observable<Blob> {
+  getZIPDownload(
+    body: PostBody<"/rest/informatieobjecten/download/zip">,
+  ): Observable<Blob> {
     return this.zacHttpClient.POST(
       "/rest/informatieobjecten/download/zip",
       body,
