971b354f3a8f37a898f48f3f30d6869d0c851e0d
Michiel Schutler <michiel@info.nl>
2026-02-25 15:45:02 +0100

    fix: PZ-10483 Fix BPMN end-to-end tests

--- src/e2e/step-definitions/bpmn.ts
+++ src/e2e/step-definitions/bpmn.ts
@@ -176,7 +176,7 @@ When(
   { timeout: TWO_MINUTES_IN_MS },
   async function (this: CustomWorld, user: z.infer<typeof worldUsers>) {
     await this.page
-      .getByRole("button", { name: "submitButtonAriaLabel" })
+      .getByText("Submit Form" )
       .click();
   },
 );
