5fd0653bf9ebfcf01fea435f11b0b3e1165cad62
Michiel Schutler <michiel@info.nl>
2026-03-05 10:05:32 +0100

    feat: Datamodel and migration

---
+++ src/main/kotlin/nl/info/zac/formio/model/BpmnProcessdefinitieTaakformulier.kt
@@ -0,0 +1,54 @@
+/*
+ * SPDX-FileCopyrightText: 2026 INFO.nl
+ * SPDX-License-Identifier: EUPL-1.2+
+ */
+package nl.info.zac.formio.model
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.SequenceGenerator
+import jakarta.persistence.Table
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+import nl.info.zac.database.flyway.FlywayIntegrator
+import nl.info.zac.util.AllOpen
+
+@Entity
+@Table(schema = FlywayIntegrator.SCHEMA, name = "bpmn_procesdefinitie_taakformulieren")
+@SequenceGenerator(
+    schema = FlywayIntegrator.SCHEMA,
+    name = "sq_bpmn_procesdefinitie_taakformulieren",
+    sequenceName = "sq_bpmn_procesdefinitie_taakformulieren",
+    allocationSize = 1
+)
+@AllOpen
+class BpmnProcessdefinitieTaakformulier {
+
+    @Id
+    @GeneratedValue(generator = "sq_bpmn_procesdefinitie_taakformulieren", strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
+    var id: Long = 0
+
+    @NotBlank
+    @Column(name = "bpmn_procesdefinitie")
+    lateinit var bpmnProcesdefinitie: String
+
+    @NotBlank
+    @Column(name = "bpmn_procesdefinitie_versie")
+    var bpmnProcesdefinitieVersie: Long = 0
+
+    @NotBlank
+    lateinit var name: String
+
+    @NotNull
+    lateinit var title: String
+
+    @NotBlank
+    lateinit var filename: String
+
+    @NotBlank
+    lateinit var content: String
+}
