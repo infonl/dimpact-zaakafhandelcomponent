bddc9107bb4d8ba134048ec6f13a52a951022a24
infonl-marcel <marcele@info.nl>
2026-03-09 16:27:36 +0100

    standalone

--- src/main/app/src/app/klanten/koppel/klanten/klant-koppel-initiator.component.ts
+++ src/main/app/src/app/klanten/koppel/klanten/klant-koppel-initiator.component.ts
@@ -13,6 +13,7 @@ import { KlantGegevens } from "../../model/klanten/klant-gegevens";
 
 @Component({
   selector: "zac-klant-koppel-initiator-persoon",
+  standalone: true,
   imports: [
     SharedModule,
     TranslateModule,
