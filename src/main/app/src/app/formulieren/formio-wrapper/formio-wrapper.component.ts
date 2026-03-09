75e07dac1091e129dcf8663a159c1d68004f9330
infonl-marcel <marcele@info.nl>
2026-03-09 14:33:28 +0100

    fixing FormIO i18n implementation

--- src/main/app/src/app/formulieren/formio-wrapper/formio-wrapper.component.ts
+++ src/main/app/src/app/formulieren/formio-wrapper/formio-wrapper.component.ts
@@ -19,10 +19,12 @@ import {
 } from "@angular/core";
 import {
   ExtendedComponentSchema,
+  FormioBaseComponent,
   FormioComponent,
   FormioHookOptions,
 } from "@formio/angular";
 import { FormioBootstrapLoaderService } from "./formio-bootstrap-loader.service";
+import { FORMIO_NL_TRANSLATIONS } from "./formio-wrapper.i18n-translations.nl";
 
 @Component({
   selector: "zac-formio-wrapper",
@@ -119,6 +121,10 @@ export class FormioWrapperComponent implements OnInit, AfterViewInit {
   onChange(event: object) {
     // Filter out form.io change events that do not contain data
     if ("data" in event && event.data) this.formChange.emit(event);
+  }
+
+  onFormioReady(formioBaseComponent: FormioBaseComponent) {
+    formioBaseComponent.formio?.addLanguage("nl", FORMIO_NL_TRANSLATIONS, true);
   }
 
   onCustomEvent(event: FormioCustomEvent) {
