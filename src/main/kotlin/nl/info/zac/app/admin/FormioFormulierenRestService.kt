ad77e8833e5377dafceb003cd8b0f6e10cc3f826
Michiel Schutler <michiel@info.nl>
2026-03-11 10:01:27 +0100

    fix: Cleanup

--- src/main/kotlin/nl/info/zac/app/admin/FormioFormulierenRestService.kt
+++ src/main/kotlin/nl/info/zac/app/admin/FormioFormulierenRestService.kt
@@ -7,21 +7,15 @@ package nl.info.zac.app.admin
 import jakarta.inject.Inject
 import jakarta.inject.Singleton
 import jakarta.ws.rs.Consumes
-import jakarta.ws.rs.DELETE
 import jakarta.ws.rs.GET
-import jakarta.ws.rs.POST
 import jakarta.ws.rs.Path
-import jakarta.ws.rs.PathParam
 import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
-import jakarta.ws.rs.core.Response
 import nl.info.zac.app.admin.model.RestFormioFormulier
-import nl.info.zac.app.admin.model.RestFormioFormulierContent
 import nl.info.zac.flowable.bpmn.BpmnProcessDefinitionTaskFormService
 import nl.info.zac.policy.PolicyService
 import nl.info.zac.policy.assertPolicy
 import nl.info.zac.util.NoArgConstructor
-import org.apache.commons.lang3.NotImplementedException
 
 @Singleton
 @Path("formio-formulieren")
@@ -37,20 +31,5 @@ class FormioFormulierenRestService @Inject constructor(
         assertPolicy(policyService.readOverigeRechten().beheren)
         return bpmnProcessDefinitionTaskFormService.listForms()
             .map { RestFormioFormulier(it.id, it.bpmnProcessDefinitionKey, it.name, it.title) }
-    }
-
-    @POST
-    @Suppress("UnusedParameter")
-    fun createFormulier(restFormioFormulierContent: RestFormioFormulierContent): Response {
-        assertPolicy(policyService.readOverigeRechten().beheren)
-        throw NotImplementedException("Creating formio form moved to new endpoint")
-    }
-
-    @DELETE
-    @Path("{id}")
-    @Suppress("UnusedParameter")
-    fun deleteFormulier(@PathParam("id") id: Long): Response {
-        assertPolicy(policyService.readOverigeRechten().beheren)
-        throw NotImplementedException("Deleting formio form moved to new endpoint")
     }
 }
