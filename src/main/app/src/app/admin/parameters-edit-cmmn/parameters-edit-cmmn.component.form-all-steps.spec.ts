b3d4fe0a8412a68461febce8c624acfcf7d5b35b
infonl-marcel <marcele@info.nl>
2026-03-18 16:42:24 +0100

    lint error

--- src/main/app/src/app/admin/parameters-edit-cmmn/parameters-edit-cmmn.component.form-all-steps.spec.ts
+++ src/main/app/src/app/admin/parameters-edit-cmmn/parameters-edit-cmmn.component.form-all-steps.spec.ts
@@ -24,7 +24,6 @@ import { ReferentieTabelService } from "../referentie-tabel.service";
 import { ZaakafhandelParametersService } from "../zaakafhandel-parameters.service";
 import { ParametersEditCmmnComponent } from "./parameters-edit-cmmn.component";
 import { StaticTextComponent } from "src/app/shared/static-text/static-text.component";
-import { SideNavComponent } from "src/app/shared/side-nav/side-nav.component";
 
 describe(ParametersEditCmmnComponent.name, () => {
   let fixture: ComponentFixture<ParametersEditCmmnComponent>;
