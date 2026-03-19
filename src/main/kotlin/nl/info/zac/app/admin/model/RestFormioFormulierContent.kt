6565a5910322410ca0b8fa734f71b4045490914d
Michiel Schutler <107392628+michielschutler@users.noreply.github.com>
2026-03-19 15:25:07 +0100

    chore: Added data class

--- src/main/kotlin/nl/info/zac/app/admin/model/RestFormioFormulierContent.kt
+++ src/main/kotlin/nl/info/zac/app/admin/model/RestFormioFormulierContent.kt
@@ -7,7 +7,7 @@ package nl.info.zac.app.admin.model
 import nl.info.zac.util.NoArgConstructor
 
 @NoArgConstructor
-class RestFormioFormulierContent(
+data class RestFormioFormulierContent(
     var filename: String,
     var content: String
 )
