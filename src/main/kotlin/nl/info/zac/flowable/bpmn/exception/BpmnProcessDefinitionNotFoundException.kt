6d9cb36e00f0173731a3f6c2dfe58675535741ea
Edgar Vonk <edgar@info.nl>
2026-03-30 15:10:48 +0200

    feat: enhance BpmnProcessDefinitionNotFoundException with error code and update messages

--- src/main/kotlin/nl/info/zac/flowable/bpmn/exception/BpmnProcessDefinitionNotFoundException.kt
+++ src/main/kotlin/nl/info/zac/flowable/bpmn/exception/BpmnProcessDefinitionNotFoundException.kt
@@ -4,10 +4,10 @@
  */
 package nl.info.zac.flowable.bpmn.exception
 
-import nl.info.zac.exception.ErrorCode.ERROR_CODE_BPMN_TASK_FORM_NOT_FOUND
+import nl.info.zac.exception.ErrorCode.ERROR_CODE_BPMN_PROCESS_DEFINITION_NOT_FOUND
 import nl.info.zac.exception.ZacSetupException
 
 class BpmnProcessDefinitionNotFoundException(override val message: String) : ZacSetupException(
     message,
-    ERROR_CODE_BPMN_TASK_FORM_NOT_FOUND
+    ERROR_CODE_BPMN_PROCESS_DEFINITION_NOT_FOUND,
 )
