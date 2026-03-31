b0d5ddea6aed6942cc322c318323f1ae8894bfbf
Edgar Vonk <edgar@info.nl>
2026-03-31 11:41:32 +0200

    chore: add structured JSON WildFly logging; work in progress

--- src/main/kotlin/nl/info/zac/log/LogUtils.kt
+++ src/main/kotlin/nl/info/zac/log/LogUtils.kt
@@ -9,6 +9,6 @@ import java.util.logging.Level
 import java.util.logging.Logger
 
 /**
- * Simple wapper function around [Logger.log] to make it easier to unit test classes that use logging.
+ * Simple wapper function around [Logger.log] to make it easier to log messages, for example in unit tests.
  */
 fun log(logger: Logger, level: Level, message: String, throwable: Throwable) = logger.log(level, message, throwable)
