ba0bfa42c07aa4b1d61822784476de44379cfc95
Edgar Vonk <edgar@info.nl>
2026-04-01 12:38:48 +0000

    feat: add structured JSON WildFly logging (#5638)
    Use structured JSON WildFly logging. Spin off from
    https://github.com/infonl/dimpact-zaakafhandelcomponent/pull/5593 to add
    JSON logging only, without yet any (MDC) context.
    
    Example of a normal ZAC log line to the console, now structured as JSON,
    with this change:
    
    ```
    {"timestamp":"2026-03-31T11:17:43.559+02:00","sequence":269,"loggerClassName":"java.util.logging.Logger","loggerName":"nl.info.zac.authentication.UserPrincipalFilter","level":"INFO","message":"User logged in: 'beheerder1newiam' with groups: [beheerders-elk-domein], functional roles: '[beheerder_elk_domein, offline_access, default-roles-zac, uma_authorization]' and application roles per zaaktype: {Test zaaktype 2=[recordmanager, coordinator, behandelaar, raadpleger, beheerder], Test zaaktype 1=[recordmanager, coordinator, behandelaar, raadpleger, beheerder], BPMN test zaaktype 2=[recordmanager, coordinator, behandelaar, raadpleger, beheerder], BPMN test zaaktype 1=[recordmanager, coordinator, behandelaar, raadpleger, beheerder], Test zaaktype 3=[recordmanager, coordinator, behandelaar, raadpleger, beheerder]}","threadName":"default task-2","threadId":236,"mdc":{},"ndc":"","hostName":"le2293","processName":"/Library/Java/JavaVirtualMachines/temurin-25.jdk/Contents/Home/bin/java","processId":15584,"service":"zac"}
    ```
    
    Solves PZ-10775

--- src/main/kotlin/nl/info/zac/log/LogUtils.kt
+++ src/main/kotlin/nl/info/zac/log/LogUtils.kt
@@ -9,6 +9,6 @@ import java.util.logging.Level
 import java.util.logging.Logger
 
 /**
- * Simple wapper function around [Logger.log] to make it easier to unit test classes that use logging.
+ * Simple wrapper function around [Logger.log] to make it easier to handle exception logging, for example in unit tests.
  */
 fun log(logger: Logger, level: Level, message: String, throwable: Throwable) = logger.log(level, message, throwable)
