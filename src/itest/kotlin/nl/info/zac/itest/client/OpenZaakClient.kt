6af22dcd55f89d386d6421c11e2ce1f66310e3e7
Edgar Vonk <edgar@info.nl>
2026-04-07 15:17:13 +0200

    feat: add createEnkelvoudigInformatieobject to OpenZaakClient

--- src/itest/kotlin/nl/info/zac/itest/client/OpenZaakClient.kt
+++ src/itest/kotlin/nl/info/zac/itest/client/OpenZaakClient.kt
@@ -6,9 +6,21 @@ package nl.info.zac.itest.client
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm.HMAC256
+import nl.info.zac.itest.config.ItestConfiguration.BRON_ORGANISATIE
+import nl.info.zac.itest.config.ItestConfiguration.DOCUMENT_FILE_TITLE
+import nl.info.zac.itest.config.ItestConfiguration.DOCUMENT_STATUS_IN_BEWERKING
+import nl.info.zac.itest.config.ItestConfiguration.DOCUMENT_VERTROUWELIJKHEIDS_AANDUIDING_VERTROUWELIJK
+import nl.info.zac.itest.config.ItestConfiguration.FAKE_AUTHOR_NAME
+import nl.info.zac.itest.config.ItestConfiguration.INFORMATIE_OBJECT_TYPE_BIJLAGE_UUID
+import nl.info.zac.itest.config.ItestConfiguration.OPEN_ZAAK_BASE_URI
 import nl.info.zac.itest.config.ItestConfiguration.OPEN_ZAAK_CLIENT_ID
 import nl.info.zac.itest.config.ItestConfiguration.OPEN_ZAAK_CLIENT_SECRET
 import nl.info.zac.itest.config.ItestConfiguration.OPEN_ZAAK_EXTERNAL_URI
+import org.json.JSONObject
+import java.io.File
+import java.net.URLDecoder
+import java.time.LocalDate
+import java.util.Base64
 import java.util.Date
 import java.util.UUID
 
@@ -19,6 +31,52 @@ class OpenZaakClient(
         itestHttpClient.performZgwApiGetRequest(
             url = "$OPEN_ZAAK_EXTERNAL_URI/zaken/api/v1/rollen?zaak=$OPEN_ZAAK_EXTERNAL_URI/zaken/api/v1/zaken/$zaakUUID"
         )
+
+    /**
+     * Creates an enkelvoudig informatieobject directly in Open Zaak's DRC API,
+     * bypassing ZAC. Use this to simulate externally created documents in integration tests.
+     *
+     * The file is loaded from test resources and base64-encoded for the [inhoud] field.
+     *
+     * @param fileName Name of the file in test resources (e.g. "fäkeTestDocument.pdf")
+     * @param title Document title; defaults to [DOCUMENT_FILE_TITLE]
+     * @param informatieobjectTypeUUID UUID of the informatieobjecttype in Open Zaak;
+     *   defaults to the "bijlage" type ([INFORMATIE_OBJECT_TYPE_BIJLAGE_UUID])
+     * @param vertrouwelijkheidaanduiding Confidentiality level;
+     *   defaults to [DOCUMENT_VERTROUWELIJKHEIDS_AANDUIDING_VERTROUWELIJK]
+     * @return [ResponseContent] with the Open Zaak API response (HTTP 201 on success)
+     */
+    fun createEnkelvoudigInformatieobject(
+        fileName: String,
+        title: String = DOCUMENT_FILE_TITLE,
+        informatieobjectTypeUUID: UUID = UUID.fromString(INFORMATIE_OBJECT_TYPE_BIJLAGE_UUID),
+        vertrouwelijkheidaanduiding: String = DOCUMENT_VERTROUWELIJKHEIDS_AANDUIDING_VERTROUWELIJK
+    ): ResponseContent {
+        val file = Thread.currentThread().contextClassLoader.getResource(fileName).let {
+            File(URLDecoder.decode(it!!.path, Charsets.UTF_8))
+        }
+        val encodedContent = Base64.getEncoder().encodeToString(file.readBytes())
+        val requestBody = JSONObject(
+            mapOf(
+                "bronorganisatie" to BRON_ORGANISATIE,
+                "creatiedatum" to LocalDate.now().toString(),
+                "titel" to title,
+                "auteur" to FAKE_AUTHOR_NAME,
+                "taal" to "dut",
+                "informatieobjecttype" to
+                    "$OPEN_ZAAK_BASE_URI/catalogi/api/v1/informatieobjecttypen/$informatieobjectTypeUUID",
+                "inhoud" to encodedContent,
+                "bestandsnaam" to fileName,
+                "bestandsomvang" to file.length(),
+                "vertrouwelijkheidaanduiding" to vertrouwelijkheidaanduiding,
+                "status" to DOCUMENT_STATUS_IN_BEWERKING
+            )
+        ).toString()
+        return itestHttpClient.performZgwApiPostRequest(
+            url = "$OPEN_ZAAK_EXTERNAL_URI/documenten/api/v1/enkelvoudiginformatieobjecten",
+            requestBodyAsString = requestBody
+        )
+    }
 }
 
 /**
