148fd98726e5bfc44a39ab3b8917cb6c1de101b7
DaxRiool <dax_riool@hotmail.com>
2026-05-11 16:39:41 +0200

    fixed copilot comments

--- src/main/app/src/app/zaken/zaken-werkvoorraad/zaken-werkvoorraad.component.spec.ts
+++ src/main/app/src/app/zaken/zaken-werkvoorraad/zaken-werkvoorraad.component.spec.ts
@@ -11,7 +11,7 @@ import { provideHttpClientTesting } from "@angular/common/http/testing";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { provideNativeDateAdapter } from "@angular/material/core";
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
-import { ActivatedRoute, Data, RouterModule } from "@angular/router";
+import { ActivatedRoute, Data, provideRouter } from "@angular/router";
 import { TranslateModule } from "@ngx-translate/core";
 import { provideQueryClient } from "@tanstack/angular-query-experimental";
 import { of } from "rxjs";
@@ -51,10 +51,10 @@ describe(ZakenWerkvoorraadComponent.name, () => {
       imports: [
         ZakenWerkvoorraadComponent,
         NoopAnimationsModule,
-        RouterModule,
         TranslateModule.forRoot(),
       ],
       providers: [
+        provideRouter([]),
         {
           provide: ActivatedRoute,
           useValue: mockActivatedRoute,
