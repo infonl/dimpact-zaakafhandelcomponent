4fd6124799570555540ff0a569c1108b831d0c6a
edgarvonk <edgar@info.nl>
2026-05-25 21:37:55 +0200

    chore: replace objecttypen-wiremock with objecttypes-api; update configurations and dependencies

---
+++ scripts/docker-compose/imports/objecttypes-api/init/init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+# Wait here a bit for the Objecttypes API database to be initialized (by Objecttypes).
+# In the end better to add a health check to the Objecttypes API service in Docker Compose but
+# that is not trivial without curl and wget.
+sleep 10
+
+#
+# SPDX-FileCopyrightText: 2024 INFO.nl
+# SPDX-License-Identifier: EUPL-1.2+
+#
+
+python /app/src/manage.py loaddata demodata
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python /app/src/manage.py shell
