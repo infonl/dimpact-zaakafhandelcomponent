8100e5964898a4013cd73f16780164449fa9ae7f
Dax Riool <76945092+DaxRiool@users.noreply.github.com>
2026-06-01 14:01:03 +0200

    feature: add aanvullende informatie to fase check (#6108)
    This PR fixes the bug that the Besluit Vastleggen button was visible
    while the zaak had the status 'Intake'. This was caused because when a
    task was created the zaak was still in the intake fase but with the
    status 'Wacht op aanvullende informatie'. This status type has now been
    included in the check for the intake fase
    
    Solves PZ-11311

--- src/main/kotlin/nl/info/client/zgw/zrc/util/StatusTypeUtil.kt
+++ src/main/kotlin/nl/info/client/zgw/zrc/util/StatusTypeUtil.kt
@@ -5,6 +5,7 @@
 package nl.info.client.zgw.zrc.util
 
 import nl.info.client.zgw.ztc.model.generated.StatusType
+import nl.info.zac.configuration.ConfigurationService.Companion.STATUSTYPE_OMSCHRIJVING_AANVULLENDE_INFORMATIE
 import nl.info.zac.configuration.ConfigurationService.Companion.STATUSTYPE_OMSCHRIJVING_HEROPEND
 import nl.info.zac.configuration.ConfigurationService.Companion.STATUSTYPE_OMSCHRIJVING_INTAKE
 
@@ -19,3 +20,10 @@ fun StatusType?.isHeropend() = this != null && STATUSTYPE_OMSCHRIJVING_HEROPEND 
  * Note that in the ZGW ZRC API a zaak status and therefore also the corresponding [StatusType] may be `null`.
  */
 fun StatusType?.isIntake() = this != null && STATUSTYPE_OMSCHRIJVING_INTAKE == this.getOmschrijving()
+
+/**
+ * Returns `true` if the [StatusType] is not `null` and equals '[STATUSTYPE_OMSCHRIJVING_AANVULLENDE_INFORMATIE]'.
+ * This status is set when an aanvullende informatie task is created during the intake phase.
+ */
+fun StatusType?.isWachtOpAanvullendeInformatie() =
+    this != null && STATUSTYPE_OMSCHRIJVING_AANVULLENDE_INFORMATIE == this.getOmschrijving()
