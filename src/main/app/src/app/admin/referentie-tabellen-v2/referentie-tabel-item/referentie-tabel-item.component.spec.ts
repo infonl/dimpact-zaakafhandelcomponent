da6078b18ece0a8d9fc9839e133e1dcccdb238ba
infonl-marcel <marcele@info.nl>
2026-07-06 11:39:09 +0200

    spotlessed

--- src/main/app/src/app/admin/referentie-tabellen-v2/referentie-tabel-item/referentie-tabel-item.component.spec.ts
+++ src/main/app/src/app/admin/referentie-tabellen-v2/referentie-tabel-item/referentie-tabel-item.component.spec.ts
@@ -22,8 +22,8 @@ import { fromPartial } from "src/test-helpers";
 import { testQueryClient } from "../../../../../setupJest";
 import { UtilService } from "../../../core/service/util.service";
 import { GeneratedType } from "../../../shared/utils/generated-types";
-import { ReferentieTabelValueDialogComponent } from "./referentie-tabel-value-dialog/referentie-tabel-value-dialog.component";
 import { ReferentieTabelItemComponent } from "./referentie-tabel-item.component";
+import { ReferentieTabelValueDialogComponent } from "./referentie-tabel-value-dialog/referentie-tabel-value-dialog.component";
 
 const tabel = fromPartial<GeneratedType<"RestReferenceTable">>({
   id: 1,
