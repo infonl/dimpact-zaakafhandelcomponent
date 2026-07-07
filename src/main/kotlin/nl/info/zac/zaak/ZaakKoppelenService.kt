e3e3389e0c82e8319016f383836f23a2955ad016
felixcicatt <felix@icatt.nl>
2026-07-07 09:07:23 +0200

    cleanup

--- src/main/kotlin/nl/info/zac/zaak/ZaakKoppelenService.kt
+++ src/main/kotlin/nl/info/zac/zaak/ZaakKoppelenService.kt
@@ -8,14 +8,11 @@ package nl.info.zac.zaak
 import nl.info.zac.zaak.model.ZaakKoppelenData
 import java.util.UUID
 
+private fun allowGerelateerd(from: ZaakKoppelenData, to: ZaakKoppelenData) =
+    from.koppelen && to.lezen
 
-private fun allowFromZaak(from: ZaakKoppelenData): Boolean = from.koppelen
-
-private fun allowToZaak(to: ZaakKoppelenData): Boolean = to.lezen
-
-private fun allowHoofdzaak(zaak: ZaakKoppelenData) = zaak.koppelen
-
-private fun allowDeelzaak(zaak: ZaakKoppelenData) = zaak.koppelen
+private fun allowHoofdAndDeelzaak(hoofdzaak: ZaakKoppelenData, deelzaak: ZaakKoppelenData) =
+    hoofdzaak.koppelen && deelzaak.koppelen && hoofdzaak.isOpen == deelzaak.isOpen
 
 // a hoofdzaak CANNOT be a deelzaak, no tree-like hierarchy is allowed
 private fun canBeHoofdzaak(zaak: ZaakKoppelenData): Boolean = !zaak.isDeelzaak
@@ -23,21 +20,21 @@ private fun canBeHoofdzaak(zaak: ZaakKoppelenData): Boolean = !zaak.isDeelzaak
 // a deelzaak CANNOT be a deelzaak to multiple hoofdzaken, and CANNOT be a hoofdzaak itself
 private fun canBeNewDeelzaak(zaak: ZaakKoppelenData): Boolean = !zaak.isDeelzaak && !zaak.isHoofdzaak
 
-fun canBeRelated(from: ZaakKoppelenData, to: ZaakKoppelenData): Boolean = allowFromZaak(from) && allowToZaak(to)
+fun canBeRelated(from: ZaakKoppelenData, to: ZaakKoppelenData): Boolean =
+    allowGerelateerd(from, to)
 
 fun canBeHoofdAndDeelzaak(
     hoofdzaak: ZaakKoppelenData,
     deelzaak: ZaakKoppelenData,
     allowedDeelzaaktypes: Set<UUID>
 ): Boolean =
-    allowHoofdzaak(hoofdzaak) &&
-    allowDeelzaak(deelzaak) &&
+    allowHoofdAndDeelzaak(hoofdzaak, deelzaak) &&
     canBeHoofdzaak(hoofdzaak) &&
     canBeNewDeelzaak(deelzaak) &&
-    hoofdzaak.isOpen == deelzaak.isOpen &&
     allowedDeelzaaktypes.contains(deelzaak.zaaktypeUUID)
 
 fun hoofdAndDeelzaakCanBeOntkoppeld(hoofdzaak: ZaakKoppelenData, deelzaak: ZaakKoppelenData) =
-    allowHoofdzaak(hoofdzaak) && allowDeelzaak(deelzaak)
+    allowHoofdAndDeelzaak(hoofdzaak, deelzaak)
 
-fun relatedZakenCanBeOntkoppeld(from: ZaakKoppelenData, to: ZaakKoppelenData) = allowFromZaak(from) && allowToZaak(to)
+fun relatedZakenCanBeOntkoppeld(from: ZaakKoppelenData, to: ZaakKoppelenData) =
+    allowGerelateerd(from, to)
