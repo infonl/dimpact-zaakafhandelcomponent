c720fb2025aff8410897304ffd74a152ccb119b6
infonl-marcel <marcele@info.nl>
2026-08-03 09:29:19 +0200

    spotless

--- src/main/app/src/app/taken/taak-view/taak-view.component.ts
+++ src/main/app/src/app/taken/taak-view/taak-view.component.ts
@@ -411,7 +411,8 @@ export class TaakViewComponent
 
   onHardCodedFormSubmit(formGroup: FormGroup, partial = false) {
     const taskBody:
-      PutBody<"/rest/taken/taakdata"> | PatchBody<"/rest/taken/complete"> = {
+      | PutBody<"/rest/taken/taakdata">
+      | PatchBody<"/rest/taken/complete"> = {
       ...this.taak!,
       taakdata: {
         ...this.taak!.taakdata,
