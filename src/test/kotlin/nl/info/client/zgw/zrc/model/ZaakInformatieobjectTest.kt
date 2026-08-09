626367cab3723d7c10eca4c0db64306bfc0bc22e
edgarvonk <edgar@info.nl>
2026-08-09 22:35:44 +0200

    test: verify titel/beschrijving survive JSON-B round-trip on ZaakInformatieobject
    The @JsonbCreator constructor doesn't include titel/beschrijving as
    creator params. Add a regression test confirming JSON-B still populates
    them via standard property mapping after the creator runs, so they
    aren't silently dropped on deserialization.
    
    Solves PZ-12221

--- src/test/kotlin/nl/info/client/zgw/zrc/model/ZaakInformatieobjectTest.kt
+++ src/test/kotlin/nl/info/client/zgw/zrc/model/ZaakInformatieobjectTest.kt
@@ -7,6 +7,7 @@ package nl.info.client.zgw.zrc.model
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.checkUnnecessaryStub
+import jakarta.json.bind.JsonbBuilder
 import nl.info.client.zgw.model.createZaakInformatieobjectForCreatesAndUpdates
 import java.util.UUID
 
@@ -22,6 +23,30 @@ class ZaakInformatieobjectTest : BehaviorSpec({
 
             then("it returns the UUID extracted from the zaak URI") {
                 result shouldBe zaakUUID
+            }
+        }
+    }
+
+    given("a GET response JSON payload with all fields populated, including titel and beschrijving") {
+        val jsonb = JsonbBuilder.create()
+        val json = """
+            {
+                "url": "https://example.com/zaakinformatieobjecten/${UUID.randomUUID()}",
+                "uuid": "${UUID.randomUUID()}",
+                "informatieobject": "https://example.com/informatieobjecten/${UUID.randomUUID()}",
+                "zaak": "https://example.com/zaken/${UUID.randomUUID()}",
+                "aardRelatieWeergave": "Hoort bij, omgekeerd: kent",
+                "titel": "fakeTitel",
+                "beschrijving": "fakeBeschrijving"
+            }
+        """.trimIndent()
+
+        `when`("it is deserialized via JSON-B") {
+            val zaakInformatieobject = jsonb.fromJson(json, ZaakInformatieobject::class.java)
+
+            then("titel and beschrijving are populated, not silently dropped") {
+                zaakInformatieobject.titel shouldBe "fakeTitel"
+                zaakInformatieobject.beschrijving shouldBe "fakeBeschrijving"
             }
         }
     }
