4596ea16201bc703b614d777c0ff02efc5a86ff6
felixcicatt <104420825+felixcicatt@users.noreply.github.com>
2026-08-10 07:41:53 +0000

    cleanup

--- src/main/app/src/app/shared/table-zoek-filters/date-range-filter/date-range-filter.component.spec.ts
+++ src/main/app/src/app/shared/table-zoek-filters/date-range-filter/date-range-filter.component.spec.ts
@@ -6,7 +6,7 @@
 import { HarnessLoader } from "@angular/cdk/testing";
 import { TestbedHarnessEnvironment } from "@angular/cdk/testing/testbed";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { TranslateService } from "@ngx-translate/core";
+import { TranslateModule } from "@ngx-translate/core";
 import { MAT_DATE_LOCALE, MatNativeDateModule } from "@angular/material/core";
 import {
   MatEndDateHarness,
