39c905f1972ac4988eda34b30416dd07c8a2a539
DaxRiool <dax_riool@hotmail.com>
2026-08-12 14:01:59 +0200

    code changes after sdd flow: removed zaakUUID from endpoint and updated frontend

--- src/main/app/src/app/informatie-objecten/informatie-object-view/informatie-object-view.component.ts
+++ src/main/app/src/app/informatie-objecten/informatie-object-view/informatie-object-view.component.ts
@@ -150,17 +150,13 @@ export class InformatieObjectViewComponent
       this.route.data.subscribe((data) => {
         this.infoObject =
           data.informatieObject as GeneratedType<"RestEnkelvoudigInformatieobject">;
-        this.zaak = data.zaak as GeneratedType<"RestZaak">;
         this.informatieObjectenService
-          .readEnkelvoudigInformatieobject(
-            this.infoObject.uuid!,
-            this.zaak?.uuid,
-          )
+          .readEnkelvoudigInformatieobject(this.infoObject.uuid!)
           .subscribe((infoObject) => {
             this.laatsteVersieInfoObject = infoObject;
-            this.toevoegenActies();
             this.updateVersieInformatie();
-            this.loadZaakInformatieobjecten();
+            this.
+            loadZaakInformatieobjecten();
           });
         this.documentPreviewBeschikbaar = FileFormatUtil.isPreviewAvailable(
           this.infoObject.formaat as FileFormat,
@@ -377,7 +373,7 @@ export class InformatieObjectViewComponent
 
   private loadInformatieObject() {
     this.informatieObjectenService
-      .readEnkelvoudigInformatieobject(this.infoObject.uuid!, this.zaak?.uuid)
+      .readEnkelvoudigInformatieobject(this.infoObject.uuid!)
       .subscribe((infoObject) => {
         this.infoObject = infoObject;
         this.laatsteVersieInfoObject = infoObject;
@@ -391,12 +387,7 @@ export class InformatieObjectViewComponent
 
   protected haalVersieOp(versie: number) {
     this.websocketService.removeListener(this.documentListener);
-    this.router.navigate([
-      "/informatie-objecten",
-      this.infoObject.uuid,
-      versie,
-      this.zaak?.uuid,
-    ]);
+    this.router.navigate(["/informatie-objecten", this.infoObject.uuid, versie]);
   }
 
   protected versieToegevoegd(
@@ -465,19 +456,23 @@ export class InformatieObjectViewComponent
   }
 
   /**
-   * Voor het geval dat er bij navigatie naar het enkelvoudiginformatieobject geen zaak meegegeven is,
-   * dan wordt deze via de verkorte zaak gegevens opgehaald.
+   * De zaak van het document wordt via de gekoppelde zaakinformatieobjecten opgehaald,
+   * zodat de knoppen in het linkermenu pas gebouwd worden nadat de zaak (indien aanwezig) bekend is.
    *
-   * Als er ook geen verkorte zaak gegevens beschikbaar, dan is dit een document zonder zaak.
+   * Als er geen gekoppelde zaak is, is dit een document zonder zaak.
    */
   private loadZaak() {
     const zaakobject = this.zaakInformatieObjecten.at(0);
-    if (!this.zaak && zaakobject?.zaakIdentificatie) {
+    if (zaakobject?.zaakIdentificatie) {
       this.zakenService
         .readZaakByID(zaakobject.zaakIdentificatie)
         .subscribe((zaak) => {
           this.zaak = zaak;
+          this.toevoegenActies();
         });
+    } else {
+      this.zaak = undefined;
+      this.toevoegenActies();
     }
   }
 }
