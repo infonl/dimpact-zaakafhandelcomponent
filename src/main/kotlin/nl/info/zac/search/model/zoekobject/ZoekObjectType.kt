665fbef8a19502d83eefc8ecea0f87e2af05a672
Edgar Vonk <edgar@info.nl>
2026-09-03 11:02:29 +0200

    fix: reindex zaak before taak and document during full reindex
    Zaak is the most important object type in ZAC, so reindexAll must
    process it first regardless of ZoekObjectType's declaration order.
    
    Solves PZ-12562

--- src/main/kotlin/nl/info/zac/search/model/zoekobject/ZoekObjectType.kt
+++ src/main/kotlin/nl/info/zac/search/model/zoekobject/ZoekObjectType.kt
@@ -5,7 +5,9 @@
 package nl.info.zac.search.model.zoekobject
 
 enum class ZoekObjectType(val zoekObjectClass: Class<out ZoekObject>) {
-    TAAK(TaakZoekObject::class.java),
+    // The order is important here.
+    // For a full reindex we always want to reindex zaken first, before taken and documenten.
     ZAAK(ZaakZoekObject::class.java),
+    TAAK(TaakZoekObject::class.java),
     DOCUMENT(DocumentZoekObject::class.java)
 }
