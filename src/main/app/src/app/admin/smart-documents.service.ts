c466dae8fc9b362aaad1f610301e0714803f2b82
DaxRiool <dax_riool@hotmail.com>
2026-09-09 17:24:18 +0200

    removed staletime

--- src/main/app/src/app/admin/smart-documents.service.ts
+++ src/main/app/src/app/admin/smart-documents.service.ts
@@ -8,7 +8,6 @@ import { queryOptions } from "@tanstack/angular-query-experimental";
 import { lastValueFrom, map } from "rxjs";
 import { PostBody } from "../shared/http/http-client";
 import { ZacHttpClient } from "../shared/http/zac-http-client";
-import { StaleTimes } from "../shared/http/zac-query-client";
 import { GeneratedType } from "../shared/utils/generated-types";
 
 export interface TemplateMapping {
@@ -40,7 +39,6 @@ export class SmartDocumentsService {
     return queryOptions({
       queryKey: ["smartDocumentsTemplatesMapping", zaakafhandelUUID],
       queryFn: () => lastValueFrom(this.getTemplatesMapping(zaakafhandelUUID)),
-      staleTime: StaleTimes.Instant,
     });
   }
 
